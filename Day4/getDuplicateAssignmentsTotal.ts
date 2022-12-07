5927bce01fac0dd62e6a3fa7736210db8ac425fd
kaBeech <contact@kaBeech.com>
2022-12-07 14:23:54 -0800

    Scaffold getDuplicateAssignmentsTotal

--- Day4/getDuplicateAssignmentsTotal.ts
+++ Day4/getDuplicateAssignmentsTotal.ts
@@ -2,22 +2,28 @@ import { convertMultiLineFileToArray } from "../tools/conversionFunctions.ts";
 
 let total = 0;
 
-const assignmentsCouples = [] as string[][]
+const assignmentsCouples = [] as string[][];
 
 const populateassignmentsCouples = (assignmentsString: string) => {
-  if (assignmentsString.length < 1) return
+  if (assignmentsString.length < 1) return;
   return assignmentsCouples.push(assignmentsString.split(","));
-}
+};
+
+const sumDuplicateAssignments = (assignmentsCouple) => {
+  total += checkForDuplicateAssignment();
+};
 
 const getDuplicateAssignmentsTotal = async (assignmentsFile: string) => {
   total = 0;
-  assignmentsCouples.splice(0, assignmentsCouples.length)
+  assignmentsCouples.splice(0, assignmentsCouples.length);
 
   const assignmentsStrings = await convertMultiLineFileToArray(
     assignmentsFile,
   ) as string[];
 
   assignmentsStrings.forEach(populateassignmentsCouples);
+
+  assignmentsCouples.forEach(sumDuplicateAssignments);
 
   return total;
 };
