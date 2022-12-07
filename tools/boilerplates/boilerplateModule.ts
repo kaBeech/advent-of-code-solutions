99a484e2c6d328b771cac29e074f32590ce9093b
kaBeech <contact@kaBeech.com>
2022-12-07 14:17:40 -0800

    Add console.log to remove warning squiggles

--- tools/boilerplates/boilerplateModule.ts
+++ tools/boilerplates/boilerplateModule.ts
@@ -3,9 +3,11 @@ import { convertMultiLineFileToArray } from "../../tools/conversionFunctions.ts"
 let total = 0;
 
 const boilerplateFunction = async (input: string) => {
+  total = 0;
+
   const inputStringArray = await convertMultiLineFileToArray(input) as string[];
 
-  total = 0;
+  console.log(inputStringArray)
 
   return total;
 };
