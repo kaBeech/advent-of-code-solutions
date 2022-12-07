62999b1730d33d11bb067b08c307435f5ca47194
kaBeech <contact@kaBeech.com>
2022-12-07 15:12:02 -0800

    Add dummy return

--- Day4/checkForDuplicateAssignment.ts
+++ Day4/checkForDuplicateAssignment.ts
@@ -1,8 +1,7 @@
-import { convertMultiLineFileToArray } from "../tools/conversionFunctions.ts";
 import { assignmentCouple } from "./types.ts";
 
 const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
-
+    return 1
 };
 
 export { checkForDuplicateAssignment };
