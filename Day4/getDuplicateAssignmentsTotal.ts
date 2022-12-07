71e79d701cb9cdb3e91352c6049b86a7e05a2c24
kaBeech <contact@kaBeech.com>
2022-12-07 14:13:52 -0800

    Add input formatting

--- Day4/getDuplicateAssignmentsTotal.ts
+++ Day4/getDuplicateAssignmentsTotal.ts
@@ -2,12 +2,22 @@ import { convertMultiLineFileToArray } from "../tools/conversionFunctions.ts";
 
 let total = 0;
 
-const getDuplicateAssignmentsTotal = async (input: any) => {
-  if (typeof (input) === "string") {
-    input = await convertMultiLineFileToArray(input) as any[];
-  }
+const assignmentsCoupleArray = [] as string[][]
 
+const populateAssignmentsCoupleArray = (assignmentsString: string) => {
+  if (assignmentsString.length < 1) return
+  return assignmentsCoupleArray.push(assignmentsString.split(","));
+}
+
+const getDuplicateAssignmentsTotal = async (assignmentsFile: string) => {
   total = 0;
+  assignmentsCoupleArray.splice(0, assignmentsCoupleArray.length)
+
+  const assigmentsStringArray = await convertMultiLineFileToArray(
+    assignmentsFile,
+  ) as string[];
+
+  assigmentsStringArray.forEach(populateAssignmentsCoupleArray);
 
   return total;
 };
