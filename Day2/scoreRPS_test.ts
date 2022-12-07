180ed8e032b9480c0a00aed60fb8d59daba21227
kaBeech <contact@kaBeech.com>
2022-12-06 21:24:04 -0800

    Consolidate Day2 tests

--- Day2/scoreRPS_test.ts
+++ Day2/scoreRPS_test.ts
@@ -11,14 +11,26 @@ Deno.test("testInput returns a number", async () => {
     assertEquals("number", typeof(result))
 });
 
-Deno.test("testInput returns 15", async () => {
+Deno.test("testInput returns 15 with simple method", async () => {
     const result = await scoreRPS(testInput, "simple");
 
     assertEquals(15, result)
 });
 
-Deno.test("testInput2 returns 9", async () => {
+Deno.test("testInput2 returns 9 with simple method", async () => {
     const result2 = await scoreRPS(testInput2, "simple");
 
     assertEquals(9, result2)
+});
+
+Deno.test("testInput returns 12 with crypto method", async () => {
+    const result = await scoreRPS(testInput, "crypto");
+
+    assertEquals(12, result)
+});
+
+Deno.test("testInput2 returns 14 with crypto method", async () => {
+    const result2 = await scoreRPS(testInput2, "crypto");
+
+    assertEquals(14, result2)
 });
