9bdaad6f731db1d4f618b533a2cca97fde71d6b9
kaBeech <contact@kaBeech.com>
2022-12-07 15:21:16 -0800

    Implement checkForDuplicateAssignment

--- Day4/checkForDuplicateAssignment.ts
+++ Day4/checkForDuplicateAssignment.ts
@@ -1,7 +1,21 @@
 import { assignmentCouple } from "./types.ts";
 
 const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
-    return 1
+  if (assignmentsCouple.length < 2) {
+    throw new Error(`Couple too short: ${assignmentsCouple}`);
+  }
+  if (assignmentsCouple.length > 2) {
+    throw new Error(`Couple too long: ${assignmentsCouple}`);
+  }
+  if (
+    assignmentsCouple[0][0] === assignmentsCouple[1][0] ||
+    assignmentsCouple[0][2] === assignmentsCouple[1][2]
+  ) return 1;
+  if (
+    assignmentsCouple[0][0] < assignmentsCouple[1][0] &&
+    assignmentsCouple[0][2] > assignmentsCouple[1][2]
+  ) return 1;
+  return 0;
 };
 
 export { checkForDuplicateAssignment };
