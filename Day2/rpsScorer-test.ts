7b1152ca9f222554f8ea4cbb7fa2591340757f46
kaBeech <contact@kaBeech.com>
2022-12-06 16:44:15 -0800

    Add tests

--- Day2/rpsScorer-test.ts
+++ Day2/rpsScorer-test.ts
@@ -1,10 +1,15 @@
 
-import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { assertEquals, assertInstanceOf } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { rpsScorer } from "./rpsScorer.ts"
 
 const testInput = "testInput.txt";
 const testInput2 = "testInput2.txt";
 
+Deno.test("testInput returns a number", async () => {
+    const result = await rpsScorer(testInput);
+
+    assertInstanceOf(result, Number)
+});
 
 Deno.test("testInput returns 15", async () => {
     const result = await rpsScorer(testInput);
