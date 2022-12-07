13c33007cf6b39b07c43ed0d22149701f8472b27
kaBeech <contact@kaBeech.com>
2022-12-07 13:45:41 -0800

    Adjust boilerplateModule

--- tools/boilerplates/boilerplateModule.ts
+++ tools/boilerplates/boilerplateModule.ts
@@ -2,10 +2,8 @@ import { convertMultiLineFileToArray } from "../../tools/conversionFunctions.ts"
 
 let total = 0;
 
-const boilerplateFunction = async (input: any) => {
-  if (typeof (input) === "string") {
-    input = await convertMultiLineFileToArray(input) as any[];
-  }
+const boilerplateFunction = async (input: string) => {
+  const inputStringArray = await convertMultiLineFileToArray(input) as string[];
 
   total = 0;
 
