17d58437b400f53b680496f4c01f15931279c3d0
kaBeech <contact@kaBeech.com>
2022-12-07 13:11:59 -0800

    Start Day 4

---
+++ Day4/boilerplateModule.ts
@@ -0,0 +1,15 @@
+import { convertMultiLineFileToArray } from "../tools/conversionFunctions.ts";
+
+let total = 0;
+
+const boilerplateFunction = async (input: any) => {
+  if (typeof (input) === "string") {
+    input = await convertMultiLineFileToArray(input) as any[];
+  }
+
+  total = 0;
+
+  return total;
+};
+
+export { boilerplateFunction };
