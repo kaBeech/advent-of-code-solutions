2f1fce6018f2e6defbf238629d1a5b6686dbb004
kaBeech <contact@kaBeech.com>
2022-12-07 14:42:42 -0800

    Add boilerplate checkForDuplicateAssignment

---
+++ Day4/checkForDuplicateAssignment.ts
@@ -0,0 +1,8 @@
+import { convertMultiLineFileToArray } from "../tools/conversionFunctions.ts";
+import { assignmentCouple } from "./types.ts";
+
+const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
+
+};
+
+export { checkForDuplicateAssignment };
