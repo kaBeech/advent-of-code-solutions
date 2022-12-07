cee87662c23c0b968cfaea909fb168680e1ac3b7
kaBeech <contact@kaBeech.com>
2022-12-06 18:04:57 -0800

    Add outcomeScorer

--- Day2/rpsScorer.ts
+++ Day2/rpsScorer.ts
@@ -1,6 +1,7 @@
 import { Round } from "./types";
 
 let shapeSubtotal = 0;
+let outcomeSubtotal = 0;
 
 const shapeScorer = (round: Round) => {
     switch(round[0]) {
@@ -13,14 +14,46 @@ const shapeScorer = (round: Round) => {
     }
 }
 
+const outcomeScorer = (round: Round) => {
+    switch(round[0]) {
+        case "A":
+             switch(round[2]) {
+                case "X":
+                    return outcomeSubtotal += 3;
+                case "Y":
+                    return outcomeSubtotal += 0;
+                case "Z":
+                    return outcomeSubtotal += 6;
+             }
+             break;
+        case "B":
+            switch(round[2]) {
+                case "X":
+                    return outcomeSubtotal += 6;
+                case "Y":
+                    return outcomeSubtotal += 3;
+                case "Z":
+                    return outcomeSubtotal += 0;
+            }
+            break;
+        case "C":
+            switch(round[2]) {
+                case "X":
+                    return outcomeSubtotal += 0;
+                case "Y":
+                    return outcomeSubtotal += 6;
+                case "Z":
+                    return outcomeSubtotal += 3;
+            }
+    }
+}
 
 const rpsScorer = async (input: string) => {
     const inputString = await Deno.readTextFile(input);
     const rounds = inputString.split(/\n/);
     
     rounds.forEach(shapeScorer)
-
-
+    rounds.forEach(outcomeScorer)
     
     return rounds.length
 }
