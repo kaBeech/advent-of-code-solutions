f3bc0fac5ca7525f9646e0671707abb3faa4a2ce
kaBeech <contact@kaBeech.com>
2022-12-07 16:57:59 -0800

    Handle partial overlap

--- Day4/checkForDuplicateAssignment.ts
+++ Day4/checkForDuplicateAssignment.ts
@@ -1,6 +1,6 @@
 import { assignmentCouple } from "./types.ts";
 
-const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
+const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple, overlapMethod: "full" | "partial") => {
   if (assignmentsCouple.length < 2) {
     throw new Error(`Couple too short: ${assignmentsCouple}`);
   }
@@ -9,6 +9,15 @@ const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
   }
   const assignment1 = assignmentsCouple[0].split("-");
   const assignment2 = assignmentsCouple[1].split("-");
+
+  if (overlapMethod === "partial") {
+    if (
+        !(+assignment1[1] < +assignment2[0] ||
+          +assignment2[1] < +assignment1[0])
+      ) return 0;
+      return 1;
+  }
+
   if (
     +assignment1[0] === +assignment2[0] ||
     +assignment1[1] === +assignment2[1]
@@ -19,6 +28,12 @@ const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
     +assignment1[0] > +assignment2[0] &&
       +assignment1[1] < +assignment2[1]
   ) return 1;
+
+  if (
+    !(+assignment1[1] < +assignment2[0] ||
+      +assignment2[1] < +assignment1[0])
+  ) return 1;
+
   return 0;
 };
 
