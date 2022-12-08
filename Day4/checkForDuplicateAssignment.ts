a00dad25c3e81a0f7da1b435c637212eb3c1221b
kaBeech <contact@kaBeech.com>
2022-12-07 16:35:30 -0800

    Convert assignmentstrings to numbers before comparing

--- Day4/checkForDuplicateAssignment.ts
+++ Day4/checkForDuplicateAssignment.ts
@@ -10,14 +10,14 @@ const checkForDuplicateAssignment = (assignmentsCouple: assignmentCouple) => {
   const assignment1 = assignmentsCouple[0].split("-");
   const assignment2 = assignmentsCouple[1].split("-");
   if (
-    assignment1[0] === assignment2[0] ||
-    assignment1[1] === assignment2[1]
+    +assignment1[0] === +assignment2[0] ||
+    +assignment1[1] === +assignment2[1]
   ) return 1;
   if (
-    assignment1[0] < assignment2[0] &&
-      assignment1[1] > assignment2[1] ||
-    assignment1[0] > assignment2[0] &&
-      assignment1[1] < assignment2[1]
+    +assignment1[0] < +assignment2[0] &&
+      +assignment1[1] > +assignment2[1] ||
+    +assignment1[0] > +assignment2[0] &&
+      +assignment1[1] < +assignment2[1]
   ) return 1;
   return 0;
 };
