bb381b0f901abb609b74fb720af44e6360307c23
kaBeech <contact@kaBeech.com>
2022-12-08 03:57:34 -0800

    Make checkForDuplicates tests sync

--- Day6/checkForDuplicates_test.ts
+++ Day6/checkForDuplicates_test.ts
@@ -4,20 +4,20 @@ import { checkForDuplicates } from "./checkForDuplicates.ts";
 const testInput = ["A", "b", "C", "D"];
 const testInput2 = ["b", "b", "Z", "X"];
 
-Deno.test("testInput returns a boolean", async () => {
-  const result = await checkForDuplicates(testInput);
+Deno.test("testInput returns a boolean", () => {
+  const result =  checkForDuplicates(testInput);
 
   assertEquals("boolean", typeof (result));
 });
 
-Deno.test("testInput returns false", async () => {
-  const result = await checkForDuplicates(testInput);
+Deno.test("testInput returns false", () => {
+  const result = checkForDuplicates(testInput);
 
   assertEquals(false, result);
 });
 
-Deno.test("testInput2 returns true", async () => {
-  const result = await checkForDuplicates(testInput2);
+Deno.test("testInput2 returns true", () => {
+  const result = checkForDuplicates(testInput2);
 
   assertEquals(true, result);
 });
