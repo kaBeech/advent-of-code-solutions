959e618603e9d32041eab80bcfeb5806d2ea9d10
kaBeech <contact@kaBeech.com>
2022-12-08 01:27:18 -0800

    Write Part 2 tests

--- Day5/getTopCrates_test.ts
+++ Day5/getTopCrates_test.ts
@@ -3,28 +3,52 @@ import { getTopCrates } from "./getTopCrates.ts";
 
 const testInput = "testInput.txt";
 const testInput2 = "testInput2.txt";
-const part1Input = "instructions.txt";
+const challengeInput = "instructions.txt";
 
-Deno.test("testInput returns a number", async () => {
-  const result = await getTopCrates(testInput);
+Deno.test("testInput returns a number with single method", async () => {
+  const result = await getTopCrates(testInput, "single");
 
   assertEquals("string", typeof (result));
 });
 
-Deno.test("testInput returns 'CMZ'", async () => {
-  const result = await getTopCrates(testInput);
+Deno.test("testInput returns 'CMZ' with single method", async () => {
+  const result = await getTopCrates(testInput, "single");
 
   assertEquals("CMZ", result);
 });
 
-Deno.test("testInput2 returns 'ZQW'", async () => {
-  const result = await getTopCrates(testInput2);
+Deno.test("testInput2 returns 'ZQW' with single method", async () => {
+  const result = await getTopCrates(testInput2, "single");
 
   assertEquals("ZQW", result);
 });
 
-Deno.test("part1Input returns 'CVCWCRTVQ'", async () => {
-  const result = await getTopCrates(part1Input);
+Deno.test("part1Input returns 'CVCWCRTVQ' with single method", async () => {
+  const result = await getTopCrates(challengeInput, "single");
+
+  assertEquals("CVCWCRTVQ", result);
+});
+
+Deno.test("testInput returns a number with bulk method", async () => {
+  const result = await getTopCrates(testInput, "bulk");
+
+  assertEquals("string", typeof (result));
+});
+
+Deno.test("testInput returns 'MCD' with bulk method", async () => {
+  const result = await getTopCrates(testInput, "bulk");
+
+  assertEquals("MCD", result);
+});
+
+Deno.test("testInput2 returns 'NQW' with bulk method", async () => {
+  const result = await getTopCrates(testInput2, "bulk");
+
+  assertEquals("NQW", result);
+});
+
+Deno.test("part1Input returns 'CVCWCRTVQ' with bulk method", async () => {
+  const result = await getTopCrates(challengeInput, "bulk");
 
   assertEquals("CVCWCRTVQ", result);
 });
