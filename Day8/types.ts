f5edd60ed3d6ba864b56120c16adb06dd4588603
kaBeech <contact@kaBeech.com>
2022-12-13 01:44:07 -0800

    Cleanup

--- Day8/types.ts
+++ Day8/types.ts
@@ -1,4 +1,4 @@
-import { integer, ModalBoolean, SingleDigitInteger } from "../tools/commonTypes.ts";
+import { ModalBoolean, SingleDigitInteger } from "../tools/commonTypes.ts";
 
 interface Tree {
   location: number[];
