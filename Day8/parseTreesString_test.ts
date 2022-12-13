3b6998a4a56f9bc06328b6c7b786611aaca10926
kaBeech <contact@kaBeech.com>
2022-12-12 20:35:50 -0800

    Update parstTreesString tests

--- Day8/parseTreesString_test.ts
+++ Day8/parseTreesString_test.ts
@@ -4,6 +4,19 @@ import { parseTreesString } from "./parseTreesString.ts";
 const testInput = "testInput.txt";
 const testInput2 = "testInput2.txt";
 
+Deno.test("every tree returned has a visibility array that is 4 elements long", async () => {
+  const result = await parseTreesString(testInput);
+  let invalidVisibilityCounter = 0;
+  for (const tree of result.trees) {
+    if (tree.visibility.length !== 4) {
+      invalidVisibilityCounter += 1;
+      console.log(tree.visibility);
+    }
+  }
+
+  assertEquals(0, invalidVisibilityCounter);
+});
+
 Deno.test("every tree returned has a location array that is 2 elements long", async () => {
   const result = await parseTreesString(testInput);
   let invalidLocationCounter = 0;
