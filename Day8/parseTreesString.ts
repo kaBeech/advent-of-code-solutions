b2f8b162fe2e0d3e03955f3732a29f8de8b963de
kaBeech <contact@kaBeech.com>
2022-12-13 02:13:43 -0800

    Pass tests

--- Day8/parseTreesString.ts
+++ Day8/parseTreesString.ts
@@ -1,12 +1,30 @@
+import { SingleDigitInteger } from "../tools/commonTypes.ts";
+import Tree from "./Tree.ts";
 import { TreeMap } from "./types.ts";
 
 const parseTreesString = async (treesFile: string) => {
-
   const treesString = await Deno.readTextFile(treesFile);
+  let columnCounter = 0;
+  let rowCounter = 0;
+  const trees = [];
   const treesStringTrimmed = treesString.trimEnd();
-  treesStringTrimmed.split(/\n/);
   
-  return "test" as unknown as TreeMap;
+  for (const char of treesStringTrimmed) {
+    if (char === "\n") {
+      columnCounter = 0;
+      rowCounter += 1;
+    }
+    if (char === "0" || (+char > 0 && +char < 10)) {
+      const tree = Tree(rowCounter, columnCounter, +char as SingleDigitInteger);
+      trees.push(tree);
+      columnCounter += 1;
+    }
+  }
+
+  return {
+    trees,
+    sideLength: columnCounter,
+  } as unknown as TreeMap;
 };
 
 export { parseTreesString };
