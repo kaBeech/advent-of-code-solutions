e94cc4710d318fc743a90a92a0f6f4caa3fd0dba
kaBeech <contact@kaBeech.com>
2022-12-12 19:31:53 -0800

    Remove unused conversionFunctions

--- tools/conversionFunctions.ts
+++ tools/conversionFunctions.ts
@@ -4,23 +4,13 @@ const convertMultiLineFileToArray = async (input: string) => {
   return inputStringTrimmed.split(/\n/);
 };
 
-const convertMultiLineStringToArray = (inputString: string) => {
-  return inputString.split(/\n/);
-};
-
 const convertMultiParagraphFileToArray = async (input: string) => {
   const inputString = await Deno.readTextFile(input);
   const inputStringTrimmed = inputString.trimEnd()
   return inputStringTrimmed.split(/\n\n/);
 };
 
-const convertMultiParagraphStringToArray = (inputString: string) => {
-  return inputString.split(/\n\n/);
-};
-
 export {
   convertMultiLineFileToArray,
-  convertMultiLineStringToArray,
   convertMultiParagraphFileToArray,
-  convertMultiParagraphStringToArray,
 };
