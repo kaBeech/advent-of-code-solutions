f8ccf449454f6dce18c3cae83b05acd5577c87d7
kaBeech <contact@kaBeech.com>
2022-12-13 15:04:43 -0800

    Break out verifyXYCoordinates

--- tools/conversionFunctions/convertXYCoordinatesToIndexNumber.ts
+++ tools/conversionFunctions/convertXYCoordinatesToIndexNumber.ts
@@ -1,36 +1,10 @@
+import { verifyXYCoordinates } from "../verificationFunctions/verifyXYCoordinates.ts";
+
 const convertXYCoordinatesToIndexNumber = (
   xyCoordinates: number[],
   base: number,
 ) => {
-  if (base < 0 || base % 1 !== 0) {
-    throw new Error(
-      `Base must be a positive integer! Received: ${base}`,
-    );
-  }
-  if (xyCoordinates.length < 2) {
-    throw new Error(
-      `Coordinate array must have 2 elements - input is too short! Received: [${xyCoordinates}]`,
-    );
-  }
-  if (xyCoordinates.length > 2) {
-    throw new Error(
-      `Coordinate array must have 2 elements - input is too long! Received: [${xyCoordinates}]`,
-    );
-  }
-  for (const coordinate of xyCoordinates) {
-    if (coordinate % 1 !== 0) {
-      throw new Error(
-        `Coordinates must all be integers! Received: [${xyCoordinates}]`,
-      );
-    }
-    if (coordinate < 0 || coordinate >= base) {
-      throw new Error(
-        `Coordinates must all be in domain! Received Coordinates: [${xyCoordinates}], Domain: 0 to ${
-          base - 1
-        }`,
-      );
-    }
-  }
+  verifyXYCoordinates(xyCoordinates, base);
 
   const index = (xyCoordinates[0] + (xyCoordinates[1] * base));
 
