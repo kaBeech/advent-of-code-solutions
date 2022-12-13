46e44b06adb6069ce62fb0b131fcd6e3f11d71d1
kaBeech <contact@kaBeech.com>
2022-12-13 04:09:25 -0800

    Update testInput

--- Day8/checkVisibility_test.ts
+++ Day8/checkVisibility_test.ts
@@ -3,45 +3,46 @@ import {
   assertThrows,
 } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { checkVisibility } from "./checkVisibility.ts";
-import { TreeMap } from "./types.ts";
+import { parseTreesString } from "./parseTreesString.ts";
 
-const testTreeMap = 0 as unknown as TreeMap;
-const testTreeMap2 = 0 as unknown as TreeMap;
 
-Deno.test("testTreeMap returns a boolean", () => {
-  const result = checkVisibility(24, testTreeMap, 0);
+const testTreeMap = parseTreesString("testInput.txt")
+const testTreeMap2 = parseTreesString("testInput2.txt")
+
+Deno.test("testTreeMap returns a boolean", async () => {
+  const result = checkVisibility(24, await testTreeMap, 0);
 
   assertEquals("boolean", typeof (result));
 });
 
-Deno.test("visible tree  at the edge (in negative direction) returns true", () => {
-  const result = checkVisibility(8, testTreeMap2, 0);
+Deno.test("visible tree  at the edge (in negative direction) returns true", async () => {
+  const result = checkVisibility(8, await testTreeMap2, 0);
 
   assertEquals(true, result);
 });
 
-Deno.test("visible tree in the middle (in positive direction) returns true", () => {
-  const result = checkVisibility(24, testTreeMap, 2);
+Deno.test("visible tree in the middle (in positive direction) returns true", async () => {
+  const result = checkVisibility(24, await testTreeMap, 2);
 
   assertEquals(true, result);
 });
 
-Deno.test("non-visible tree in positive direction returns false", () => {
-  const result = checkVisibility(24, testTreeMap2, 3);
+Deno.test("non-visible tree in positive direction returns false", async () => {
+  const result = checkVisibility(24, await testTreeMap2, 3);
 
   assertEquals(false, result);
 });
 
-Deno.test("non-visible tree in negative direction returns false", () => {
-  const result = checkVisibility(5, testTreeMap, 1);
+Deno.test("non-visible tree in negative direction returns false", async () => {
+  const result = checkVisibility(5, await testTreeMap, 1);
 
   assertEquals(false, result);
 });
 
 Deno.test("calling with a non-integer index throws error", () => {
   assertThrows(
-    () => {
-      checkVisibility(24.5, testTreeMap, 2);
+    async () => {
+      checkVisibility(24.5, await testTreeMap, 2);
     },
     Error,
     "Index must be a positive integer: 24.5",
@@ -50,8 +51,8 @@ Deno.test("calling with a non-integer index throws error", () => {
 
 Deno.test("calling with index outside of domain throws error", () => {
   assertThrows(
-    () => {
-      checkVisibility(24000, testTreeMap, 2);
+    async () => {
+      checkVisibility(24000, await testTreeMap, 2);
     },
     Error,
     "Index must be within domain! Index: 24000, Domain: 0-24",
