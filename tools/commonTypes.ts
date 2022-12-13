f4a0f6541c258de50cfd18e67f8a84e304b13be4
kaBeech <contact@kaBeech.com>
2022-12-13 00:45:04 -0800

    Add SingleDigitInteger type

--- tools/commonTypes.ts
+++ tools/commonTypes.ts
@@ -10,8 +10,10 @@ type integer =
   | "8"
   | "9";
 
+type SingleDigitInteger = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
 type twoDigitInteger = `${integer}${integer}`;
 
 type ModalBoolean = true | false | null;
 
-export type { integer, twoDigitInteger, ModalBoolean };
+export type { integer, ModalBoolean, twoDigitInteger, SingleDigitInteger };
