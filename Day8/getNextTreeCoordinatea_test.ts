61d6cd4559cfc1acd1fb11ccd363340ad9d698ee
kaBeech <contact@kaBeech.com>
2022-12-13 15:22:43 -0800

    Write tests

---
+++ Day8/getNextTreeCoordinatea_test.ts
@@ -0,0 +1,28 @@
+import {
+  assertEquals,
+} from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { getNextTreeCoordinates } from "./getNextTreeCoordinates.ts";
+
+Deno.test("correct coordinates are returned for direction 0", () => {
+  const result = getNextTreeCoordinates([57, 32], 99, 0);
+
+  assertEquals(result, [56, 32]);
+});
+
+Deno.test("correct coordinates are returned for direction 1", () => {
+  const result = getNextTreeCoordinates([57, 32], 99, 1);
+
+  assertEquals(result, [57, 31]);
+});
+
+Deno.test("correct coordinates are returned for direction 2", () => {
+  const result = getNextTreeCoordinates([57, 32], 99, 2);
+
+  assertEquals(result, [58, 32]);
+});
+
+Deno.test("correct coordinates are returned for direction 3", () => {
+  const result = getNextTreeCoordinates([57, 32], 99, 3);
+
+  assertEquals(result, [57, 33]);
+});
