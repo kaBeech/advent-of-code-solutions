907754ebd6cddb522f1de2ab98513ead1b9a7eec
kaBeech <contact@kaBeech.com>
2022-12-13 02:08:42 -0800

    Cleanup

--- Day8/parseTreesString_test.ts
+++ Day8/parseTreesString_test.ts
@@ -4,45 +4,6 @@ import { parseTreesString } from "./parseTreesString.ts";
 const testInput = "testInput.txt";
 const testInput2 = "testInput2.txt";
 
-Deno.test("every tree returned has a visibility array that is 4 elements long", async () => {
-  const result = await parseTreesString(testInput);
-  let invalidVisibilityCounter = 0;
-  for (const tree of result.trees) {
-    if (tree.visibility.length !== 4) {
-      invalidVisibilityCounter += 1;
-      console.log(tree.visibility);
-    }
-  }
-
-  assertEquals(invalidVisibilityCounter, 0);
-});
-
-Deno.test("every tree returned has a location array that is 2 elements long", async () => {
-  const result = await parseTreesString(testInput);
-  let invalidLocationCounter = 0;
-  for (const tree of result.trees) {
-    if (tree.location.length !== 2) {
-      invalidLocationCounter += 1;
-      console.log(tree.location);
-    }
-  }
-
-  assertEquals(invalidLocationCounter, 0);
-});
-
-Deno.test("every tree returned has a location array composed of integers", async () => {
-  const result = await parseTreesString(testInput);
-  let invalidLocationCounter = 0;
-  for (const tree of result.trees) {
-    if (tree.location[0] % 1 !== 0 || tree.location[1] % 1 !== 0) {
-      invalidLocationCounter += 1;
-      console.log(tree.location);
-    }
-  }
-
-  assertEquals(invalidLocationCounter, 0);
-});
-
 Deno.test("testInput returns an object incuding an integer reporting the sideLength", async () => {
   const result = await parseTreesString(testInput);
 
