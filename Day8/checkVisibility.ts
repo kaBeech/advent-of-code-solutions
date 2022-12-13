02b257a68bdecad78b961ed66b8287a2879d223b
kaBeech <contact@kaBeech.com>
2022-12-12 20:51:28 -0800

    Scaffold checkVisibility

---
+++ Day8/checkVisibility.ts
@@ -0,0 +1,9 @@
+import { OrthagonalDirection2D, TreeMap } from "./types.ts";
+
+const checkVisibility = (treeIndex: number, treeMap: TreeMap, direction: OrthagonalDirection2D) => {
+
+  if (true) return true;
+  return false
+};
+
+export { checkVisibility };
