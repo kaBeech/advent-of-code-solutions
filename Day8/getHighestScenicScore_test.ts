75d6c46f9a5dda37898969e7bac9983f6a3e0252
kaBeech <contact@kaBeech.com>
2022-12-13 15:28:00 -0800

    Write tests

---
+++ Day8/getHighestScenicScore_test.ts
@@ -0,0 +1,31 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { getHighestScenicScore } from "./getHighestScenicScore.ts";
+
+const testInput = "testInput.txt";
+const testInput2 = "testInput2.txt";
+const challengeInput = "treeGrid.txt";
+
+
+Deno.test("testInput returns a number", async () => {
+  const result = await getHighestScenicScore(testInput);
+
+  assertEquals(typeof (result), "number");
+});
+
+Deno.test("testInput returns 8", async () => {
+  const result = await getHighestScenicScore(testInput);
+
+  assertEquals(result, 8);
+});
+
+Deno.test("testInput2 returns 36", async () => {
+  const result = await getHighestScenicScore(testInput2);
+
+  assertEquals(result, 36);
+});
+
+Deno.test("challengeInput returns 1818", async () => {
+  const result = await getHighestScenicScore(challengeInput);
+
+  assertEquals(result, 1818);
+});
