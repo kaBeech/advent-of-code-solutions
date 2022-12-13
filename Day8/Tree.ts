5b42dba693c09646def166bb9c0b8f418208e8e6
kaBeech <contact@kaBeech.com>
2022-12-13 00:31:55 -0800

    Flesh out Tree

--- Day8/Tree.ts
+++ Day8/Tree.ts
@@ -1,17 +1,43 @@
+import { ModalBoolean } from "../tools/commonTypes.ts";
 import { integer } from "../tools/commonTypes.ts";
+import { OrthagonalDirection2D } from "./types.ts";
+
+interface TreeState {
+  location: number[];
+  height: integer;
+  visibility: ModalBoolean[];
+}
+
+const locationGetter = (state: TreeState) => ({
+  getLocation: () => state.location,
+});
+
+const heightGetter = (state: TreeState) => ({
+  getHeight: () => state.height,
+});
+
+const visibilityGetter = (state: TreeState) => ({
+  getVisibility: () => state.visibility,
+});
+
+const visibilitySetter = (state: TreeState) => ({
+  setVisibility: (visibility: boolean, direction: OrthagonalDirection2D) => {
+    state.visibility[direction] = visibility;
+  },
+});
 
 const Tree = (row: number, column: number, height: integer) => {
-    const state = {
-        location: [row, column];
-        height,
-        visibility: [null, null, null, null] as ModalBoolean[];
-      }
-    return {
-        ...locationGetter(state),
-        ...heightGetter(state),
-        ...visibilityGetter(state),
-        ...visibilitySetter(state),
-    };
+  const state = {
+    location: [row, column],
+    height,
+    visibility: [null, null, null, null],
   };
-  
-  export default Tree;
+  return {
+    ...locationGetter(state),
+    ...heightGetter(state),
+    ...visibilityGetter(state),
+    ...visibilitySetter(state),
+  };
+};
+
+export default Tree;
