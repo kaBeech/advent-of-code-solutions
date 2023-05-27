64c26ab29621d121651170e5fe234677a33c8800
kaBeech <contact@kaBeech.com>
2023-05-27 15:12:14 -0700

    Write handleHeadPositionChange function

--- Day9/handleHeadPositionChange.ts
+++ Day9/handleHeadPositionChange.ts
@@ -1,14 +1,30 @@
 import { XYCoordinate } from "../tools/commonTypes.ts";
+import { getDifference } from "../tools/mathFunctions/getDifference.ts";
 
 const handleHeadPositionChange = (
-  headPosition: XYCoordinate,
   currentPosition: XYCoordinate,
+  headPosition: XYCoordinate,
 ): XYCoordinate => {
-  console.log(currentPosition, headPosition);
+  const xDifference = getDifference(headPosition[0], currentPosition[0]);
+  const yDifference = getDifference(headPosition[1], currentPosition[1]);
 
-  const result: XYCoordinate = [0, 0];
+  // if (( xDifference === 2 && yDifference === 2) || xDifference > 2 || yDifference > 2 ) { throw error }
 
-  return result;
+  if (
+    xDifference < 2 &&
+    yDifference < 2
+  ) {
+    return currentPosition;
+  }
+
+  const newPosition = [currentPosition[0], currentPosition[1]];
+
+  newPosition[1] > currentPosition[1] ? newPosition[1]++ : newPosition;
+  newPosition[1] < currentPosition[1] ? newPosition[1]-- : newPosition;
+  newPosition[0] < currentPosition[0] ? newPosition[0]-- : newPosition;
+  newPosition[0] > currentPosition[0] ? newPosition[0]++ : newPosition;
+
+  return newPosition as XYCoordinate;
 };
 
 export { handleHeadPositionChange };
