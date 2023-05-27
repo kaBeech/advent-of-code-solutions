771b102fd95980c0d7a648e71634db1b0ad87776
kaBeech <contact@kaBeech.com>
2023-05-27 13:47:09 -0700

    Update test format

--- Day9/tests/countVisitedPositions_test.ts
+++ Day9/tests/countVisitedPositions_test.ts
@@ -4,32 +4,32 @@ import {
 } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { countVisitedPositions } from "../countVisitedPositions.ts";
 
-const testInput = "tests/testInput.txt";
+const testInput1 = "tests/testInput.txt";
 const testInput2 = "tests/testInput2.txt";
 // const challengeInput = "tests/ropeMovementDirections.txt";
 
-Deno.test("testInput returns an integer", async () => {
-  const result = await countVisitedPositions(testInput);
+Deno.test("returns an integer", async () => {
+  const result1 = await countVisitedPositions(testInput1);
+  const result2 = await countVisitedPositions(testInput2);
 
-  assertEquals(result, Math.floor(result));
+  assertEquals(result1, Math.floor(result1));
+  assertEquals(result2, Math.floor(result2));
 });
 
-Deno.test("testInput returns a positive number", async () => {
-  const result = await countVisitedPositions(testInput);
+Deno.test("returns a positive number", async () => {
+  const result1 = await countVisitedPositions(testInput1);
+  const result2 = await countVisitedPositions(testInput2);
 
-  assert(result >= 0);
+  assert(result1 >= 0);
+  assert(result2 >= 0);
 });
 
-Deno.test("testInput returns 13", async () => {
-  const result = await countVisitedPositions(testInput);
+Deno.test("returns correct answer", async () => {
+  const result1 = await countVisitedPositions(testInput1);
+  const result2 = await countVisitedPositions(testInput2);
 
-  assertEquals(result, 13);
-});
-
-Deno.test("testInput2 returns 6", async () => {
-  const result = await countVisitedPositions(testInput2);
-
-  assertEquals(result, 6);
+  assertEquals(result1, 13);
+  assertEquals(result2, 6);
 });
 
 // Deno.test("challengeInput returns 0", async () => {
