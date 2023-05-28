2020da6ea86664852b4d0da75f6cd27c88332707
kaBeech <contact@kaBeech.com>
2023-05-27 20:51:14 -0700

    Cleanup

--- Day9/handleHeadPositionChange.ts
+++ Day9/handleHeadPositionChange.ts
@@ -26,36 +26,10 @@ const handleHeadPositionChange = (
 
   const newTailPosition = [currentTailPosition[0], currentTailPosition[1]];
 
-  // HEAD
-  if (xDifference === ropeLength) {
-    headPosition[0] < currentTailPosition[0]
-      ? newTailPosition[0]--
-      : newTailPosition[0]++;
-    if (yDifference === ropeLength - 1) {
-      headPosition[1] < currentTailPosition[1]
-        ? newTailPosition[1]--
-        : newTailPosition[1]++;
-    }
-  }
-
-  if (yDifference === ropeLength) {
-    headPosition[1] < currentTailPosition[1]
-      ? newTailPosition[1]--
-      : newTailPosition[1]++;
-    if (xDifference === ropeLength - 1) {
-      headPosition[0] < currentTailPosition[0]
-        ? newTailPosition[0]--
-        : newTailPosition[0]++;
-    }
-  }
-  //
-      headPosition[1] >
-      currentTailPosition[1] &&
-    newTailPosition[1]++;
+  headPosition[1] > currentTailPosition[1] && newTailPosition[1]++;
   headPosition[1] < currentTailPosition[1] && newTailPosition[1]--;
   headPosition[0] < currentTailPosition[0] && newTailPosition[0]--;
   headPosition[0] > currentTailPosition[0] && newTailPosition[0]++;
-  //parent of 7e66d9a (Make Rope length modular)
   return newTailPosition as XYCoordinate;
 };
 
