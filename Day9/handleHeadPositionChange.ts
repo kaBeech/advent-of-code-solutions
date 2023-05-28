02bfaa00334349c571409023b16fe507ad0af5ce
kaBeech <contact@kaBeech.com>
2023-05-27 20:49:11 -0700

    Revert "Make Rope length modular"
    This reverts commit 7e66d9ab011bb21dd34b3e8ab505bc6484d4e9a4.

--- Day9/handleHeadPositionChange.ts
+++ Day9/handleHeadPositionChange.ts
@@ -4,23 +4,29 @@ import { getDifference } from "../tools/mathFunctions/getDifference.ts";
 const handleHeadPositionChange = (
   currentTailPosition: XYCoordinate,
   headPosition: XYCoordinate,
-  ropeLength: number,
 ): XYCoordinate => {
   const xDifference = getDifference(headPosition[0], currentTailPosition[0]);
   const yDifference = getDifference(headPosition[1], currentTailPosition[1]);
 
   if (
-    (xDifference === ropeLength && yDifference === ropeLength) ||
-    xDifference > ropeLength ||
-    yDifference > ropeLength
+    (xDifference === 2 && yDifference === 2) || xDifference > 2 ||
+    yDifference > 2
   ) {
     throw new Error(
       `Head position is too far away from (i.e. not adjacent to) current tail position! Received currentTailPosition: [${currentTailPosition}], headPosition: [${headPosition}]`,
     );
   }
 
+  if (
+    xDifference < 2 &&
+    yDifference < 2
+  ) {
+    return currentTailPosition;
+  }
+
   const newTailPosition = [currentTailPosition[0], currentTailPosition[1]];
 
+  // HEAD
   if (xDifference === ropeLength) {
     headPosition[0] < currentTailPosition[0]
       ? newTailPosition[0]--
@@ -42,7 +48,14 @@ const handleHeadPositionChange = (
         : newTailPosition[0]++;
     }
   }
-
+  //
+      headPosition[1] >
+      currentTailPosition[1] &&
+    newTailPosition[1]++;
+  headPosition[1] < currentTailPosition[1] && newTailPosition[1]--;
+  headPosition[0] < currentTailPosition[0] && newTailPosition[0]--;
+  headPosition[0] > currentTailPosition[0] && newTailPosition[0]++;
+  //parent of 7e66d9a (Make Rope length modular)
   return newTailPosition as XYCoordinate;
 };
 
