7e66d9ab011bb21dd34b3e8ab505bc6484d4e9a4
kaBeech <contact@kaBeech.com>
2023-05-27 20:05:41 -0700

    Make Rope length modular

--- Day9/handleHeadPositionChange.ts
+++ Day9/handleHeadPositionChange.ts
@@ -4,32 +4,34 @@ import { getDifference } from "../tools/mathFunctions/getDifference.ts";
 const handleHeadPositionChange = (
   currentTailPosition: XYCoordinate,
   headPosition: XYCoordinate,
+  ropeLength: number,
 ): XYCoordinate => {
   const xDifference = getDifference(headPosition[0], currentTailPosition[0]);
   const yDifference = getDifference(headPosition[1], currentTailPosition[1]);
 
   if (
-    (xDifference === 2 && yDifference === 2) || xDifference > 2 ||
-    yDifference > 2
+    (xDifference === ropeLength && yDifference === ropeLength) ||
+    xDifference > ropeLength ||
+    yDifference > ropeLength
   ) {
     throw new Error(
       `Head position is too far away from (i.e. not adjacent to) current tail position! Received currentTailPosition: [${currentTailPosition}], headPosition: [${headPosition}]`,
     );
   }
 
-  if (
-    xDifference < 2 &&
-    yDifference < 2
-  ) {
-    return currentTailPosition;
-  }
-
   const newTailPosition = [currentTailPosition[0], currentTailPosition[1]];
 
-  headPosition[1] > currentTailPosition[1] && newTailPosition[1]++;
-  headPosition[1] < currentTailPosition[1] && newTailPosition[1]--;
-  headPosition[0] < currentTailPosition[0] && newTailPosition[0]--;
-  headPosition[0] > currentTailPosition[0] && newTailPosition[0]++;
+  if (xDifference === ropeLength) {
+    headPosition[0] < currentTailPosition[0]
+      ? newTailPosition[1]--
+      : newTailPosition[1]++;
+  }
+
+  if (yDifference === ropeLength) {
+    headPosition[1] < currentTailPosition[1]
+      ? newTailPosition[0]--
+      : newTailPosition[0]++;
+  }
 
   return newTailPosition as XYCoordinate;
 };
