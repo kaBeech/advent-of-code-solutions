d33df9fb4708e9f16ed370f835da4cc0066ffcb1
kaBeech <contact@kaBeech.com>
2023-05-28 18:43:53 -0700

    Solve Part1

--- 2021-Deno-Fast/Day2/app.ts
+++ 2021-Deno-Fast/Day2/app.ts
@@ -2,19 +2,41 @@ import { convertMultiLineStringToArray } from "../../tools/conversionFunctions/c
 
 const app = (async (
   challengeInputFile?: string,
-): Promise<{ solutionPart1: number; solutionPart2: number }> => {
+): Promise<{ solutionPart1: any; solutionPart2: any }> => {
   if (!challengeInputFile) {
-    challengeInputFile = "testInput.txt";
+    challengeInputFile = "challengeInput.txt";
   }
 
   const challengeInput = await Deno.readTextFile(challengeInputFile);
   const challengeInputFormatted = convertMultiLineStringToArray(challengeInput);
 
-  const solutionPart1 = +challengeInputFormatted[0];
-  const solutionPart2 = 0;
+  let horizPos = 0;
+  let depth = 0;
 
-  console.log(`Part 1: What is the answer to Part 1?
-    Solution: ${solutionPart1}`);
+  challengeInputFormatted.forEach((movementInstruction) => {
+    const movementInstructionFormatted = movementInstruction.split(" ");
+    switch (movementInstructionFormatted[0][0]) {
+      case "f":
+        horizPos += +movementInstructionFormatted[1];
+        break;
+      case "d":
+        depth += +movementInstructionFormatted[1];
+        break;
+      case "u":
+        depth -= +movementInstructionFormatted[1];
+        break;
+      default:
+        console.log(`Error!!!!!!`);
+    }
+  });
+
+  const solutionPart1 = depth * horizPos;
+  const solutionPart2 = +challengeInputFormatted[0];
+
+  console.log(
+    `What do you get if you multiply your final horizontal position by your final depth?
+    Solution: ${solutionPart1}`,
+  );
   console.log(`Part 2: What is the answer to Part 2?
     Solution: ${solutionPart2}`);
 
