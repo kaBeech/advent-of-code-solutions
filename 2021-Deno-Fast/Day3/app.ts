d6606c6ab6c1936e5251ea9651d5896fac48d453
kaBeech <contact@kaBeech.com>
2023-05-28 20:38:24 -0700

    Setup workspace

---
+++ 2021-Deno-Fast/Day3/app.ts
@@ -0,0 +1,77 @@
+import { convertMultiLineStringToArray } from "../../tools/conversionFunctions/convertStringToArray.ts";
+
+const app = (async (
+  challengeInputFile?: string,
+): Promise<
+  {
+    testSolution: { solutionPart1: any; solutionPart2: any };
+    challengeSolution: { solutionPart1: any; solutionPart2: any };
+  }
+> => {
+  if (!challengeInputFile) {
+    challengeInputFile = "challengeInput.txt";
+  }
+
+  //
+  // NO EDITS ABOVE THIS LINE
+  //
+
+  const testInput = await Deno.readTextFile("testInput.txt");
+  const testInputFormatted = convertMultiLineStringToArray(testInput);
+  const challengeInput = await Deno.readTextFile(challengeInputFile);
+  const challengeInputFormatted = convertMultiLineStringToArray(challengeInput);
+
+  //
+  // ONLY INPUT FORMATTING ABOVE THIS LINE
+  //
+
+  const getSolution = (input: any[], test: boolean) => {
+    let solutionAlert = "Challenge ";
+    test && (solutionAlert = "Test ");
+
+    // Set up variables
+    //
+
+    input.forEach((input) => {
+      // Format input
+      //
+
+      // Compute
+      //
+    });
+
+    // Update solution calculation
+    //
+    const solutionPart1 = +challengeInputFormatted[0];
+    const solutionPart2 = +challengeInputFormatted[0];
+
+    //
+    // ONLY QUESTION PHRASING EDITS BELOW THIS LINE
+    //
+
+    console.log(`Part 1: What is the answer to Part 1?
+    ${solutionAlert} Solution: ${solutionPart1}`);
+    console.log(`Part 2: What is the answer to Part 2?
+    ${solutionAlert} Solution: ${solutionPart2}`);
+    console.log("Done!");
+
+    //
+    // NO EDITS BELOW THIS LINE
+    //
+
+    return {
+      solutionPart1,
+      solutionPart2,
+    };
+  };
+
+  const testSolution = getSolution(testInputFormatted, true);
+  const challengeSolution = getSolution(challengeInputFormatted, false);
+
+  return {
+    testSolution,
+    challengeSolution,
+  };
+})();
+
+export { app };
---
+++ 2021-Deno-Fast/Day3/retrospective.md
@@ -0,0 +1,42 @@
+# What went well during the sprint?
+
+### That was fast and fun!
+
+- Yeah!
+
+###
+
+- This is a real simple program, but I feel the code I used to get the correct solution is much more readable than Day 1's was
+
+- This likely helped me get to the correct solution quickly!
+
+### Destructively editing Part 2 app.ts = <3
+
+- Copying the Part 1 app.ts fort archive also = <3
+    - Just make sure to write the Part 2 app in the right file!
+
+## Takeaways
+
+- Have fun!
+
+# What can I learn from difficult points?
+
+### Remember to update input
+
+- This time I had the opposite issue as Day1 - I forgot I was using testInput and wondering why my answer was off!
+
+- Actually, I went ahead and edited my boilerplate app.ts so it runs both the testInput and the challengeInput. This way I don't have to remember as much - I just have to pay attention to the console!
+
+## Takeaways
+
+- Check inputs and outputs when necessary!
+
+- But also, set things up so you don't have to remember to do as much!
+
+# Ideas for possible future implementation
+
+### Update boilerplate app.ts to include prompts for what code to enter where
+
+### Use the lap function on your stopwatch!!
+
+- That way you don't have to add your times together ;)
