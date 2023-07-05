24ca957c8805560b9eaf0ec67b6c8a8dc886c86c
kaBeech <contact@kaBeech.com>
2023-07-05 12:31:01 -0700

    Update solvePart1 module

--- 2022-Deno-Thorough/Day11/solvePart1.ts
+++ 2022-Deno-Thorough/Day11/solvePart1.ts
@@ -1,9 +1,12 @@
+import { getMonkeyBusinessLevel } from "./getMonkeyBusinessLevel.ts";
+import { playKeepAway } from "./playKeepAway.ts";
 import { populateMonkeys } from "./populateMonkeys.ts";
 import { MonkeyType } from "./types.ts";
 
 const solvePart1 = async (challengeInput: string): Promise<number> => {
   const monkeys: MonkeyType[] = await populateMonkeys(challengeInput);
-  return 0;
+  playKeepAway(monkeys, 20);
+  return getMonkeyBusinessLevel(monkeys);
 };
 
 export { solvePart1 };
