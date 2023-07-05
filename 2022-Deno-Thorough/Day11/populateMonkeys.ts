5ad105dfa7070fa6997599caaa9b5a7e2b5a0d8d
kaBeech <contact@kaBeech.com>
2023-07-05 14:24:29 -0700

    Add tests

--- 2022-Deno-Thorough/Day11/populateMonkeys.ts
+++ 2022-Deno-Thorough/Day11/populateMonkeys.ts
@@ -1,18 +1,23 @@
 import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
 import { Monkey } from "./Monkey.ts";
-import { MonkeyState } from "./types.ts";
+import { MonkeyState, MonkeyType } from "./types.ts";
 
 const populateMonkeys = async (
   challengeInput: string,
-): Promise<MonkeyState[]> => {
+  test?: "test" | undefined,
+): Promise<MonkeyType[] | MonkeyState[]> => {
   const challengeInputArray = await convertMultiParagraphFileToArray(
     challengeInput,
   );
-  const monkeys: MonkeyState[] = [];
+  const monkeys: MonkeyType[] = [];
+  const monkeyStates: MonkeyType[] = [];
   challengeInputArray.forEach((monkeyInput) => {
     const monkey = Monkey();
     monkeys.push(monkey);
   });
+  if (test) {
+    return monkeyStates;
+  }
   return monkeys;
 };
 
--- 2022-Deno-Thorough/Day11/tests/populateMonkeysTest.ts
+++ 2022-Deno-Thorough/Day11/tests/populateMonkeysTest.ts
@@ -1,11 +1,18 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { testMonkeys } from "./testMonkeys.ts";
+import { testMonkeys, testMonkeyStates } from "./testMonkeys.ts";
 import { populateMonkeys } from "../populateMonkeys.ts";
 
 const testInput = "tests/testInput.txt";
 
-Deno.test("testMonkeys returns 10605", async () => {
+Deno.test("populateMonkeys creates monkeys", async () => {
   const result = await populateMonkeys(testInput);
-
   assertEquals(result, testMonkeys);
+});
+
+Deno.test("populateMonkeys sets the correct monkeyState for each monkey", async () => {
+  const result = await populateMonkeys(testInput, "test");
+
+  result.forEach((monkeyState, index) => {
+    assertEquals(monkeyState, testMonkeyStates[index]);
+  });
 });
