572561d3ad14a4cefd74986e41584589543cfe3b
kaBeech <contact@kaBeech.com>
2023-07-05 16:52:44 -0700

    Add getMonkeyBusinessLevel calculation

--- 2022-Deno-Thorough/Day11/getMonkeyBusinessLevel.ts
+++ 2022-Deno-Thorough/Day11/getMonkeyBusinessLevel.ts
@@ -1,9 +1,27 @@
-import { MonkeyState, MonkeyType } from "./types.ts";
+import { MonkeyType } from "./types.ts";
 
 const getMonkeyBusinessLevel = (
   monkeys: MonkeyType[],
 ): number => {
-  return 0;
+  let mostActiveMonkey = monkeys[0];
+  let secondMostActiveMonkey = monkeys[1];
+  monkeys.forEach((monkey) => {
+    if (
+      monkey.getTotalItemsInspected() >
+        mostActiveMonkey.getTotalItemsInspected()
+    ) {
+      secondMostActiveMonkey = mostActiveMonkey;
+      mostActiveMonkey = monkey;
+    } else if (
+      monkey.getTotalItemsInspected() >
+        secondMostActiveMonkey.getTotalItemsInspected()
+    ) {
+      secondMostActiveMonkey = monkey;
+    }
+  });
+  const monkeyBusinessLevel = mostActiveMonkey.getTotalItemsInspected() *
+    secondMostActiveMonkey.getTotalItemsInspected();
+  return monkeyBusinessLevel;
 };
 
 export { getMonkeyBusinessLevel };
