49bf025eb78a06ce33badc34c5bd77e260b8608f
kaBeech <contact@kaBeech.com>
2023-07-05 12:24:52 -0700

    Scaffold populateMonkeys

---
+++ 2022-Deno-Thorough/Day11/populateMonkeys.ts
@@ -0,0 +1,17 @@
+import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { Monkey } from "./Monkey.ts";
+import { MonkeyType } from "./types.ts";
+
+const populateMonkeys = async (
+  challengeInput: string,
+): Promise<MonkeyType[]> => {
+  const challengeInputArray = await convertMultiParagraphFileToArray(
+    challengeInput,
+  );
+  const monkeys: MonkeyType[] = [];
+  challengeInputArray.forEach((line) => {
+    const monkey = Monkey();
+    monkeys.push(monkey);
+  });
+  return monkeys;
+};
