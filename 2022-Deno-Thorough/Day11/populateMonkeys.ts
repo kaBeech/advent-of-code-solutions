850ff59ff8e24f0cbd65684d199da70c3bb56c45
kaBeech <contact@kaBeech.com>
2023-07-05 15:45:13 -0700

    Add regex parsing

--- 2022-Deno-Thorough/Day11/populateMonkeys.ts
+++ 2022-Deno-Thorough/Day11/populateMonkeys.ts
@@ -1,3 +1,4 @@
+import { Operator } from "../../tools/commonTypes.ts";
 import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
 import { Monkey } from "./Monkey.ts";
 import { MonkeyState, MonkeyType } from "./types.ts";
@@ -12,7 +13,27 @@ const populateMonkeys = async (
   const monkeys: MonkeyType[] = [];
   const monkeyStates: MonkeyType[] = [];
   challengeInputArray.forEach((monkeyInput) => {
-    const monkey = Monkey();
+    const name = +(monkeyInput[0].match(/\d+/) as RegExpMatchArray);
+    const itemsByWorryLevel: number[] = [];
+    const itemsByWorryLevelStrings = monkeyInput[1].match(/\d+/g);
+    itemsByWorryLevelStrings?.forEach((item) => itemsByWorryLevel.push(+item));
+    const operator =
+      (monkeyInput[2].match(/[+]|[*]/) as RegExpMatchArray)[0] as Operator;
+    let operand: RegExpMatchArray | null | number | "old" = monkeyInput[2]
+      .match(/\d+/);
+    operand ? operand = +operand : operand = "old";
+    const divisor = +(monkeyInput[3].match(/\d+/) as RegExpMatchArray);
+    const trueDestination = +(monkeyInput[4].match(/\d+/) as RegExpMatchArray);
+    const falseDestination = +(monkeyInput[5].match(/\d+/) as RegExpMatchArray);
+    const monkey = Monkey(
+      name,
+      itemsByWorryLevel,
+      operator,
+      operand,
+      divisor,
+      trueDestination,
+      falseDestination,
+    );
     monkeys.push(monkey);
   });
   if (test) {
