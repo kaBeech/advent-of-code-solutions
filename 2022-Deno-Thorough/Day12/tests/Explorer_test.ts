9562f587a9d2453702059253be3b56964b7e30bf
kaBeech <contact@kaBeech.com>
2023-07-13 23:35:46 -0700

    Add Explorer test

---
+++ 2022-Deno-Thorough/Day12/tests/Explorer_test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { Explorer } from "../Explorer.ts";
+import { populateTileMap } from "../populateTileMap.ts";
+
+const testTileMap = await populateTileMap("tests/testInput.txt");
+
+const testExplorer = Explorer(testTileMap.startTile, testTileMap.endTile);
+
+Deno.test("exploring the testTileMap yields shortestPath of 31 steps", () => {
+  assertEquals(testExplorer.explore(), 31);
+});
