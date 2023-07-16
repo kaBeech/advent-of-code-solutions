c005372f2769172d9cc5d5c61afcc0da785de2e8
kaBeech <contact@kaBeech.com>
2023-07-16 16:41:22 -0700

    Keep -1 index from being valid for adjacent tiles

--- 2022-Deno-Thorough/Day12/Tile.ts
+++ 2022-Deno-Thorough/Day12/Tile.ts
@@ -44,7 +44,7 @@ const getAdjacentTiles = (allTiles: TileType[][], x: number, y: number) => {
   const adjacentCoordinates = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]];
   const adjacentTiles: TileType[] = [];
   adjacentCoordinates.forEach(([x, y]) => {
-    if (allTiles[y]?.[x] !== undefined) {
+    if (x >= 0 && y >= 0 && x < allTiles[0].length && y < allTiles.length) {
       adjacentTiles.push(allTiles[y][x]);
     }
   });
