75a95ea14dc2d847d33b08a9f24a828ff707b2b7
kaBeech <contact@kaBeech.com>
2023-07-16 19:38:20 -0700

    Add test output

--- 2022-Deno-Thorough/Day12/app.ts
+++ 2022-Deno-Thorough/Day12/app.ts
@@ -12,18 +12,20 @@ const app = (async (
     testInputFile = "tests/testInput.txt";
   }
 
-  // const testSolutionPart1 = await solvePart1(testInputFile);
-  const testSolutionPart2 = await solvePart2(testInputFile);
+  const testSolutionPart1 = await solvePart1(testInputFile);
+  const testSolutionPart2 = solvePart2(testInputFile);
 
   const solutionPart1 = await solvePart1(challengeInputFile);
-  const solutionPart2 = await solvePart2(challengeInputFile);
+  const solutionPart2 = solvePart2(challengeInputFile);
 
   console.log(
     `Part 1: What is the fewest steps required to move from your current position to the location that should get the best signal?
+    Test Solution: ${testSolutionPart1}
     Solution: ${solutionPart1}`,
   );
   console.log(`Part 2: What is the answer to Part 2?
-    Solution: ${solutionPart2}`);
+  Test Solution: ${testSolutionPart2}
+  Solution: ${solutionPart2}`);
 
   return {
     solutionPart1,
