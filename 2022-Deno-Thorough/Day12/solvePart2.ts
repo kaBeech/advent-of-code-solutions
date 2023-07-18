2d8aa92281563304d1f582bfa29dd99ec64d87d6
kaBeech <contact@kaBeech.com>
2023-07-18 14:43:46 -0700

    Connect solvePart2 to the appropriate modules

--- 2022-Deno-Thorough/Day12/solvePart2.ts
+++ 2022-Deno-Thorough/Day12/solvePart2.ts
@@ -1,5 +1,11 @@
-const solvePart2 = (challengeInput: string): string => {
-  return challengeInput;
+import { Explorer } from "./Explorer.ts";
+import { populateTileMap } from "./populateTileMap.ts";
+
+const solvePart2 = async (challengeInput: string): Promise<number> => {
+  const tileMap = await populateTileMap(challengeInput);
+  const explorer = Explorer(tileMap);
+  const shortestPathLength = explorer.findShortestPathToLowestElevation()!;
+  return shortestPathLength;
 };
 
 export { solvePart2 };
