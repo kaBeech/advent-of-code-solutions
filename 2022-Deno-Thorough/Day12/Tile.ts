6de1a0705d2b2012c251663aa76ca2b4efe8ae27
kaBeech <contact@kaBeech.com>
2023-07-18 14:04:16 -0700

    Cleanup

--- 2022-Deno-Thorough/Day12/Tile.ts
+++ 2022-Deno-Thorough/Day12/Tile.ts
@@ -1,6 +1,5 @@
 import { xyCoordinatesGetter } from "../../tools/commonMethods/getMethods.ts";
 import { XYCoordinates } from "../../tools/commonTypes.ts";
-import { TileType } from "./types.ts";
 
 interface TileState {
   coordinates: XYCoordinates;
@@ -41,41 +40,3 @@ const fewestStepsSetter = (state: TileState) => ({
     state.fewestSteps = fewestSteps;
   },
 });
-
-// const adjacentTilesByPreferenceGetter = (
-//   state: TileState,
-// ) => ({
-//   getAdjacentTilesByPreference: () => {
-//     if (!state.tileSurveyed) {
-//       const adjacentTiles = getAdjacentTiles(
-//         state.tileMap.allTiles,
-//         state.coordinates.x,
-//         state.coordinates.y,
-//       );
-//       const accessibleAdjacentTiles = adjacentTiles.filter((tile) =>
-//         tile.getElevation() >= state.elevation - 1
-//       );
-//       state.adjacentTilesByPreference = accessibleAdjacentTiles
-//         .sort((a, b) => {
-//           return a.getDistanceFromFinish() -
-//             b.getDistanceFromFinish();
-//         })
-//         .sort((a, b) => {
-//           return a.getElevation() - b.getElevation();
-//         });
-//       state.tileSurveyed = true;
-//     }
-//     return state.adjacentTilesByPreference;
-//   },
-// });
-
-// const getAdjacentTiles = (allTiles: TileType[][], x: number, y: number) => {
-//   const adjacentCoordinates = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]];
-//   const adjacentTiles: TileType[] = [];
-//   adjacentCoordinates.forEach(([x, y]) => {
-//     if (x >= 0 && y >= 0 && x < allTiles[0].length && y < allTiles.length) {
-//       adjacentTiles.push(allTiles[y][x]);
-//     }
-//   });
-//   return adjacentTiles;
-// };
