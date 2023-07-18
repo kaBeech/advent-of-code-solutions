190ddb61fd2b629ddbe9c60d8f86769ea48c57c4
kaBeech <contact@kaBeech.com>
2023-07-18 13:45:32 -0700

    Revise Tile object

--- 2022-Deno-Thorough/Day12/Tile.ts
+++ 2022-Deno-Thorough/Day12/Tile.ts
@@ -1,15 +1,11 @@
 import { xyCoordinatesGetter } from "../../tools/commonMethods/getMethods.ts";
 import { XYCoordinates } from "../../tools/commonTypes.ts";
-import { getCartesianDistanceXY } from "../../tools/mathFunctions/getCartesianDistance.ts";
-import { TileMap, TileType } from "./types.ts";
+import { TileType } from "./types.ts";
 
 interface TileState {
   coordinates: XYCoordinates;
   elevation: number;
-  tileSurveyed: boolean;
-  adjacentTilesByPreference: TileType[];
-  distanceFromFinish: number | undefined;
-  tileMap: TileMap;
+  fewestSteps: number | undefined;
 }
 
 export { Tile };
@@ -17,22 +13,18 @@ export { Tile };
 const Tile = (
   coordinates: XYCoordinates,
   elevation: number,
-  tileMap: TileMap,
 ) => {
   const state = {
     coordinates,
     elevation,
-    tileMap,
-    tileSurveyed: false,
-    adjacentTilesByPreference: [] as TileType[],
-    distanceFromFinish: undefined as number | undefined,
+    fewestSteps: undefined,
   };
 
   return {
     ...elevationGetter(state),
     ...xyCoordinatesGetter(state),
-    ...adjacentTilesByPreferenceGetter(state),
-    ...distanceFromFinishGetter(state),
+    ...fewestStepsGetter(state),
+    ...fewestStepsSetter(state),
   };
 };
 
@@ -40,51 +32,50 @@ const elevationGetter = (state: TileState) => ({
   getElevation: () => state.elevation,
 });
 
-const adjacentTilesByPreferenceGetter = (
-  state: TileState,
-) => ({
-  getAdjacentTilesByPreference: () => {
-    if (!state.tileSurveyed) {
-      const adjacentTiles = getAdjacentTiles(
-        state.tileMap.allTiles,
-        state.coordinates.x,
-        state.coordinates.y,
-      );
-      const accessibleAdjacentTiles = adjacentTiles.filter((tile) =>
-        tile.getElevation() >= state.elevation - 1
-      );
-      state.adjacentTilesByPreference = accessibleAdjacentTiles
-        .sort((a, b) => {
-          return a.getDistanceFromFinish() -
-            b.getDistanceFromFinish();
-        })
-        .sort((a, b) => {
-          return a.getElevation() - b.getElevation();
-        });
-      state.tileSurveyed = true;
-    }
-    return state.adjacentTilesByPreference;
+const fewestStepsGetter = (state: TileState) => ({
+  getFewestSteps: () => state.fewestSteps,
+});
+
+const fewestStepsSetter = (state: TileState) => ({
+  setFewestSteps: (fewestSteps: number) => {
+    state.fewestSteps = fewestSteps;
   },
 });
 
-const getAdjacentTiles = (allTiles: TileType[][], x: number, y: number) => {
-  const adjacentCoordinates = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]];
-  const adjacentTiles: TileType[] = [];
-  adjacentCoordinates.forEach(([x, y]) => {
-    if (x >= 0 && y >= 0 && x < allTiles[0].length && y < allTiles.length) {
-      adjacentTiles.push(allTiles[y][x]);
-    }
-  });
-  return adjacentTiles;
-};
+// const adjacentTilesByPreferenceGetter = (
+//   state: TileState,
+// ) => ({
+//   getAdjacentTilesByPreference: () => {
+//     if (!state.tileSurveyed) {
+//       const adjacentTiles = getAdjacentTiles(
+//         state.tileMap.allTiles,
+//         state.coordinates.x,
+//         state.coordinates.y,
+//       );
+//       const accessibleAdjacentTiles = adjacentTiles.filter((tile) =>
+//         tile.getElevation() >= state.elevation - 1
+//       );
+//       state.adjacentTilesByPreference = accessibleAdjacentTiles
+//         .sort((a, b) => {
+//           return a.getDistanceFromFinish() -
+//             b.getDistanceFromFinish();
+//         })
+//         .sort((a, b) => {
+//           return a.getElevation() - b.getElevation();
+//         });
+//       state.tileSurveyed = true;
+//     }
+//     return state.adjacentTilesByPreference;
+//   },
+// });
 
-const distanceFromFinishGetter = (state: TileState) => ({
-  getDistanceFromFinish: () => {
-    !state.distanceFromFinish &&
-      (state.distanceFromFinish = getCartesianDistanceXY(
-        state.coordinates,
-        state.tileMap.endTile.getCoordinates(),
-      ));
-    return state.distanceFromFinish;
-  },
-});
+// const getAdjacentTiles = (allTiles: TileType[][], x: number, y: number) => {
+//   const adjacentCoordinates = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]];
+//   const adjacentTiles: TileType[] = [];
+//   adjacentCoordinates.forEach(([x, y]) => {
+//     if (x >= 0 && y >= 0 && x < allTiles[0].length && y < allTiles.length) {
+//       adjacentTiles.push(allTiles[y][x]);
+//     }
+//   });
+//   return adjacentTiles;
+// };
