3beee38ac090a8b159f369119107e637fa2bd2c6
kaBeech <contact@kaBeech.com>
2023-07-18 14:44:41 -0700

    Connect solutionPart2

--- 2022-Deno-Thorough/Day12/app.ts
+++ 2022-Deno-Thorough/Day12/app.ts
@@ -4,7 +4,7 @@ import { solvePart2 } from "./solvePart2.ts";
 const app = (async (
   challengeInputFile?: string,
   testInputFile?: string,
-): Promise<{ solutionPart1: number; solutionPart2: string }> => {
+): Promise<{ solutionPart1: number; solutionPart2: number }> => {
   if (!challengeInputFile) {
     challengeInputFile = "tests/challengeInput.txt";
   }
@@ -13,19 +13,21 @@ const app = (async (
   }
 
   const testSolutionPart1 = await solvePart1(testInputFile);
-  const testSolutionPart2 = solvePart2(testInputFile);
+  const testSolutionPart2 = await solvePart2(testInputFile);
 
   const solutionPart1 = await solvePart1(challengeInputFile);
-  const solutionPart2 = solvePart2(challengeInputFile);
+  const solutionPart2 = await solvePart2(challengeInputFile);
 
   console.log(
     `Part 1: What is the fewest steps required to move from your current position to the location that should get the best signal?
     Test Solution: ${testSolutionPart1}
     Solution: ${solutionPart1}`,
   );
-  console.log(`Part 2: What is the answer to Part 2?
+  console.log(
+    `Part 2: What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
   Test Solution: ${testSolutionPart2}
-  Solution: ${solutionPart2}`);
+  Solution: ${solutionPart2}`,
+  );
 
   return {
     solutionPart1,
