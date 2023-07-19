365a389a92612bed65dd1373cebf871e5dcebc75
kaBeech <contact@kaBeech.com>
2023-07-18 18:38:41 -0700

    Cleanup

--- 2022-Deno-Thorough/Day12/tests/getAdjacentTiles_test.ts
+++ 2022-Deno-Thorough/Day12/tests/getAdjacentTiles_test.ts
@@ -1,24 +1,24 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { testExplorerState } from "./testObjects.ts";
+import { testTileMap } from "./testObjects.ts";
 import { getAdjacentTiles } from "../getAdjacentTiles.ts";
 
 Deno.test("getting adjacent tiles on corner tiles returns 2 tiles", () => {
   assertEquals(
-    getAdjacentTiles(testExplorerState.tileMap.allTiles, 0, 0).length,
+    getAdjacentTiles(testTileMap.allTiles, 0, 0).length,
     2,
   );
 });
 
 Deno.test("getting adjacent tiles on side tiles returns 3 tiles", () => {
   assertEquals(
-    getAdjacentTiles(testExplorerState.tileMap.allTiles, 0, 1).length,
+    getAdjacentTiles(testTileMap.allTiles, 0, 1).length,
     3,
   );
 });
 
 Deno.test("getting adjacent tiles on middle tiles returns 4 tiles", () => {
   assertEquals(
-    getAdjacentTiles(testExplorerState.tileMap.allTiles, 1, 1).length,
+    getAdjacentTiles(testTileMap.allTiles, 1, 1).length,
     4,
   );
 });
--- 2022-Deno-Thorough/Day12/tests/testObjects.ts
+++ 2022-Deno-Thorough/Day12/tests/testObjects.ts
@@ -3,42 +3,6 @@ import { populateTileMap } from "../populateTileMap.ts";
 
 const testTileMap = await populateTileMap("tests/testInput.txt");
 
-const testExplorerState = {
-  tileMap: testTileMap,
-  currentTile: testTileMap.endTile,
-  destinationTile: testTileMap.startTile,
-  destinationTileVisited: false,
-  lowestPossibleElevationVisited: false,
-  fewestStepsToLowestPossibleElevation: undefined,
-  queuedTiles: [testTileMap.endTile],
-};
-
-const testExplorerStateOnCornerTile = {
-  tileMap: testTileMap,
-  currentTile: testTileMap.allTiles[0][0],
-  destinationTile: testTileMap.startTile,
-  destinationTileVisited: false,
-  lowestPossibleElevationVisited: false,
-  fewestStepsToLowestPossibleElevation: undefined,
-  queuedTiles: [testTileMap.allTiles[0][0]],
-};
-
-const testExplorerStateOnSideTile = {
-  tileMap: testTileMap,
-  currentTile: testTileMap.allTiles[0][1],
-  destinationTile: testTileMap.startTile,
-  destinationTileVisited: false,
-  lowestPossibleElevationVisited: false,
-  fewestStepsToLowestPossibleElevation: undefined,
-  queuedTiles: [testTileMap.allTiles[0][1]],
-};
-
 const testExplorer = Explorer(testTileMap);
 
-export {
-  testExplorer,
-  testExplorerState,
-  testExplorerStateOnCornerTile,
-  testExplorerStateOnSideTile,
-  testTileMap,
-};
+export { testExplorer, testTileMap };
