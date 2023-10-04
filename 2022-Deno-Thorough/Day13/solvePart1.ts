8f1070af9be19ff0ab02eac4572d608508069994
kaBeech <contact@kaBeech.com>
2023-10-04 01:24:28 -0700

    Add solvePart1 function

--- 2022-Deno-Thorough/Day13/solvePart1.ts
+++ 2022-Deno-Thorough/Day13/solvePart1.ts
@@ -1,5 +1,21 @@
-const solvePart1 = async (challengeInput: string): Promise<string> => {
-  return challengeInput;
+import { convertMultiLineFileToDoubleArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { comparePackets } from "./comparePackets.ts";
+
+const solvePart1 = async (challengeInput: string): Promise<number> => {
+  let indexSum = 0;
+  const packetPairArray = await convertMultiLineFileToDoubleArray(
+    challengeInput,
+  ) as string[][];
+
+  packetPairArray.forEach((packetPair, index) => {
+    const leftPacket = JSON.parse(packetPair[0]);
+    const rightPacket = JSON.parse(packetPair[1]);
+    if (comparePackets(leftPacket, rightPacket) === "right order") {
+      indexSum += index;
+    }
+  });
+
+  return indexSum;
 };
 
 export { solvePart1 };
