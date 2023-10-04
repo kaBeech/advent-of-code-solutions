493fb832f09922d8a01b2b8568f7e2245febfb4b
kaBeech <contact@kaBeech.com>
2023-10-04 02:45:39 -0700

    Process packetPairArray into flat packetArray

--- 2022-Deno-Thorough/Day13/solvePart2.ts
+++ 2022-Deno-Thorough/Day13/solvePart2.ts
@@ -1,5 +1,23 @@
+import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { comparePackets } from "./comparePackets.ts";
+
 const solvePart2 = async (challengeInput: string): Promise<number> => {
-  return 2;
+  let indexSum = 0;
+  const packetPairArray = await convertMultiParagraphFileToArray(
+    challengeInput,
+  ) as string[][];
+  const packetArray = packetPairArray.flat();
+  console.log(packetArray);
+
+  packetPairArray.forEach((packetPair, index) => {
+    const leftPacket = JSON.parse(packetPair[0]);
+    const rightPacket = JSON.parse(packetPair[1]);
+    if (comparePackets(leftPacket, rightPacket) === "right order") {
+      indexSum += index + 1;
+    }
+  });
+
+  return indexSum;
 };
 
 export { solvePart2 };
