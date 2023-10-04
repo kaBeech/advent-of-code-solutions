765b3ccf74bd50017fb643f97e7c7e5fa3d788f9
kaBeech <contact@kaBeech.com>
2023-10-04 03:16:40 -0700

    Add Packet type

--- 2022-Deno-Thorough/Day13/types.ts
+++ 2022-Deno-Thorough/Day13/types.ts
@@ -1,24 +1,5 @@
-type exampleType1 =
-  | "X"
-  | "Y"
-  | "Z";
+type Packet =
+  | number
+  | Array<Packet>;
 
-type exampleType2 =
-  | "A"
-  | "B"
-  | "C";
-
-interface ExampleType3 {
-  exampleFunction: () => number;
-  exampleNullableFunction: () => number | undefined;
-  exampleVoidFunction: (fewestSteps: number) => void;
-}
-
-interface ExampleType4 {
-  exampleProperty: ExampleType3;
-  exampleArray: ExampleType3[][];
-}
-
-type exampleTypeCombined = `${exampleType1} ${exampleType2}`;
-
-export type { exampleType1, exampleType2, exampleTypeCombined };
+export type { Packet };
