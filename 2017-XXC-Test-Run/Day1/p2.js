9cc792aff5b2ff3813be2d62abf204754062deb5
kaBeech <contact@kaBeech.com>
2023-11-07 17:31:05 -0800

    Solve Part 2

--- 2017-XXC-Test-Run/Day1/p2.js
+++ 2017-XXC-Test-Run/Day1/p2.js
@@ -1,3 +1,13 @@
-export default function (ci) {
-  return ci;
-}
+const a = (await Deno.readTextFile("ci.txt")).split("");
+export const p2 = (() => {
+  let r;
+  let s = 0;
+  const l = a.length / 2;
+  a.forEach((c, i) => {
+    r = a[(i + l) % a.length];
+    r === c && (s += +c);
+  });
+  return {
+    s,
+  };
+})();
