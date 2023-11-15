b1ab50568683d5b20a89fb9d065acb3ba99e0a87
kaBeech <contact@kaBeech.com>
2023-11-14 22:21:02 -0800

    Write basic logic

--- 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part2-scala/solve-part-2/src/main/scala/Main.scala
+++ 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part2-scala/solve-part-2/src/main/scala/Main.scala
@@ -1,8 +1,28 @@
 @main def solvePart2: Unit =
-  println("Part 2: " + msg + " " + currentIndex + " " + numberOfStepsTaken)
+  println(
+    "Part 2: " + jumpUntilOutOfBounds(
+      currentIndex,
+      maze,
+      numberOfStepsTaken
+    )
+  )
+
+def maze = List(0, 3, 0, 1, -3)
 
 def currentIndex = 0
 
 def numberOfStepsTaken = 0
 
-def msg = "I was compiled by Scala 3. :)"
+def jumpUntilOutOfBounds(
+    currentIndex: Int,
+    maze: List[Int],
+    numberOfStepsTaken: Int
+): Int =
+  if (currentIndex >= maze.length || currentIndex < 0) numberOfStepsTaken
+  else
+    val newIndex = currentIndex + maze(currentIndex)
+    jumpUntilOutOfBounds(
+      newIndex,
+      maze.updated(currentIndex, maze(currentIndex) + 1),
+      numberOfStepsTaken + 1
+    )
