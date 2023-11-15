c3c4bee3ff7f30abe6be4450dffa144a5a26b06b
kaBeech <contact@kaBeech.com>
2023-11-14 21:23:56 -0800

    Add Part1 and Part2 folders

--- 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part1/parseInput.ts
+++ 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part1/parseInput.ts
@@ -1,10 +1,10 @@
-import { convertMultiLineFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { convertMultiLineFileToArray } from "../../../tools/conversionFunctions/convertFileToArray.ts";
 import { Maze } from "./types.ts";
 
 export const parseInput = async (): Promise<Maze> => {
   const maze: Maze = [];
   const mazeString: string[] = await convertMultiLineFileToArray(
-    "./challengeInput.txt",
+    "../challengeInput.txt",
   );
   mazeString.forEach((jumpInstruction) => {
     maze.push(parseInt(jumpInstruction));
