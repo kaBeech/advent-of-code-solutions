e0dc1deacf709b86397640cbf1a1a684e9402e96
kaBeech <contact@kaBeech.com>
2023-11-14 22:07:48 -0800

    Add variables

--- 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part2-scala/solve-part-2/src/main/scala/Main.scala
+++ 2017-XXC-Test-Run/Day5-no-comments-and-scala/Part2-scala/solve-part-2/src/main/scala/Main.scala
@@ -1,5 +1,8 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+@main def solvePart2: Unit =
+  println("Part 2: " + msg + " " + currentIndex + " " + numberOfStepsTaken)
+
+def currentIndex = 0
+
+def numberOfStepsTaken = 0
 
 def msg = "I was compiled by Scala 3. :)"
