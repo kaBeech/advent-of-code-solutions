2fb0a69295f986d2d9b30a65fa7ae5ee64a571d0
kaBeech <contact@kaBeech.com>
2023-12-04 21:15:40 -0800

    Use arrays instead of tuples

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day02-Carbon/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day02-Carbon/parseInput.ts
@@ -1,7 +1,8 @@
 // Carbon doesn't have much string manipulation or a way to open files built
-// out yet, so I'm using Deno to parse the input into a tuple of tuples.
+// out yet, so I'm using Deno to parse the input into a tuple of tuples that
+// can cast into an array of arrays.
 // Example input: "abc/ndef/nghi"
-// Example output: `(("a", "b", "c"), ("d", "e", "f"), ("g", "h", "i"))`
+// Example output: `(("a", "b", "c", "X"...), ("d", "e", "f", "X"...), ("g", "h", "i", "X"...))`
 
 export const parseInput = (async (): Promise<string> => {
   const input: string = await Deno.readTextFile("./challengeInput.txt");
@@ -18,10 +19,14 @@ export const parseInput = (async (): Promise<string> => {
   return result;
 })();
 
-// Parse the raw string into a pseudo-array
-// Example input: `abcde`
-// Example output: `("a", "b", "c", "d", "e")`
 const parseLine = (line: string): string => {
+  // Carbon also doesn't allow tuple access by index yet, so I'm forcing every line
+  // to be the same length by padding the ending with X's. This way, we can cast
+  // the string to a fixed-length array.
+  while (line.length < 200) {
+    line += "X";
+  }
+
   let parsedLine = `(`;
   const lineArray = line.split("");
   lineArray.forEach((char) => {
