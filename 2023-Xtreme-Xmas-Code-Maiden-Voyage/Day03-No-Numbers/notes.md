25bbc33715fdd7d7a094bd09f2b9b5789e1face6
kaBeech <contact@kaBeech.com>
2023-12-05 02:56:37 -0800

    Add boilerplate and basic info

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-No-Numbers/notes.md
@@ -0,0 +1,12 @@
+# Start Day 3
+
+https://adventofcode.com/2023/day/3
+
+## Pseudocode
+
+### Part 1
+
+Variables: `ElfMap`: Elf[], `Elf`: {id: number, coordinates: XYCoordinates}, `SelectedElf`: Elf
+
+1.  Select `Elf` with id of 42 from `ElfMap`
+2.  Return `SelectedElf`
---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-No-Numbers/parseInput.ts
@@ -0,0 +1,16 @@
+import { convertMultiLineFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { ElfMap } from "./types.ts";
+
+export const parseInput = async (): Promise<ElfMap> => {
+  const elfMap: ElfMap = [];
+  const playerMapString: string[] = await convertMultiLineFileToArray(
+    "./challengeInput.txt",
+  );
+  playerMapString.forEach((rawPlayer, index) => {
+    elfMap.push({
+      id: index,
+      coordinates: { x: parseInt(rawPlayer), y: index },
+    });
+  });
+  return elfMap;
+};
