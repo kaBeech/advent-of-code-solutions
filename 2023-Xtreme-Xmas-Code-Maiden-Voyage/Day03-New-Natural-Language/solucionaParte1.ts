b0b7aa02759ee03376bd28e7d656e06a3977b6c5
kaBeech <contact@kaBeech.com>
2023-12-05 15:43:17 -0800

    Add piece number summing

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/solucionaParte1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/solucionaParte1.ts
@@ -2,7 +2,25 @@ import { analizaEntrada } from "./analizaEntrada.ts";
 import comprobaBaldosasAdyacentes from "./comprobaBaldosasAdyacentes.ts";
 import { Baldosa, MapaDeBaldosas } from "./tipos.ts";
 
-const obteneNúmeroDePieza = (baldoda: Baldosa, filaDeBaldosas): number => {};
+const obteneNúmeroDePieza = (
+  baldosa: Baldosa,
+  filaDeBaldosas: Baldosa[],
+): number => {
+  let baldosaAnterior = filaDeBaldosas[baldosa.coordinadas.x - 1];
+  let baldosaSiguiente = filaDeBaldosas[baldosa.coordinadas.x + 1];
+  let númeroDePieza = `${baldosa.valor}`;
+  while (!isNaN(+baldosaAnterior)) {
+    númeroDePieza = `${baldosaAnterior.valor}` +
+      númeroDePieza;
+    baldosaAnterior = filaDeBaldosas[baldosaAnterior.coordinadas.x - 1];
+  }
+  while (!isNaN(+baldosaSiguiente)) {
+    númeroDePieza = númeroDePieza +
+      `${baldosaSiguiente.valor}`;
+    baldosaSiguiente = filaDeBaldosas[baldosaSiguiente.coordinadas.x + 1];
+  }
+  return +númeroDePieza;
+};
 
 export const solucionaParte1 = (async (): Promise<number> => {
   const mapaDeBaldosas: MapaDeBaldosas = await analizaEntrada();
@@ -18,11 +36,8 @@ export const solucionaParte1 = (async (): Promise<number> => {
           baldosa,
         );
         if (adyacenteAUnSímbolo) {
-          let númeroDePieza = `${baldosa.valor}`;
-          if (!isNaN(+filaDeBaldosas[índice - 1])) {
-            númeroDePieza = `${filaDeBaldosas[índice - 1].valor}` +
-              númeroDePieza;
-          }
+          const númeroDePieza = obteneNúmeroDePieza(baldosa, filaDeBaldosas);
+          sumaDeNúmerosDePiezas += númeroDePieza;
         }
       }
     });
