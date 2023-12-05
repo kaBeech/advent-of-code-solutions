0f5bf7dc931bbd65527c229699ea5f1a0714e28d
kaBeech <contact@kaBeech.com>
2023-12-05 03:31:33 -0800

    Parse input

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-No-Numbers/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-No-Numbers/parseInput.ts
@@ -1,16 +1,30 @@
-import { convertMultiLineFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
-import { ElfMap } from "./types.ts";
+import { convertMultiLineFileToDoubleArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { convertSingleDigitNumberToPseudoNumberString } from "../../tools/conversionFunctions/convertNumberToPseudoNumber.ts";
+import { PseudoNumber, Tile, TileMap, TileValue } from "./types.ts";
 
-export const parseInput = async (): Promise<ElfMap> => {
-  const elfMap: ElfMap = [];
-  const playerMapString: string[] = await convertMultiLineFileToArray(
-    "./challengeInput.txt",
+export const parseInput = async (): Promise<TileMap> => {
+  const rawTileMap: string[][] = await convertMultiLineFileToDoubleArray(
+    "./testInput.txt",
   );
-  playerMapString.forEach((rawPlayer, index) => {
-    elfMap.push({
-      id: index,
-      coordinates: { x: parseInt(rawPlayer), y: index },
+  const tileMap: TileMap = [];
+  rawTileMap.forEach((rawTileRow, index) => {
+    const tileMapRow: Tile[] = [];
+    rawTileRow.forEach((rawTile, index2) => {
+      let value: TileValue;
+      if (!Number.isNaN(+rawTile)) {
+        value = convertSingleDigitNumberToPseudoNumberString(
+          +rawTile,
+        ) as PseudoNumber;
+      } else if (rawTile === ".") {
+        value = ".";
+      } else {
+        value = "X";
+      }
+      tileMapRow.push({
+        value,
+        coordinates: { x: index2, y: index },
+      });
     });
   });
-  return elfMap;
+  return tileMap;
 };
