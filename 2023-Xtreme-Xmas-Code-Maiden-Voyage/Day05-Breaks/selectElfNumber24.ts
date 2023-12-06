12d9b1f4aa270a521e482b46b73fb8898dae6b6a
kaBeech <contact@kaBeech.com>
2023-12-06 01:00:51 -0800

    Cleanup

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day05-Breaks/selectElfNumber24.ts
+++
@@ -1,8 +0,0 @@
-import { Elf, ElfMap } from "./types.ts";
-
-export default function (
-  elfMap: ElfMap,
-): Elf {
-  const elfNumber24 = elfMap.find((elf) => elf.id === 24)!;
-  return elfNumber24;
-}
