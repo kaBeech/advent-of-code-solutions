22695e871de65a402287defd53c7f556d4512d94
kaBeech <contact@kaBeech.com>
2023-12-05 16:04:34 -0800

    Remove infinite loop

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/comprobaBaldosasAdyacentes.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/comprobaBaldosasAdyacentes.ts
@@ -5,21 +5,19 @@ export default function (
   baldosa: Baldosa,
 ): boolean {
   let baldosaEstaAdyacenteAUnSímbolo = false;
-  while (!baldosaEstaAdyacenteAUnSímbolo) {
-    mapaDeBaldosas.forEach((filaDeBaldosas) => {
-      filaDeBaldosas.forEach((baldosaComprobada) => {
-        if (
-          Math.abs(baldosaComprobada.coordinadas.x - baldosa.coordinadas.x) <=
-            1 &&
-          Math.abs(baldosaComprobada.coordinadas.y - baldosa.coordinadas.y) <=
-            1 &&
-          baldosaComprobada.valor === ("X")
-        ) {
-          baldosaEstaAdyacenteAUnSímbolo = true;
-        }
-      });
+  mapaDeBaldosas.forEach((filaDeBaldosas) => {
+    filaDeBaldosas.forEach((baldosaComprobada) => {
+      if (
+        Math.abs(baldosaComprobada.coordinadas.x - baldosa.coordinadas.x) <=
+          1 &&
+        Math.abs(baldosaComprobada.coordinadas.y - baldosa.coordinadas.y) <=
+          1 &&
+        baldosaComprobada.valor === ("X")
+      ) {
+        baldosaEstaAdyacenteAUnSímbolo = true;
+      }
     });
-  }
+  });
 
   return baldosaEstaAdyacenteAUnSímbolo;
 }
