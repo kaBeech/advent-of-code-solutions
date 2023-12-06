f4714e20d58dff2b1d063a70922dddb94430cff6
kaBeech <contact@kaBeech.com>
2023-12-06 00:59:20 -0800

    Solve Part 2

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day05-Breaks/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day05-Breaks/parseInput.ts
@@ -5,7 +5,7 @@ import { Almanac, SeedMap, SeedMapLine } from "./types.ts";
 
 export const parseInput = async (): Promise<Almanac> => {
   const almanacRaw: string[][] = await convertMultiParagraphFileToArray(
-    "./testInput.txt",
+    "./challengeInput.txt",
   );
   const almanac: Almanac = {
     seeds: almanacRaw.shift()![0].trim().split(`:`)[1].split(` `).map((
