09782efadd3c0be05f4ef587bde8b59cc7ba7d27
kaBeech <contact@kaBeech.com>
2023-12-05 21:45:23 -0800

    Parse seed maps

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day05-Breaks/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day05-Breaks/parseInput.ts
@@ -1,8 +1,7 @@
 import {
-  convertMultiLineFileToArray,
   convertMultiParagraphFileToArray,
 } from "../../tools/conversionFunctions/convertFileToArray.ts";
-import { Almanac } from "./types.ts";
+import { Almanac, SeedMap, SeedMapLine } from "./types.ts";
 
 export const parseInput = async (): Promise<Almanac> => {
   const almanacRaw: string[][] = await convertMultiParagraphFileToArray(
@@ -13,12 +12,22 @@ export const parseInput = async (): Promise<Almanac> => {
     seeds: almanacRaw.shift()![0].trim().split(`:`)[1].split(` `).map((
       seed,
     ) => parseInt(seed)),
-    seedMaps: almanacRaw,
+    seedMaps: [],
   };
+  almanacRaw.forEach((rawSeedMap) => {
+    const seedMap: SeedMap = [];
+    rawSeedMap.shift();
+    rawSeedMap.forEach((rawSeedMapLine) => {
+      rawSeedMapLine.trim().split(` `);
+      const seedMapLine: SeedMapLine = {
+        sourceRangeStart: +rawSeedMapLine[0],
+        destinationRangeStart: +rawSeedMapLine[1],
+        rangeLength: +rawSeedMapLine[2],
+      };
+      seedMap.push(seedMapLine);
+    });
+    almanac.seedMaps.push(seedMap);
+  });
   almanac.seeds = almanac.seeds.filter((seed) => !isNaN(seed));
-  console.log(almanac);
-  return {
-    seeds: almanacRaw[0].map((seed) => parseInt(seed)),
-    seedMaps: almanacRaw,
-  };
+  return almanac;
 };
