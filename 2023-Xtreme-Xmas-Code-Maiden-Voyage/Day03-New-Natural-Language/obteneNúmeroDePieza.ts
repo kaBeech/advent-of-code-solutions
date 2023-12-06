50d3ed786ddb5881906a78487d67bb5074debbb3
kaBeech <contact@kaBeech.com>
2023-12-05 16:07:35 -0800

    Modularize code

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/obteneNúmeroDePieza.ts
@@ -0,0 +1,28 @@
+import { Baldosa } from "./tipos.ts";
+
+export default function (baldosa: Baldosa, filaDeBaldosas: Baldosa[]): number {
+  let baldosaAnterior = filaDeBaldosas[baldosa.coordinadas.x - 1];
+  let baldosaSiguiente = filaDeBaldosas[baldosa.coordinadas.x + 1];
+  let númeroDePieza = `${baldosa.valor}`;
+  while (!isNaN(+baldosaAnterior.valor)) {
+    númeroDePieza = `${baldosaAnterior.valor}` +
+      númeroDePieza;
+    baldosaAnterior.agregadaALaSuma = true;
+    if (filaDeBaldosas[baldosaAnterior.coordinadas.x - 1]) {
+      baldosaAnterior = filaDeBaldosas[baldosaAnterior.coordinadas.x - 1];
+    } else {
+      baldosaAnterior.valor = ".";
+    }
+  }
+  while (!isNaN(+baldosaSiguiente.valor)) {
+    númeroDePieza = númeroDePieza +
+      `${baldosaSiguiente.valor}`;
+    baldosaSiguiente.agregadaALaSuma = true;
+    if (filaDeBaldosas[baldosaSiguiente.coordinadas.x + 1]) {
+      baldosaSiguiente = filaDeBaldosas[baldosaSiguiente.coordinadas.x + 1];
+    } else {
+      baldosaSiguiente.valor = ".";
+    }
+  }
+  return +númeroDePieza;
+}
--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/solucionaParte1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day03-New-Natural-Language/solucionaParte1.ts
@@ -1,37 +1,7 @@
 import { analizaEntrada } from "./analizaEntrada.ts";
 import comprobaBaldosasAdyacentes from "./comprobaBaldosasAdyacentes.ts";
-import { Baldosa, MapaDeBaldosas } from "./tipos.ts";
-
-const obteneNúmeroDePieza = (
-  baldosa: Baldosa,
-  filaDeBaldosas: Baldosa[],
-): number => {
-  let baldosaAnterior = filaDeBaldosas[baldosa.coordinadas.x - 1];
-  let baldosaSiguiente = filaDeBaldosas[baldosa.coordinadas.x + 1];
-  let númeroDePieza = `${baldosa.valor}`;
-  while (!isNaN(+baldosaAnterior.valor)) {
-    númeroDePieza = `${baldosaAnterior.valor}` +
-      númeroDePieza;
-    baldosaAnterior.agregadaALaSuma = true;
-    if (filaDeBaldosas[baldosaAnterior.coordinadas.x - 1]) {
-      baldosaAnterior = filaDeBaldosas[baldosaAnterior.coordinadas.x - 1];
-    } else {
-      baldosaAnterior.valor = ".";
-    }
-  }
-  while (!isNaN(+baldosaSiguiente.valor)) {
-    númeroDePieza = númeroDePieza +
-      `${baldosaSiguiente.valor}`;
-    baldosaSiguiente.agregadaALaSuma = true;
-    if (filaDeBaldosas[baldosaSiguiente.coordinadas.x + 1]) {
-      baldosaSiguiente = filaDeBaldosas[baldosaSiguiente.coordinadas.x + 1];
-    } else {
-      baldosaSiguiente.valor = ".";
-    }
-  }
-  console.log(`Part 2: La pieza es ${númeroDePieza}`);
-  return +númeroDePieza;
-};
+import { MapaDeBaldosas } from "./tipos.ts";
+import obteneNúmeroDePieza from "./obteneNúmeroDePieza.ts";
 
 export const solucionaParte1 = (async (): Promise<number> => {
   const mapaDeBaldosas: MapaDeBaldosas = await analizaEntrada();
