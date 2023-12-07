0e0919457bbb245392901ad07585c64b616003f3
kaBeech <contact@kaBeech.com>
2023-12-07 15:20:42 -0800

    Solve Part 1 with the program!

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day06-No-Named-Functions/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day06-No-Named-Functions/solvePart1.ts
@@ -1,22 +1,22 @@
 import { Race } from "./types.ts";
 
 export default (function (): number {
-  const testRaces: Race[] = [
-    { time: 7, distance: 9 },
-    { time: 15, distance: 40 },
-    { time: 30, distance: 200 },
-  ];
-
-  // const challengeRaces: Race[] = [
-  //   { time: 45, distance: 295 },
-  //   { time: 98, distance: 1734 },
-  //   { time: 83, distance: 1278 },
-  //   { time: 73, distance: 1210 },
+  // const testRaces: Race[] = [
+  //   { time: 7, distance: 9 },
+  //   { time: 15, distance: 40 },
+  //   { time: 30, distance: 200 },
   // ];
+
+  const challengeRaces: Race[] = [
+    { time: 45, distance: 295 },
+    { time: 98, distance: 1734 },
+    { time: 83, distance: 1278 },
+    { time: 73, distance: 1210 },
+  ];
 
   const numbersOfWaysToWin: number[] = [];
 
-  testRaces.forEach((race) => {
+  challengeRaces.forEach((race) => {
     const minimumTimeToNotWinTheRace =
       (-race.time + Math.sqrt(race.time ** 2 - -4 * -race.distance)) /
       -2;
