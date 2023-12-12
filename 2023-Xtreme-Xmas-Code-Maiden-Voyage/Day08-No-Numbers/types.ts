5941f725fc4f0dd3c9b5b2a2fbfc37609c61a37d
kaBeech <contact@kaBeech.com>
2023-12-11 23:05:14 -0800

    Update Instruction type

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/types.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/types.ts
@@ -2,6 +2,10 @@ export interface Instruction {
   id: string;
   l: string;
   r: string;
+  lastEndingNode?: Instruction;
+  distanceFromLastEndingNode?: number;
+  nextEndingNode?: Instruction;
+  distanceFromNextEndingNode?: number;
 }
 
 export interface Maps {
