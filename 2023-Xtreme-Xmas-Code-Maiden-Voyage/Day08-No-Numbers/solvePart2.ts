49b68a1ebb24495e1141dbd2300f7fe45c563f80
kaBeech <contact@kaBeech.com>
2023-12-12 02:19:07 -0800

    Add code tracking back in

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/solvePart2.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/solvePart2.ts
@@ -19,6 +19,7 @@ export default (async function (): Promise<string[]> {
   let endReached = false;
 
   while (!endReached) {
+    // console.log(`Current instructions: ${JSON.stringify(currentInstructions)}`);
     let nonEndInstructionFound = false;
     let currentDirection: string;
     if (reservoirInUse) {
@@ -44,6 +45,14 @@ export default (async function (): Promise<string[]> {
     }
     const newInstructions = [];
     for (let currentInstruction of currentInstructions) {
+      // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
+      // console.log(
+      //   `last char: ${getLastChar(currentInstruction.id)}`,
+      //   currentDirection,
+      // );
+      const lastEndingNode = currentInstruction.lastEndingNode;
+      let distanceFromLastEndingNode =
+        currentInstruction.distanceFromLastEndingNode;
       currentInstruction = followInstruction(
         maps.instructions,
         currentInstruction,
@@ -51,15 +60,38 @@ export default (async function (): Promise<string[]> {
       )!;
       if (getLastChar(currentInstruction.id) !== `Z`) {
         nonEndInstructionFound = true;
+        if (lastEndingNode) {
+          distanceFromLastEndingNode!++;
+          if (!currentInstruction.lastEndingNode) {
+            currentInstruction.lastEndingNode = lastEndingNode,
+              currentInstruction.distanceFromLastEndingNode =
+                distanceFromLastEndingNode;
+          }
+        }
+      } else {
+        if (
+          currentInstruction.lastEndingNode &&
+          !currentInstruction.lastEndingNode.nextEndingNode
+        ) {
+          currentInstruction.lastEndingNode.nextEndingNode = currentInstruction;
+          currentInstruction.lastEndingNode.distanceFromNextEndingNode =
+            currentInstruction.distanceFromLastEndingNode;
+        }
+        currentInstruction.lastEndingNode = currentInstruction;
+        currentInstruction.distanceFromLastEndingNode = 0;
       }
       newInstructions.push(currentInstruction);
+      // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
     }
     totalStepsArray.push(`Step!`);
     currentInstructions = newInstructions;
+    // console.log(`Current instructions: ${JSON.stringify(currentInstructions)}`);
     if (!nonEndInstructionFound) {
       endReached = true;
     }
+    // console.log(JSON.stringify(totalStepsArray.length));
   }
+  // console.log(`Current instructions: ${JSON.stringify(currentInstructions)}`);
 
   console.log(
     `Part 2: The number of steps it takes before all current nodes' ids end in "Z" is: ${
