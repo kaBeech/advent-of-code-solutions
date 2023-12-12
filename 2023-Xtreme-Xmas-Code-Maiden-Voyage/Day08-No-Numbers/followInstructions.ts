a3826a033412865d1bee40626531637472253158
kaBeech <contact@kaBeech.com>
2023-12-12 12:06:18 -0800

    Modularize

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/followInstructions.ts
@@ -0,0 +1,56 @@
+import followInstruction from "./followInstruction.ts";
+import getLastChar from "./getLastChar.ts";
+import { Instruction, Maps } from "./types.ts";
+
+export default (
+  currentDirection: string,
+  currentInstructions: Instruction[],
+  maps: Maps,
+  nonEndInstructionFound: boolean,
+) => {
+  const newInstructions: Instruction[] = [];
+
+  for (let currentInstruction of currentInstructions) {
+    // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
+    // console.log(
+    //   `last char: ${getLastChar(currentInstruction.id)}`,
+    //   currentDirection,
+    // );
+    const lastEndingNode = currentInstruction.lastEndingNode;
+    // deno-lint-ignore prefer-const
+    let distanceFromLastEndingNode =
+      currentInstruction.distanceFromLastEndingNode;
+    currentInstruction = followInstruction(
+      maps.instructions,
+      currentInstruction,
+      currentDirection,
+    )!;
+    if (getLastChar(currentInstruction.id) !== `Z`) {
+      if (!nonEndInstructionFound) {
+        nonEndInstructionFound = true;
+      }
+      if (lastEndingNode) {
+        distanceFromLastEndingNode!++;
+        if (!currentInstruction.lastEndingNode) {
+          currentInstruction.lastEndingNode = lastEndingNode,
+            currentInstruction.distanceFromLastEndingNode =
+              distanceFromLastEndingNode;
+        }
+      }
+    } else {
+      if (
+        currentInstruction.lastEndingNode &&
+        !currentInstruction.lastEndingNode.nextEndingNode
+      ) {
+        currentInstruction.lastEndingNode.nextEndingNode = currentInstruction;
+        currentInstruction.lastEndingNode.distanceFromNextEndingNode =
+          currentInstruction.distanceFromLastEndingNode;
+      }
+      currentInstruction.lastEndingNode = currentInstruction;
+      currentInstruction.distanceFromLastEndingNode = 0;
+    }
+    newInstructions.push(currentInstruction);
+    // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
+  }
+  return { newInstructions, nonEndInstructionFound };
+};
--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/solvePart2.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/solvePart2.ts
@@ -1,7 +1,7 @@
-import followInstruction from "./followInstruction.ts";
 import { parseInput } from "./parseInput.ts";
 import { Maps } from "./types.ts";
-import getLastChar from "./getLastChar.ts";
+import processDirectionData from "./processDirectionData.ts";
+import followInstructions from "./followInstructions.ts";
 
 export default (async function (): Promise<string[]> {
   const maps: Maps = await parseInput();
@@ -11,9 +11,9 @@ export default (async function (): Promise<string[]> {
     const idLastChar = idArray.pop();
     return idLastChar === `A`;
   });
-  const directions = maps.directions.split(``);
-  const directionsReservoir = [];
-  const totalStepsArray = [];
+  let directions = maps.directions.split(``);
+  let directionsReservoir: string[] = [];
+  const totalStepsArray: string[] = [];
   let currentInstructions = startingInstructions;
   let reservoirInUse = false;
   let endReached = false;
@@ -21,70 +21,30 @@ export default (async function (): Promise<string[]> {
   while (!endReached) {
     // console.log(`Current instructions: ${JSON.stringify(currentInstructions)}`);
     let nonEndInstructionFound = false;
-    let currentDirection: string;
-    if (reservoirInUse) {
-      // This checks whether directionsReservoir is empty without using any numbers
-      if (directionsReservoir == false) {
-        reservoirInUse = false;
-        currentDirection = directions.shift()!;
-        directionsReservoir.push(currentDirection);
-      } else {
-        currentDirection = directionsReservoir.shift()!;
-        directions.push(currentDirection);
-      }
-    } else {
-      // This checks whether directions is empty without using any numbers
-      if (directions == false) {
-        reservoirInUse = true;
-        currentDirection = directionsReservoir.shift()!;
-        directions.push(currentDirection);
-      } else {
-        currentDirection = directions.shift()!;
-        directionsReservoir.push(currentDirection);
-      }
-    }
-    const newInstructions = [];
-    for (let currentInstruction of currentInstructions) {
-      // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
-      // console.log(
-      //   `last char: ${getLastChar(currentInstruction.id)}`,
-      //   currentDirection,
-      // );
-      const lastEndingNode = currentInstruction.lastEndingNode;
-      let distanceFromLastEndingNode =
-        currentInstruction.distanceFromLastEndingNode;
-      currentInstruction = followInstruction(
-        maps.instructions,
-        currentInstruction,
-        currentDirection,
-      )!;
-      if (getLastChar(currentInstruction.id) !== `Z`) {
-        nonEndInstructionFound = true;
-        if (lastEndingNode) {
-          distanceFromLastEndingNode!++;
-          if (!currentInstruction.lastEndingNode) {
-            currentInstruction.lastEndingNode = lastEndingNode,
-              currentInstruction.distanceFromLastEndingNode =
-                distanceFromLastEndingNode;
-          }
-        }
-      } else {
-        if (
-          currentInstruction.lastEndingNode &&
-          !currentInstruction.lastEndingNode.nextEndingNode
-        ) {
-          currentInstruction.lastEndingNode.nextEndingNode = currentInstruction;
-          currentInstruction.lastEndingNode.distanceFromNextEndingNode =
-            currentInstruction.distanceFromLastEndingNode;
-        }
-        currentInstruction.lastEndingNode = currentInstruction;
-        currentInstruction.distanceFromLastEndingNode = 0;
-      }
-      newInstructions.push(currentInstruction);
-      // console.log(`Current instruction: ${JSON.stringify(currentInstruction)}`);
-    }
+
+    const directionData = processDirectionData(
+      reservoirInUse,
+      directions,
+      directionsReservoir,
+    );
+
+    // Evaluate whether these are necessary
+    reservoirInUse = directionData.reservoirInUse;
+    directions = directionData.directions;
+    directionsReservoir = directionData.directionsReservoir;
+    const currentDirection = directionData.currentDirection;
+
+    const instructionsResults = followInstructions(
+      currentDirection,
+      currentInstructions,
+      maps,
+      nonEndInstructionFound,
+    );
+
+    currentInstructions = instructionsResults.newInstructions;
+    nonEndInstructionFound = instructionsResults.nonEndInstructionFound;
     totalStepsArray.push(`Step!`);
-    currentInstructions = newInstructions;
+
     // console.log(`Current instructions: ${JSON.stringify(currentInstructions)}`);
     if (!nonEndInstructionFound) {
       endReached = true;
