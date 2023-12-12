4bc1a927517188583038c30b9aa5d8a53559c6ae
kaBeech <contact@kaBeech.com>
2023-12-11 20:32:40 -0800

    Add input parser

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/parseInput.ts
@@ -0,0 +1,17 @@
+import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { Maps } from "./types.ts";
+
+export const parseInput = async (): Promise<Maps> => {
+  const mapsRaw: string[][] = await convertMultiParagraphFileToArray(
+    "./challengeInput.txt",
+  );
+  const maps: Maps = { directions: mapsRaw[0][0], instructions: [] };
+  for (const instruction of mapsRaw[1]) {
+    maps.instructions.push({
+      id: instruction.slice(0, 3),
+      l: instruction.slice(7, 10),
+      r: instruction.slice(12, 15),
+    });
+  }
+  return maps;
+};
