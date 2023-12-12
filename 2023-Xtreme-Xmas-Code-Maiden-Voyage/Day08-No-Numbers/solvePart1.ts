5c876b464948d1d9a85e3a594ef23910d1999d9d
kaBeech <contact@kaBeech.com>
2023-12-11 20:52:05 -0800

    Write solvePart1()

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/solvePart1.ts
@@ -0,0 +1,38 @@
+import { parseInput } from "./parseInput.ts";
+import { Instruction, Maps } from "./types.ts";
+
+export default (async function (): Promise<string[]> {
+  const maps: Maps = await parseInput();
+  const start = maps.instructions.find((instruction: Instruction) =>
+    instruction.id === `AAA`
+  )!;
+
+  const directions = maps.directions.split(``);
+  const directionsReservoir = [];
+  const totalStepsArray = [];
+  let currentInstruction = start;
+
+  while (currentInstruction.id !== `ZZZ`) {
+    let currentDirection: string;
+    if (!directions) {
+      currentDirection = directionsReservoir.shift()!;
+      directions.push(currentDirection);
+    } else {
+      currentDirection = directions.shift()!;
+      directionsReservoir.push(currentDirection);
+    }
+    currentInstruction = followInstruction(
+      currentInstruction,
+      currentDirection,
+    )!;
+    totalStepsArray.push(`Step!`);
+  }
+
+  console.log(
+    `Part 1: The number of steps it takes to reach "ZZZ" is equal to the number of elements in this array: ${
+      JSON.stringify(totalStepsArray)
+    }`,
+  );
+
+  return totalStepsArray;
+})();
