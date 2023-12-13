1bf9aa2fd67280cdafa8d305213f70274df707bc
kaBeech <contact@kaBeech.com>
2023-12-13 03:04:13 -0800

    Add sequence extrapolator

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day09-Non-Latin-Based-Declarations/حل_الجزء_الأول.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day09-Non-Latin-Based-Declarations/حل_الجزء_الأول.ts
@@ -1,6 +1,7 @@
 import تحليل_المدخلات from "./تحليل_المدخلات.ts";
 import { تسلسلات_تاريخية, تقرير } from "./أنواع.ts";
 import بناء_تسلسلات from "./بناء_تسلسلات.ts";
+import استقراء_تسلسل from "./استقراء_تسلسل.ts";
 
 export default (async function (): Promise<تقرير> {
   const تقرير: تقرير = await تحليل_المدخلات();
@@ -11,8 +12,16 @@ export default (async function (): Promise<تقرير> {
     تقرير_التسلسلات_التاريخية.push(بناء_تسلسلات(تاريخ));
   }
 
+  for (let تسلسلات_تاريخية of تقرير_التسلسلات_التاريخية) {
+    تسلسلات_تاريخية = استقراء_تسلسل(تسلسلات_تاريخية);
+  }
+
+  console.log(تقرير_التسلسلات_التاريخية);
+
   console.log(
-    `Part 1: The sum of the extrapolated data is ${JSON.stringify(تقرير)}`,
+    `Part 1: The sum of the extrapolated data is ${
+      JSON.stringify(`ExtrapolatedSum`)
+    }`,
   );
 
   return تقرير;
