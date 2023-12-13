5105e8088fd372915d327decfbcf2b76b2b3ed7f
kaBeech <contact@kaBeech.com>
2023-12-13 14:16:01 -0800

    Find starting tile

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day10-New-Lang-Perl/solvepart1.pl
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day10-New-Lang-Perl/solvepart1.pl
@@ -5,17 +5,29 @@ use warnings;
 
 open(my $input,  "<",  "testInput.dat")  or die "Can't open testInput.dat: $!";
 
-my @lines = <$input>;
 my @values = ["|" , "-" , "7" , "F" , "J" , "L"];
+my @directions = ["^" , ">" , "v" , "<"];
+my @lines = <$input>;
+my %startingTile = (
+    coordinates => [0,0],
+    value => "S",
+);
 
 print("Hello, World!\n");
 
 print(@lines);
 
-my $line = @lines[0];
+# Find the starting tile
+my $i = 0;
+foreach (@lines) {
+    if (index($_, "S") != -1) {
+        $startingTile{"coordinates"} = [index($_, "S"), $i];
+    }
+    $i += 1;
+}
 
 print("\n");
 print("\n");
-my $testTile = substr($line, 0, 1);
+my $testTile = substr($lines[$startingTile{"coordinates"}[1]], $startingTile{"coordinates"}[0], 1);
 print($testTile);
 
