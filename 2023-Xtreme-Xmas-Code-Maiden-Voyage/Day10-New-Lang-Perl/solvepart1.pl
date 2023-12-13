4a3d8cd65d3455e3b6cd156b6cfd728609a84549
kaBeech <contact@kaBeech.com>
2023-12-13 06:44:05 -0800

    Verify input parsing

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day10-New-Lang-Perl/solvepart1.pl
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day10-New-Lang-Perl/solvepart1.pl
@@ -6,8 +6,15 @@ use warnings;
 open(my $input,  "<",  "testInput.dat")  or die "Can't open testInput.dat: $!";
 
 my @lines = <$input>;
-my @values = ["|" , "-" , "7" , "F" , "J" , "L"]
+my @values = ["|" , "-" , "7" , "F" , "J" , "L"];
 
-print("Hello World\n");
+print("Hello, World!\n");
 
-print(@lines)
+print(@lines);
+
+my $line = @lines[0];
+
+print("\n");
+print("\n");
+my $nth = substr($line, 0, 1);
+print($nth);
