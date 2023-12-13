6361398fd463a7c040db08b028d3a2fd7cccbcd5
kaBeech <contact@kaBeech.com>
2023-12-13 01:48:55 -0800

    Add input parser

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day09-Non-Latin-Based-Declarations/تحليل_المدخلات.ts
@@ -0,0 +1,20 @@
+import {
+  convertMultiLineFileToDoubleArray,
+} from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { تاريخ, تقرير } from "./أنواع.ts";
+
+export default async (): Promise<تقرير> => {
+  const تقرير_خام: string[][] = await convertMultiLineFileToDoubleArray(
+    "./testInput.txt",
+    ` `,
+  );
+  const تقرير: تقرير = [];
+  for (const تاريخ_خام of تقرير_خام) {
+    const تاريخ: تاريخ = [];
+    for (const رقم of تاريخ_خام) {
+      تاريخ.push(+رقم);
+    }
+    تقرير.push(تاريخ);
+  }
+  return تقرير;
+};
