974ce551abb8f2a3115350bf5b40527997e39e28
kaBeech <contact@kaBeech.com>
2023-12-12 18:41:00 -0800

    Remove numbers from ParseInput

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day08-No-Numbers/parseInput.ts
@@ -5,12 +5,23 @@ export const parseInput = async (): Promise<Maps> => {
   const mapsRaw: string[][] = await convertMultiParagraphFileToArray(
     "./challengeInput.dat",
   );
-  const maps: Maps = { directions: mapsRaw[0][0], instructions: [] };
-  for (const instruction of mapsRaw[1]) {
+  const maps: Maps = {
+    directions: mapsRaw.shift()!.shift()!,
+    instructions: [],
+  };
+  for (const instruction of mapsRaw.shift()!) {
+    console.log(instruction);
+    const rawIdAndDirections = instruction.split(`=`);
+    const id = rawIdAndDirections.shift()!.trim();
+    const rawDirections = rawIdAndDirections.shift()!.trim().split(`,`);
+    const l = rawDirections.shift()!.trim().split(``);
+    l.shift();
+    const r: string[] = rawDirections.shift()!.trim().split(``);
+    r.pop();
     maps.instructions.push({
-      id: instruction.slice(0, 3),
-      l: instruction.slice(7, 10),
-      r: instruction.slice(12, 15),
+      id,
+      l: l.join(``),
+      r: r.join(``),
     });
   }
   return maps;
