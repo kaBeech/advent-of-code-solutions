fe1f9ba0dc21ec2deeb8eccb2f2b16619e549a52
kaBeech <contact@kaBeech.com>
2023-12-13 21:20:33 -0800

    Expand Map

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day11-New-Codebase-Lang-Ruby/solvePart1.rb
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day11-New-Codebase-Lang-Ruby/solvePart1.rb
@@ -1,14 +1,28 @@
 $map = Array.new
 
-File.foreach('challengeInput.dat') { 
-  |row|
+File.foreach('testInput.dat') { |row|
   puts row
   
   $processedRow = Array.new
   row.each_char { |c|
-    $processedRow.push(c)
+    if c == '#' or c == '.'
+      $processedRow.push(c)
+    end
   }
   $map.push($processedRow)
+}
+
+$i = 0
+$expandedMap = Array.new
+$map.each { |row|
+  $expandedMap.push(row)
+  if row.all? { |c| c == '.'}
+    $expandedMap.push(row)
+  end
+}
+
+$expandedMap.each { |row|
+  puts row.join
 }
 
 # puts $map[1][7]
