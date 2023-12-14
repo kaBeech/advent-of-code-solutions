a36f089bd1caf13f275a78194f6960cfc0d3726a
kaBeech <contact@kaBeech.com>
2023-12-13 22:20:59 -0800

    Demonstrate proof of concept with optimized approach

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day11-New-Codebase-Lang-Ruby/solvePart2.rb
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day11-New-Codebase-Lang-Ruby/solvePart2.rb
@@ -12,55 +12,49 @@ File.foreach('testInput.dat') { |row|
   $map.push($processedRow)
 }
 
-$verticallyExpandedMap = Array.new
+puts "Input parsed."
+
+$emptyRows = Array.new
+$i = 0
 $map.each { |row|
-  $verticallyExpandedMap.push(row)
   if row.all? { |c| c == '.'}
-    9.times do
-      $verticallyExpandedMap.push(row)
-    end
+      $emptyRows.push($i)
   end
+  $i += 1
 }
 
-$flippedVerticallyExpandedMap = Array.new
+puts "Empty Rows found."
+
+$flippedMap = Array.new
 $i = 0
-while $i < $verticallyExpandedMap[0].length do
+while $i < $map[0].length do
   $flippedColumn = Array.new
-  $verticallyExpandedMap.each { |row|
+  $map.each { |row|
     $flippedColumn.push(row[$i])
   }
-  $flippedVerticallyExpandedMap.push($flippedColumn)
+  $flippedMap.push($flippedColumn)
   $i += 1
 end
 
-$flippedFullyExpandedMap = Array.new
-$flippedVerticallyExpandedMap.each { |row|
-  $flippedFullyExpandedMap.push(row)
-  if row.all? { |c| c == '.'}
-    9.times do
-      $flippedFullyExpandedMap.push(row)
-    end
-  end
-}
-
-$fullyExpandedMap = Array.new
-$i = 0
-while $i < $flippedFullyExpandedMap[0].length do
-  $row = Array.new
-  $flippedFullyExpandedMap.each { |flippedColumn|
-    $row.push(flippedColumn[$i])
-  }
-  $fullyExpandedMap.push($row)
-  $i += 1
-end
-
-$fullyExpandedMap.each { |row|
+puts "Map flipped once."
+$flippedMap.each { |row|
   puts row.join
 }
 
+$emptyColumns = Array.new
+$i = 0
+$flippedMap.each { |row|
+  if row.all? { |c| c == '.'}
+    $emptyColumns.push($i)
+  end
+  $i += 1
+}
+
+puts "Empty Columns found."
+
 $galaxies = Array.new
 $y = 0
-$fullyExpandedMap.each { |row|
+$map.each { |row|
   $x = 0
   row.each { |c|
     if c == '#'
@@ -71,14 +65,34 @@ $fullyExpandedMap.each { |row|
   $y += 1
 }
 
+puts "Galaxies found."
+
 $totalDistanceBetweenAllGalaxies = 0
 $unprocessedGalaxies = $galaxies.dup
 $galaxies.each { |galaxy|
   $unprocessedGalaxies.delete(galaxy)
   $unprocessedGalaxies.each { |unprocessedGalaxy|
-    $distanceBetweenGalaxies = (galaxy[0] - unprocessedGalaxy[0]).abs + (galaxy[1] - unprocessedGalaxy[1]).abs
+    $yDistanceBetweenGalaxies = (galaxy[0] - unprocessedGalaxy[0]).abs
+    $xDistanceBetweenGalaxies = (galaxy[1] - unprocessedGalaxy[1]).abs
+    $emptyRows.each { |emptyRow|
+      if galaxy[1] < emptyRow and unprocessedGalaxy[1] > emptyRow
+        $yDistanceBetweenGalaxies += 99
+      end
+      if galaxy[1] > emptyRow and unprocessedGalaxy[1] < emptyRow
+        $yDistanceBetweenGalaxies += 99
+      end
+    }
+    $emptyColumns.each { |emptyColumn|
+      if galaxy[0] < emptyColumn and unprocessedGalaxy[0] > emptyColumn
+        $xDistanceBetweenGalaxies += 99
+      end
+      if galaxy[0] > emptyColumn and unprocessedGalaxy[0] < emptyColumn
+        $xDistanceBetweenGalaxies += 99
+      end
+    }
+    $distanceBetweenGalaxies = $yDistanceBetweenGalaxies + $xDistanceBetweenGalaxies
     $totalDistanceBetweenAllGalaxies += $distanceBetweenGalaxies
   }
 }
 
-puts "Part 1: The total distance between all galaxies is: " + $totalDistanceBetweenAllGalaxies.to_s
+puts "Part 2: The total distance between all galaxies is: " + $totalDistanceBetweenAllGalaxies.to_s
