33e8b0454034fa7c12683abd618beb7dd7146a78
kaBeech <contact@kaBeech.com>
2023-12-25 20:41:05 -0800

    Add round 2 skeleton to incorporate finding smudges

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day13-Theme-Sports/scoreRebound.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day13-Theme-Sports/scoreRebound.ts
@@ -1,13 +1,20 @@
 import gainPossession from "./gainPossession.ts";
 import rebound from "./rebound.ts";
+import reboundInRound2 from "./reboundInRound2.ts";
 
-export default (fieldSetup: string[]): number => {
-  const defensiveReboundPosition = rebound(fieldSetup);
+export default (fieldSetup: string[], round2?: boolean): number => {
+  let defensiveReboundPosition = 0;
+  if (round2) {
+    defensiveReboundPosition = reboundInRound2(fieldSetup);
+  } else defensiveReboundPosition = rebound(fieldSetup);
   if (defensiveReboundPosition !== 0) {
     return 100 * defensiveReboundPosition;
   } else {
     const offensiveFieldSetup = gainPossession(fieldSetup);
-    const offensiveReboundPosition = rebound(offensiveFieldSetup);
+    let offensiveReboundPosition = 0;
+    if (round2) {
+      offensiveReboundPosition = reboundInRound2(offensiveFieldSetup);
+    } else offensiveReboundPosition = rebound(offensiveFieldSetup);
     if (offensiveReboundPosition === 0) {
       throw new Error(`No rebound positions found.`);
     }
