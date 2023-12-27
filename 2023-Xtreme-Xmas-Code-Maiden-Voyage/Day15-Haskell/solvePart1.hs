e7b76132a55bf05cce13166972958f2533b67eae
kaBeech <contact@kaBeech.com>
2023-12-26 23:06:00 -0800

    Reorganize

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/solvePart1.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/solvePart1.hs
@@ -2,16 +2,16 @@ exampleHASHString = "HASH"
 
 testInput = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
-runHASHAlgorithmOnChar currentValue nextCharacter = (currentValue + fromEnum nextCharacter) * 17 `mod` 256
+solvePart1 hashedInput = sum (getHASHedValues hashedInput)
+
+getHASHedValues puzzleInput = map runHASHAlgorithm (splitStringOn (== ',') puzzleInput)
 
 runHASHAlgorithm hashString = foldl runHASHAlgorithmOnChar 0 hashString
+
+runHASHAlgorithmOnChar currentValue nextCharacter = (currentValue + fromEnum nextCharacter) * 17 `mod` 256
 
 splitStringOn delimiter string = case dropWhile delimiter string of
   "" -> []
   string' -> element : splitStringOn delimiter string''
     where
       (element, string'') = break delimiter string'
-
-getHASHedValues puzzleInput = map runHASHAlgorithm (splitStringOn (== ',') puzzleInput)
-
-solvePart1 hashedInput = sum (getHASHedValues hashedInput)
