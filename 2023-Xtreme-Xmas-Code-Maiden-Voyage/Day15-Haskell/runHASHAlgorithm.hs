67435662130e428cd22a23b492fa99ae27655bec
kaBeech <contact@kaBeech.com>
2023-12-26 22:10:20 -0800

    Cleanup

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
@@ -1,12 +1,5 @@
--- getASCIICode = fromEnum
-
--- multiplyBy17 x = x * 17
-
--- modulo256 x = x `mod` 256
-
 runHASHAlgorithmOnChar currentValue nextCharacter = (currentValue + fromEnum nextCharacter) * 17 `mod` 256
 
--- exampleHASH = [72, 65, 83, 72]
-exampleHASH = "rn=1"
+exampleHASHString = "HASH"
 
-testHASHAlgorithm startingValue = foldl runHASHAlgorithmOnChar 0 exampleHASH
+testHASHAlgorithm startingValue = foldl runHASHAlgorithmOnChar 0 exampleHASHString
