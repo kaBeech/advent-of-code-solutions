ff187826d9a6a7a75bae4edd48f303e4d58337c8
kaBeech <contact@kaBeech.com>
2023-12-26 22:04:11 -0800

    Convert chars to ASCII code

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
@@ -1,9 +1,12 @@
+getASCIICode = fromEnum
+
 multiplyBy17 x = x * 17
 
 modulo256 x = x `mod` 256
 
-runHASHAlgorithmOnChar currentValue nextCharacter = modulo256 (multiplyBy17 (currentValue + nextCharacter))
+runHASHAlgorithmOnChar currentValue nextCharacter = modulo256 (multiplyBy17 (currentValue + getASCIICode nextCharacter))
 
-exampleHASH = [72, 65, 83, 72]
+-- exampleHASH = [72, 65, 83, 72]
+exampleHASH = ['r', 'n', '=', '1']
 
 testHASHAlgorithm startingValue = foldl runHASHAlgorithmOnChar 0 exampleHASH
