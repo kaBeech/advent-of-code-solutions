4adbb7cd7d50bf63bf4e81219e2833d441289674
kaBeech <contact@kaBeech.com>
2023-12-26 21:50:20 -0800

    Add algorithm test function

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
@@ -2,4 +2,8 @@ multiplyBy17 x = x * 17
 
 modulo256 x = x `mod` 256
 
-runHASHAlgorithm currentValue nextCharacter = modulo256 (multiplyBy17 (currentValue + nextCharacter))
+runHASHAlgorithmOnChar currentValue nextCharacter = modulo256 (multiplyBy17 (currentValue + nextCharacter))
+
+exampleHASH = [72, 65, 83, 72]
+
+testHASHAlgorithm startingValue = foldl runHASHAlgorithmOnChar 0 exampleHASH
