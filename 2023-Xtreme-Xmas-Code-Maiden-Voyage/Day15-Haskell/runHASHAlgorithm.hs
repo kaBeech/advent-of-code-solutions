9ec94ea06096c1b3c64f256ef468ae48e5323173
kaBeech <contact@kaBeech.com>
2023-12-26 22:28:36 -0800

    Add testInput

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/runHASHAlgorithm.hs
@@ -9,3 +9,7 @@ splitStringOn delimiter string = case dropWhile delimiter string of
   string' -> element : splitStringOn delimiter string''
     where
       (element, string'') = break delimiter string'
+
+testInput = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+testArray = splitStringOn (== ',') testInput
