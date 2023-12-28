fdd31e8ddbcdaff0ceed329251a8dc19edc23bd3
kaBeech <contact@kaBeech.com>
2023-12-27 16:13:45 -0800

    Add countEmptyBoxSections (scratch)

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/test.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/test.ts
@@ -10,6 +10,10 @@ const countUnplacedItems = (items: Item[]) => {
   return items.filter((item) => item.placement_status === "unplaced").length;
 };
 
+const countEmptyBoxSections = (box: BoxSection[]) => {
+  return box.filter((boxSection) => boxSection.contains === "empty").length;
+};
+
 const getPossibleArrangements = (
   record: BoxAndItemsRecord,
   boxIndex: number,
@@ -18,5 +22,7 @@ const getPossibleArrangements = (
   if (
     countUnplacedItems(record.items) === 0
   ) {
+    if (countEmptyBoxSections(record.box) === 0) {
+    }
   }
 };
