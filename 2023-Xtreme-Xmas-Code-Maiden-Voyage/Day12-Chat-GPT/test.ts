09d2cdf6e6f397d9efbe21fca1d0445c02813183
kaBeech <contact@kaBeech.com>
2023-12-27 16:10:51 -0800

    Add scratch cache

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/test.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/test.ts
@@ -1,7 +1,14 @@
 import { BoxAndItemsRecord } from "./solvePart2.ts";
 
+interface PossibleArrangementsRecord {
+  boxIndex: number;
+  numberOfUnplacedItems: number;
+  possibleArrangements: number;
+}
+
 const getPossibleArrangements = (
   record: BoxAndItemsRecord,
   boxIndex: number,
 ) => {
+  const cache: PossibleArrangementsRecord[] = [];
 };
