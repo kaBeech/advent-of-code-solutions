3f00c4f8bd6998ab76f1c2ddd72e63c0d7567c51
kaBeech <contact@kaBeech.com>
2023-12-28 18:48:55 -0800

    Update editing copy

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/solvePart2Edited.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day12-Chat-GPT/solvePart2Edited.py
@@ -5,7 +5,6 @@ class PossibleArrangementRecord:
         self.possibleArrangements = possibleArrangements
 
 def getNumberOfPossibleArrangements(box, items, boxIndex, cache=[]):
-
     if not items:
         return 1
 
@@ -33,15 +32,14 @@ def getNumberOfPossibleArrangements(box, items, boxIndex, cache=[]):
     return possibleArrangements
 
 def checkIfItemCanFit(box, itemLength, boxIndex):
-    # 
     if boxIndex + itemLength > len(box):
         return False
-# 
+
     for i in range(boxIndex, boxIndex + itemLength):
         if box[i] == '.':
             return False
-        
-    if len(box) > boxIndex + itemLength and box[boxIndex + itemLength] == '#':
+
+    if boxIndex + itemLength < len(box) and box[boxIndex + itemLength] == '#':
         return False
 
     return True
@@ -51,6 +49,29 @@ def placeItem(box, items, boxIndex, cache):
     boxIndex += 1 + items_copy[0]
     items_copy.pop(0)
     return getNumberOfPossibleArrangements(box, items_copy, boxIndex, cache)
+
+def parseInputString(input_string):
+    box, items_part = input_string.split()
+    box = box.strip()
+    items = [int(item) for item in items_part.split(',')]
+    return box, items
+
+def main():
+    totalPossibleArrangements = 0
+
+    with open("testInput.dat", "r") as file:
+        input_strings = file.readlines()
+
+    for input_string in input_strings:
+        box, items = parseInputString(input_string)
+        arrangements = getNumberOfPossibleArrangements(box, items, 0, [])
+        totalPossibleArrangements += arrangements
+
+    return totalPossibleArrangements
+
+result = main()
+print("Total Possible Arrangements:", result)
+
 
 print("Hello, World!!")
 
