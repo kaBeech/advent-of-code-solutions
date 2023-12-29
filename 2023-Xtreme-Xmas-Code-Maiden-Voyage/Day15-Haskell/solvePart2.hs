5f425c76fcfacb8de0d2acb8074d957bdc2b6415
kaBeech <contact@kaBeech.com>
2023-12-28 22:10:08 -0800

    Reorganize

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/solvePart2.hs
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day15-Haskell/solvePart2.hs
@@ -26,11 +26,11 @@ getLabel step = head (splitStringOn (== '=') (filter (/= '-') step))
 
 processLens box lens = if checkIfBoxHasALensWithThisLabel box lens then replaceLensInBox box lens else addLensToBox box lens
 
+checkIfBoxHasALensWithThisLabel box lens = any (\x -> getLabel x == getLabel lens) box
+
 replaceLensInBox box lens = map (\x -> if getLabel lens == getLabel x then lens else x) box
 
 addLensToBox box lens = box ++ [lens]
-
-checkIfBoxHasALensWithThisLabel box lens = any (\x -> getLabel x == getLabel lens) box
 
 -- HASH Algorithm --
 
