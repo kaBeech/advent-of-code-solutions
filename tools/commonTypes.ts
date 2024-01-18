ebf27e905884263e501ff3302b38548c04e672fa
kaBeech <contact@kaBeech.com>
2024-01-17 23:45:24 -0800

    Make code easier to change

--- tools/commonTypes.ts
+++ tools/commonTypes.ts
@@ -1,10 +1,10 @@
-type ModalBoolean = true | false | null;
+export type ModalBoolean = true | false | null;
 
-type Operator = `+` | `-` | `*` | `/` | `%`;
+export type Operator = `+` | `-` | `*` | `/` | `%`;
 
-type SingleDigitInteger = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type SingleDigitInteger = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-type SingleDigitIntegerString =
+export type SingleDigitIntegerString =
   | `0`
   | `1`
   | `2`
@@ -16,25 +16,14 @@ type SingleDigitIntegerString =
   | `8`
   | `9`;
 
-type TwoDigitIntegerString =
+export type TwoDigitIntegerString =
   `${SingleDigitIntegerString}${SingleDigitIntegerString}`;
 
-type XYCoordinateArray = [number, number];
+export type XYCoordinateArray = [number, number];
 
-interface XYCoordinates {
+export interface XYCoordinates {
   x: number;
   y: number;
 }
 
-type CardinalDirection = `North` | `East` | `South` | `West`;
-
-export type {
-  CardinalDirection,
-  ModalBoolean,
-  Operator,
-  SingleDigitInteger,
-  SingleDigitIntegerString,
-  TwoDigitIntegerString,
-  XYCoordinateArray,
-  XYCoordinates,
-};
+export type CardinalDirection = `North` | `East` | `South` | `West`;
