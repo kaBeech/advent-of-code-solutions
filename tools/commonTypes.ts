5959a5b5efc697bbff13c61924e33c5a4dd0231a
kaBeech <contact@kaBeech.com>
2024-01-17 23:44:50 -0800

    Use string literals

--- tools/commonTypes.ts
+++ tools/commonTypes.ts
@@ -1,20 +1,20 @@
 type ModalBoolean = true | false | null;
 
-type Operator = "+" | "-" | "*" | "/" | "%";
+type Operator = `+` | `-` | `*` | `/` | `%`;
 
 type SingleDigitInteger = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 type SingleDigitIntegerString =
-  | "0"
-  | "1"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9";
+  | `0`
+  | `1`
+  | `2`
+  | `3`
+  | `4`
+  | `5`
+  | `6`
+  | `7`
+  | `8`
+  | `9`;
 
 type TwoDigitIntegerString =
   `${SingleDigitIntegerString}${SingleDigitIntegerString}`;
