6ee51040fd72edd419097e6c246076a823de71c9
kaBeech <contact@kaBeech.com>
2024-01-17 23:30:50 -0800

    Scaffold solvePart1

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/solvePart1.ts
@@ -0,0 +1,16 @@
+import { parseInput } from "./parseInput.ts";
+import processBeam from "./processBeam.ts";
+import { Grid } from "./types.ts";
+
+export default (async function (): Promise<number> {
+  const contraption: Grid = await parseInput();
+
+  processBeam(contraption, { x: 0, y: 0 }, "right");
+
+  const totalEnergizedTiles =
+    contraption.flat().filter((tile) => tile.energized).length;
+
+  console.log(`Part 1: ${totalEnergizedTiles} tiles are energized.`);
+
+  return totalEnergizedTiles;
+})();
