98ba2ec07148eff997c56975710a7b5a98f0c5cc
kaBeech <contact@kaBeech.com>
2024-01-17 23:45:55 -0800

    Use CardinalDirection

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/solvePart1.ts
@@ -5,7 +5,7 @@ import { Grid } from "./types.ts";
 export default (async function (): Promise<number> {
   const contraption: Grid = await parseInput();
 
-  processBeam(contraption, { x: 0, y: 0 }, "right");
+  processBeam(contraption, { x: 0, y: 0 }, "East");
 
   const totalEnergizedTiles =
     contraption.flat().filter((tile) => tile.energized).length;
