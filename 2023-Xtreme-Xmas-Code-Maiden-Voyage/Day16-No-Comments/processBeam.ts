dfad25f142a093a54d0752ceac1cdcefea867e92
kaBeech <contact@kaBeech.com>
2024-01-18 00:53:13 -0800

    Clarify function naming

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/processBeam.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/processBeam.ts
@@ -1,7 +1,7 @@
 import { CardinalDirection, XYCoordinates } from "../../tools/commonTypes.ts";
 import getAdjacentCoordinates from "./getAdjacentCoordinates.ts";
-import processBeamHittingMirrorBackslash from "./processBeamHittingMirrorBackslash.ts";
-import processBeamHittingMirrorSlash from "./processBeamHittingMirrorSlash.ts";
+import processBeamHittingBackslashMirror from "./processBeamHittingBackslashMirror.ts";
+import processBeamHittingSlashMirror from "./processBeamHittingSlashMirror.ts";
 import processBeamHittingVerticalSplitter from "./processBeamHittingVerticalSplitter.ts";
 import processBeamIfWithinRange from "./processBeamIfWithinRange.ts";
 import processBeamInEmptySpace from "./processBeamInEmptySpace.ts";
@@ -27,14 +27,14 @@ export default (
       );
       break;
     case `mirror slash`:
-      processBeamHittingMirrorSlash(
+      processBeamHittingSlashMirror(
         grid,
         currentTileCoordinates,
         beamIsTravelingToThe,
       );
       break;
     case `mirror backslash`:
-      processBeamHittingMirrorBackslash(
+      processBeamHittingBackslashMirror(
         grid,
         currentTileCoordinates,
         beamIsTravelingToThe,
