c8bd3318f326dfb0c88f30131b0e644ca406635d
kaBeech <contact@kaBeech.com>
2024-01-18 02:47:01 -0800

    Modularize

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/solvePart1.ts
@@ -1,12 +1,9 @@
 import getNumberOfEnergizedTiles from "./getNumberOfEnergizedTiles.ts";
 import { parseInput } from "./parseInput.ts";
-import processBeam from "./processBeam.ts";
 import { Grid } from "./types.ts";
 
 export default (async function (): Promise<number> {
   const contraption: Grid = await parseInput();
-
-  processBeam(contraption, { x: 0, y: 0 }, "East");
 
   const totalNumberOfEnergizedTiles = getNumberOfEnergizedTiles(
     contraption,
