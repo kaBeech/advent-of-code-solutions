0aed170cf65303e1dca1a7d48c5af556b76d02c1
kaBeech <contact@kaBeech.com>
2024-01-17 22:36:22 -0800

    Add types

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/types.ts
@@ -0,0 +1,15 @@
+import { XYCoordinates } from "../../tools/commonTypes.ts";
+
+export interface Tile {
+  id: number;
+  coordinates: XYCoordinates;
+  contains:
+    | `empty space`
+    | `mirror slash`
+    | `mirror backslash`
+    | `splitter vertical`
+    | `splitter horizontal`;
+  energized: boolean;
+}
+
+export type Grid = Tile[];
