cc45b1aace2b928f6b41a4801de429b64e213420
kaBeech <contact@kaBeech.com>
2024-01-17 23:16:33 -0800

    Add input parsing

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day16-No-Comments/parseInput.ts
@@ -0,0 +1,46 @@
+import { convertMultiLineFileToDoubleArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
+import { Grid, Tile } from "./types.ts";
+
+export const parseInput = async (): Promise<Grid> => {
+  const contraption: Grid = [];
+  const inputAsDoubleArray: string[][] =
+    await convertMultiLineFileToDoubleArray(
+      "./testInput.dat",
+    );
+  let yCoordinate = 0;
+  for (const rawRow of inputAsDoubleArray) {
+    let xCoordinate = 0;
+    const row: Tile[] = [];
+    for (const rawTile of rawRow) {
+      let tileContains: Tile["contains"];
+      switch (rawTile) {
+        case `.`:
+          tileContains = "empty space";
+          break;
+        case `/`:
+          tileContains = "mirror slash";
+          break;
+        case `\\`:
+          tileContains = "mirror backslash";
+          break;
+        case `|`:
+          tileContains = "splitter vertical";
+          break;
+        case `-`:
+          tileContains = "splitter horizontal";
+          break;
+        default:
+          throw new Error(`Unknown tile type: ${rawTile}`);
+      }
+      row.push({
+        coordinates: { x: xCoordinate, y: yCoordinate },
+        contains: tileContains,
+        energized: false,
+      });
+      xCoordinate += 1;
+    }
+    contraption.push(row);
+    yCoordinate += 1;
+  }
+  return contraption;
+};
