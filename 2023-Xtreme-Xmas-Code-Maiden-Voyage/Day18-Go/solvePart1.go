e7513133105703c198a285b7014a89c751084232
kaBeech <contact@kaBeech.com>
2024-01-23 15:51:16 -0800

    Initialize path

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day18-Go/solvePart1.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day18-Go/solvePart1.go
@@ -6,6 +6,14 @@ func main() {
     fmt.Println("Part 1: The lagoon can hold", solvePart1(), "cubic meters of lava.")
 }
 
+type XYCoordinates struct {
+	x int
+	y int
+}
+
 func solvePart1() int {
+	var path []XYCoordinates = []XYCoordinates{
+		XYCoordinates{0, 0},
+	}
 	return 0
 }
