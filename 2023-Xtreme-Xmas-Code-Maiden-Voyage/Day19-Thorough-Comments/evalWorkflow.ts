456dad389e0f1d716282de4f3adf91014695bebf
kaBeech <contact@kaBeech.com>
2024-01-24 01:12:13 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalWorkflow.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalWorkflow.ts
@@ -7,10 +7,13 @@ export default (
   workflow: Workflow,
   workflows: Workflow[],
 ): boolean => {
+  // Evaluate the part against each rule in the workflow.
   for (const rule of workflow.rules) {
+    // If the part passes the rule's qualification, process the destination and don't evaluate any more rules.
     if (evalRule(part, rule)) {
       return processDestination(part, workflows, rule.destination);
     }
   }
+  // If the part didn't pass any rules, process the end destination.
   return processDestination(part, workflows, workflow.endDestination);
 };
