40c74b8e08e6b797f946e76f3b1cd771408087f4
kaBeech <contact@kaBeech.com>
2024-01-24 12:13:15 -0800

    Save old work (not currently used) just in case

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalPart.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalPart.ts
@@ -1,17 +1,24 @@
-import evalRuleByCategory from "./evalRuleByCategory.ts";
+import calculateStepValue from "./calculateStepValue.ts";
+import evalRule from "./evalRule.ts";
 import evalWorkflow from "./evalWorkflow.ts";
 import processDestination from "./processDestination.ts";
-import { Part, Rule, Workflow } from "./types.ts";
+import { EvaluationResult, Part, Rule, Workflow } from "./types.ts";
 
 export default (
   part: Part,
   workflow: Workflow,
   workflows: Workflow[],
   rule: Rule,
-): boolean => {
+): EvaluationResult => {
   // If the part passes the rule's qualification, process the destination.
-  if (evalRuleByCategory(part, rule)) {
-    return processDestination(part, workflows, rule.destination);
+  if (evalRule(part, rule)) {
+    return processDestination(
+      part,
+      workflows,
+      rule.category,
+      calculateStepValue(part, rule.category, rule.comparison, rule.value),
+      rule.destination,
+    );
   }
   // Otherwise, recursively evaluate the next rule in the workflow.
   return evalWorkflow(part, workflow, workflows);
