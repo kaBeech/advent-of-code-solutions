1928a2359efc1d141e1a7c5f55c00ab1be5504df
kaBeech <contact@kaBeech.com>
2024-01-24 02:57:23 -0800

    Return an EvaluationResult

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processDestination.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processDestination.ts
@@ -1,18 +1,20 @@
 import evalWorkflow from "./evalWorkflow.ts";
-import { Part, Workflow } from "./types.ts";
+import { Category, EvaluationResult, Part, Workflow } from "./types.ts";
 
 export default (
   part: Part,
   workflows: Workflow[],
+  category: Category,
+  value: number,
   destination: string,
-): boolean => {
+): EvaluationResult => {
   switch (destination) {
     case `A`:
       // If the destination is Accepted, return true so that the part is added to acceptedParts.
-      return true;
+      return { category, value, passes: true };
     case `R`:
       // If the destination is Rejected, return false so that processing is halted.
-      return false;
+      return { category, value, passes: false };
     default:
       // Otherwise, recursively evaluate the workflow that the destination refers to.
       return evalWorkflow(
