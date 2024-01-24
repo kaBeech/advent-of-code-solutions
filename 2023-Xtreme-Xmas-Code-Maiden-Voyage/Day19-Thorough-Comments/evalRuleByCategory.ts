4e5fd637d77accdc186d80671b485f1e4c7b4b2c
kaBeech <contact@kaBeech.com>
2024-01-24 01:11:03 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalRuleByCategory.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/evalRuleByCategory.ts
@@ -1,6 +1,7 @@
 import { Rule } from "./types.ts";
 
 export default (partValue: number, rule: Rule): boolean => {
+  // Evaluate whether the part passes the rule based on the rule's comparison value.
   if (rule.comparison === `>`) {
     return partValue > rule.value;
   } else {
