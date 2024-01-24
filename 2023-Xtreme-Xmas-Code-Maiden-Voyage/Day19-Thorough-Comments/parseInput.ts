2f42cd2bc89e0518865165827f9e09f312f11b2c
kaBeech <contact@kaBeech.com>
2024-01-24 01:21:31 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
@@ -9,24 +9,30 @@ export default async (): Promise<ParsedInput> => {
   const workflows: Workflow[] = [];
   const parts: Part[] = [];
 
+  // Parse the rawWorkflows into Workflow objects.
   for (const rawWorkflow of rawWorkflows) {
+    // Get the workflow name from the rawWorkflow string.
     let splitWorkflow = rawWorkflow.split("{");
-    const workflowName = rawWorkflow.split("{")[0];
+    const workflowName = splitWorkflow[0];
+    // Remove the `}` from the end of the rawWorkflow string and split the string into an array of rules.
     splitWorkflow = splitWorkflow[1].slice(0, -1).split(",");
+    // Remove the end destination from the rawWorkflow string and store it separately.
     const endDestination = splitWorkflow.pop()!;
     const rules: Rule[] = [];
     for (let rawRule of splitWorkflow) {
+      // Remove the destination from the rawRule string and store it separately.
       const rawRuleSplit = rawRule.split(":");
       const destination = rawRuleSplit[1];
       rawRule = rawRuleSplit[0];
-      const rule: Rule = {
+      // Add the resulting rule to the rules array.
+      rules.push({
         category: rawRule[0] as Category,
         comparison: rawRule[1] as `>` | `<`,
         value: +rawRule.slice(2),
         destination,
-      };
-      rules.push(rule);
+      });
     }
+    // Add the resulting workflow to the workflows array.
     workflows.push({
       name: workflowName,
       rules,
@@ -34,7 +40,9 @@ export default async (): Promise<ParsedInput> => {
     });
   }
 
+  // Parse the rawParts into Part objects.
   for (let rawPart of rawParts) {
+    // convert the raw input into a string that can be parsed with JSON.parse()
     rawPart = rawPart.replace(/=/g, `: "`).replace(/,/g, `", `).replace(
       /{/g,
       `{ `,
@@ -42,6 +50,7 @@ export default async (): Promise<ParsedInput> => {
       /s/g,
       `"s"`,
     ).replace(/}/g, `" }`);
+    // Use JSON to parse the resulting rawPart string into a Part object.
     parts.push(JSON.parse(rawPart));
   }
 
