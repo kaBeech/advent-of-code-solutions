fc14f4c995b0dece5d1969f54a72cefc16c31533
kaBeech <contact@kaBeech.com>
2024-01-23 22:22:52 -0800

    Split input into component parts

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
@@ -1,8 +1,11 @@
 import { convertMultiParagraphFileToArray } from "../../tools/conversionFunctions/convertFileToArray.ts";
-import { ParsedInput } from "./types.ts";
+import { ParsedInput, Part } from "./types.ts";
 
 export default async (): Promise<ParsedInput> => {
   const input = await convertMultiParagraphFileToArray("./testInput.dat");
+
+  const rawWorkflows = input[0];
+  const rawParts = input[1];
 
   return input;
 };
