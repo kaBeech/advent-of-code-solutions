983c2d396f205f6584866ba9dd706ae8c302f32b
kaBeech <contact@kaBeech.com>
2024-01-23 22:31:19 -0800

    Add Rule and Workflow types

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/types.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/types.ts
@@ -1,3 +1,15 @@
+export interface Rule {
+  category: `x` | `m` | `a` | `s`;
+  comparison: `>` | `<`;
+  value: number;
+}
+
+export interface Workflow {
+  name: string;
+  rules: Rule[];
+  endDestination: string;
+}
+
 export interface Part {
   x: number;
   m: number;
