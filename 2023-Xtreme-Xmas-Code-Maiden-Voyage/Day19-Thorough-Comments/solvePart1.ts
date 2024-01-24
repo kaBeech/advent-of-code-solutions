39f3489c28eb20f84009e9e9a310f050817a7811
kaBeech <contact@kaBeech.com>
2024-01-23 23:04:55 -0800

    Scaffold rules for loop

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart1.ts
@@ -3,6 +3,18 @@ import parseInput from "./parseInput.ts";
 export default (async function (): Promise<number> {
   const { workflows, parts } = await parseInput();
 
+  for (const part of parts) {
+    const workflow = workflows.find((workflow) => workflow.name === `in`)!;
+    for (const rule of workflow.rules) {
+      switch (rule.category) {
+        case `x`:
+      }
+    }
+    console.log(
+      `Part ${JSON.stringify(part)} has workflow ${JSON.stringify(workflow)}`,
+    );
+  }
+
   const sumTotal = 0;
 
   console.log(
