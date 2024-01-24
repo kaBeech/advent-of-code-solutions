850b6938710ca4b6a3ff636300b843a167de248e
kaBeech <contact@kaBeech.com>
2024-01-24 01:13:36 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processDestination.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processDestination.ts
@@ -8,10 +8,13 @@ export default (
 ): boolean => {
   switch (destination) {
     case `A`:
+      // If the destination is Accepted, return true so that the part is added to acceptedParts.
       return true;
     case `R`:
+      // If the destination is Rejected, return false so that processing is halted.
       return false;
     default:
+      // Otherwise, recursively evaluate the workflow that the destination refers to.
       return evalWorkflow(
         part,
         workflows.find((workflow) => workflow.name === destination)!,
