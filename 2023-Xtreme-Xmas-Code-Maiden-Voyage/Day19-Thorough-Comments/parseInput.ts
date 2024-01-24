5a9fec4687c532e3ff9ffd7226205bddeeaca263
kaBeech <contact@kaBeech.com>
2024-01-23 22:29:21 -0800

    Parse Parts with JSON

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/parseInput.ts
@@ -6,6 +6,20 @@ export default async (): Promise<ParsedInput> => {
 
   const rawWorkflows = input[0];
   const rawParts = input[1];
+  const parts: Part[] = [];
+
+  for (let rawPart of rawParts) {
+    rawPart = rawPart.replace(/=/g, `: "`).replace(/,/g, `", `).replace(
+      /{/g,
+      `{ `,
+    ).replace(/x/g, `"x"`).replace(/m/g, `"m"`).replace(/a/g, `"a"`).replace(
+      /s/g,
+      `"s"`,
+    ).replace(/}/g, `" }`);
+    parts.push(JSON.parse(rawPart));
+  }
+
+  console.log(rawWorkflows, parts);
 
   return input;
 };
