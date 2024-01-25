5f542e7aab041d96fe07c00b7192a27e8a221ef0
kaBeech <contact@kaBeech.com>
2024-01-24 18:12:44 -0800

    Break rules processing out of workflows loop

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart2.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart2.ts
@@ -14,50 +14,53 @@ export default (async function (): Promise<number> {
     const workflow of workflows
   ) {
     // while (numberOfAcceptablePartCombinations < 3000 || finished < 3) {
-    const rulesWithDestinationA = unprocessedRules.filter((rule) =>
-      rule.destination === `A`
-    );
+
     if (workflow.endDestination === `A`) {
       // Do something.
     }
-    for (const finalRule of rulesWithDestinationA) {
-      const workflow = workflows.find((workflow) =>
-        workflow.name === finalRule.workflowName
-      )!;
-      const acceptablePartsRange: AcceptablePartsRange = {
-        x: { min: 1, max: 4000 },
-        m: { min: 1, max: 4000 },
-        a: { min: 1, max: 4000 },
-        s: { min: 1, max: 4000 },
-      };
-      for (let i = 0; i < finalRule.index; i++) {
-        const rule = workflow.rules[i];
-        switch (rule.comparison) {
-          case ">":
-            acceptablePartsRange[rule.category].max = rule.value;
-            break;
-          case "<":
-            acceptablePartsRange[rule.category].min = rule.value;
-            break;
-        }
-      }
-      switch (finalRule.comparison) {
-        case ">":
-          acceptablePartsRange[finalRule.category].min = finalRule.value + 1;
-          break;
-        case "<":
-          acceptablePartsRange[finalRule.category].max = finalRule.value - 1;
-          break;
-      }
-      endingFilters.push({
-        workflowName: workflow.name,
-        index: finalRule.index,
-        acceptablePartsRange,
-      });
-      unprocessedRules.splice(unprocessedRules.indexOf(finalRule), 1);
-    }
 
     console.log(numberOfAcceptablePartCombinations);
+  }
+
+  // process all rules with destination A
+  const rulesWithDestinationA = unprocessedRules.filter((rule) =>
+    rule.destination === `A`
+  );
+  for (const finalRule of rulesWithDestinationA) {
+    const workflow = workflows.find((workflow) =>
+      workflow.name === finalRule.workflowName
+    )!;
+    const acceptablePartsRange: AcceptablePartsRange = {
+      x: { min: 1, max: 4000 },
+      m: { min: 1, max: 4000 },
+      a: { min: 1, max: 4000 },
+      s: { min: 1, max: 4000 },
+    };
+    for (let i = 0; i < finalRule.index; i++) {
+      const rule = workflow.rules[i];
+      switch (rule.comparison) {
+        case ">":
+          acceptablePartsRange[rule.category].max = rule.value;
+          break;
+        case "<":
+          acceptablePartsRange[rule.category].min = rule.value;
+          break;
+      }
+    }
+    switch (finalRule.comparison) {
+      case ">":
+        acceptablePartsRange[finalRule.category].min = finalRule.value + 1;
+        break;
+      case "<":
+        acceptablePartsRange[finalRule.category].max = finalRule.value - 1;
+        break;
+    }
+    endingFilters.push({
+      workflowName: workflow.name,
+      index: finalRule.index,
+      acceptablePartsRange,
+    });
+    unprocessedRules.splice(unprocessedRules.indexOf(finalRule), 1);
   }
 
   // Get the sum of all parts' rating numbers added together.
