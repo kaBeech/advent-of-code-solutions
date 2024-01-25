d3484de67f23a5443d0afd547f9c3d72ca6bf2c8
kaBeech <contact@kaBeech.com>
2024-01-24 23:51:31 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/moveProcessedRule.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/moveProcessedRule.ts
@@ -14,27 +14,33 @@ export default (
   processedWorkflows: ProcessedWorkflow[],
   endingFilters: EndingFilter[],
 ) => {
+  // Move the rule from the unprocessed list to the processed list.
   processedRules.push(
     unprocessedRules.splice(unprocessedRules.indexOf(ruleBeingProcessed), 1)[0],
   );
 
+  // Record the workflow and processed rules in that workflow to use as shorthand later.
   const finalRuleWorkflow = workflows.find((workflow) =>
     workflow.name === ruleBeingProcessed.workflowName
   )!;
   const processedRulesInWorkflow = processedRules.filter((rule) =>
     rule.workflowName === ruleBeingProcessed.workflowName
   );
+
+  // If all rules in the workflow have been processed, process the workflow.
   if (
     processedRulesInWorkflow.length === finalRuleWorkflow.rules.length + 1
   ) {
     const acceptablePartsRanges: AcceptablePartsRange[] = [];
 
+    // Get all ending filters for the workflow
     const ruleEndingFilters = processedRulesInWorkflow.map((rule) =>
       endingFilters.filter((endingFilter) =>
         endingFilter.workflowName === rule.workflowName &&
         endingFilter.index === rule.index
       )
-    ).flat().filter((endingFilter) => endingFilter !== undefined);
+    ).flat();
+    // Add all acceptable parts ranges from the ending filters to the acceptable parts ranges array.
     for (const endingFilter of ruleEndingFilters) {
       if (endingFilter!.acceptablePartsRange !== null) {
         acceptablePartsRanges.push(
@@ -43,12 +49,14 @@ export default (
       }
     }
 
+    // Add the workflow to the processed workflows array.
     processedWorkflows.push({
       name: ruleBeingProcessed.workflowName,
       acceptablePartsRanges,
     });
   }
 
+  // Return the modified arrays.
   return {
     processedRules,
     unprocessedRules,
