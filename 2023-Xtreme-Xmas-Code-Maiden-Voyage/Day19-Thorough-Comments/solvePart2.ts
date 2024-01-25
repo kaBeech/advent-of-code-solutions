dee112a4d4705e271bfa4fc8496e06fa66803b99
kaBeech <contact@kaBeech.com>
2024-01-24 18:09:53 -0800

    Cleanup

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart2.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/solvePart2.ts
@@ -1,65 +1,15 @@
-import evalWorkflow from "./evalWorkflow.ts";
-import getNextPart from "./getNextPart.ts";
 import parseInput from "./parseInput.ts";
-import {
-  AcceptablePartsRange,
-  EndingFilter,
-  EvaluationResult,
-  Part,
-  Rule,
-  RuleInstance,
-  Workflow,
-} from "./types.ts";
-
-const myFunction = (
-  numberOfAcceptablePartCombinations: number,
-  workflows: Workflow[],
-  currentPart: Part,
-  ruleStack: RuleInstance[],
-) => {
-  let nextStepValue = Infinity;
-
-  while (
-    nextStepValue > 0
-  ) {
-    // If a part is accepted, add it to the acceptedParts array.
-    const result: EvaluationResult = evalWorkflow(
-      currentPart,
-      workflows.find((workflow) => workflow.name === `in`)!,
-      workflows,
-      ruleStack,
-    );
-    if (
-      result.passes
-    ) {
-      numberOfAcceptablePartCombinations += result.value;
-    }
-    nextStepValue = result.value;
-    ruleStack = result.ruleStack;
-
-    currentPart = getNextPart(currentPart, result.category, result.value);
-  }
-  const result = { numberOfAcceptablePartCombinations, ruleStack };
-  return result;
-};
+import { AcceptablePartsRange, EndingFilter, Rule } from "./types.ts";
 
 export default (async function (): Promise<number> {
   // Parse the input into workflows and parts.
   const workflows = (await parseInput()).workflows;
-
-  let currentPart = {
-    x: 1,
-    m: 1,
-    a: 1,
-    s: 1,
-  };
 
   const unprocessedRules: Rule[] = [];
   const endingFilters: EndingFilter[] = [];
 
   // Evaluate each part and make a list of the accepted parts.
   let numberOfAcceptablePartCombinations = 0;
-  let ruleStack: RuleInstance[] = [];
   for (
     const workflow of workflows
   ) {
@@ -106,27 +56,9 @@ export default (async function (): Promise<number> {
       });
       unprocessedRules.splice(unprocessedRules.indexOf(finalRule), 1);
     }
-    const result = myFunction(
-      numberOfAcceptablePartCombinations,
-      workflows,
-      currentPart,
-      ruleStack,
-    );
-    numberOfAcceptablePartCombinations =
-      result.numberOfAcceptablePartCombinations;
-    ruleStack = result.ruleStack;
-    const ruleInstanceToPass = ruleStack.pop()!;
-    currentPart = ruleInstanceToPass.partBeingProcessed;
-    currentPart[ruleInstanceToPass.rule.category] =
-      ruleInstanceToPass.rule.value;
-    if (ruleInstanceToPass.rule.comparison === `>`) {
-      currentPart[ruleInstanceToPass.rule.category] += 1;
-    }
+
     console.log(numberOfAcceptablePartCombinations);
   }
-
-  console.log(ruleStack);
-  console.log(currentPart);
 
   // Get the sum of all parts' rating numbers added together.
 
