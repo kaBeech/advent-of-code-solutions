2292bc0ffca252e4265d5cf2c1ceb6ca77a444ad
kaBeech <contact@kaBeech.com>
2024-01-24 23:58:47 -0800

    Add comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processRule.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day19-Thorough-Comments/processRule.ts
@@ -19,14 +19,19 @@ export default (
   const workflow = workflows.find((workflow) =>
     workflow.name === finalRule.workflowName
   )!;
+
+  // If there are no acceptable parts ranges, add a null ending filter and proceed to moving the rule.
   if (acceptablePartsRanges.length === 0) {
     endingFilters.push({
       workflowName: workflow.name,
       index: finalRule.index,
       acceptablePartsRange: null,
     });
+    // If there are acceptable parts ranges, add an ending filter for each acceptable parts range.
   } else {
     for (const acceptablePartsRange of acceptablePartsRanges) {
+      // Filter the acceptable ranges through all rules leading up to the final rule.
+      // Since we have to fail these rules to get to the final rule, we check for failing the comparisons.
       for (let i = 0; i < finalRule.index; i++) {
         const rule = workflow.rules[i];
         switch (rule.comparison) {
@@ -44,6 +49,7 @@ export default (
             break;
         }
       }
+      // Filter the acceptable ranges through the final rule.
       switch (finalRule.comparison) {
         case ">":
           acceptablePartsRange[finalRule.category].min = Math.max(
@@ -58,12 +64,16 @@ export default (
           );
           break;
       }
+
+      // Add an ending filter.
+      // If the destination of the final rule is R (reject) set the ending filter's acceptable parts range to null.
       if (finalRule.destination === `R`) {
         endingFilters.push({
           workflowName: workflow.name,
           index: finalRule.index,
           acceptablePartsRange: null,
         });
+        // Otherwise, set the ending filter's acceptable parts range to the filtered acceptable parts range.
       } else {
         endingFilters.push({
           workflowName: workflow.name,
@@ -73,6 +83,8 @@ export default (
       }
     }
   }
+
+  // Move the rule from the unprocessed list to the processed list and check if the workflow is completely processed.
   const moveProcessedRuleResult = moveProcessedRule(
     finalRule,
     processedRules,
@@ -84,6 +96,8 @@ export default (
   processedRules = moveProcessedRuleResult.processedRules;
   unprocessedRules = moveProcessedRuleResult.unprocessedRules;
   processedWorkflows = moveProcessedRuleResult.processedWorkflows;
+
+  // Return the modified arrays.
   return {
     processedRules,
     unprocessedRules,
