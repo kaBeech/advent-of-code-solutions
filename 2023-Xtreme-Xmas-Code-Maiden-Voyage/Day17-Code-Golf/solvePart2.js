69ef64269f5ece798ae77a1dc09d46a8be5b3a43
kaBeech <contact@kaBeech.com>
2024-02-01 02:38:44 -0800

    Shorten prop names

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day17-Code-Golf/solvePart2.js
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day17-Code-Golf/solvePart2.js
@@ -2,47 +2,47 @@ import { Heap } from "npm:heap-js";
 
 const gN = (cN, m) => {
   const ns = [];
-  const { x, y } = cN.block.coordinates;
-  const min = cN.consecutiveStepsInSameDirection > 3;
-  const d = cN.direction;
+  const { x, y } = cN.b.coordinates;
+  const min = cN.s > 3;
+  const d = cN.d;
 
   if (y > 0 && d !== "s" && (d === "n" || min)) {
     ns.push({
-      block: m[y - 1][x],
-      direction: "n",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: cN.routeHeatLoss + m[y - 1][x].heatLoss,
+      b: m[y - 1][x],
+      d: "n",
+      s: 1,
+      h: cN.h + m[y - 1][x].heatLoss,
     });
   }
   if (x < m[0].length - 1 && d !== "w" && (d === "e" || min)) {
     ns.push({
-      block: m[y][x + 1],
-      direction: "e",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: cN.routeHeatLoss + m[y][x + 1].heatLoss,
+      b: m[y][x + 1],
+      d: "e",
+      s: 1,
+      h: cN.h + m[y][x + 1].heatLoss,
     });
   }
   if (y < m.length - 1 && d !== "n" && (d === "s" || min)) {
     ns.push({
-      block: m[y + 1][x],
-      direction: "s",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: cN.routeHeatLoss + m[y + 1][x].heatLoss,
+      b: m[y + 1][x],
+      d: "s",
+      s: 1,
+      h: cN.h + m[y + 1][x].heatLoss,
     });
   }
   if (x > 0 && d !== "e" && (d === "w" || min)) {
     ns.push({
-      block: m[y][x - 1],
-      direction: "w",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: cN.routeHeatLoss + m[y][x - 1].heatLoss,
+      b: m[y][x - 1],
+      d: "w",
+      s: 1,
+      h: cN.h + m[y][x - 1].heatLoss,
     });
   }
 
-  const dN = ns.find((n) => n.direction === d);
+  const dN = ns.find((n) => n.d === d);
 
   if (dN) {
-    dN.consecutiveStepsInSameDirection = cN.consecutiveStepsInSameDirection + 1;
+    dN.s = cN.s + 1;
   }
 
   return ns;
@@ -76,55 +76,51 @@ for (const rawCityRow of a) {
 export default (function () {
   const fac = m[m.length - 1][m[0].length - 1];
   let end = fac.minimumRouteHeatLoss;
-  const visited = new Map();
-  const nodesToVisit = new Heap((a, b) => a.routeHeatLoss - b.routeHeatLoss);
+  const v = new Map();
+  const nodesToVisit = new Heap((a, b) => a.h - b.h);
   nodesToVisit.push(
     {
-      block: m[0][1],
-      direction: "e",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: m[0][1].heatLoss,
+      b: m[0][1],
+      d: "e",
+      s: 1,
+      h: m[0][1].heatLoss,
     },
     {
-      block: m[1][0],
-      direction: "s",
-      consecutiveStepsInSameDirection: 1,
-      routeHeatLoss: m[1][0].heatLoss,
+      b: m[1][0],
+      d: "s",
+      s: 1,
+      h: m[1][0].heatLoss,
     }
   );
 
   while (nodesToVisit.length > 0) {
     const currentNode = nodesToVisit.pop();
-    const { block, routeHeatLoss, direction, consecutiveStepsInSameDirection } =
-      currentNode;
-    const { x, y } = block.coordinates;
+    const { b, h, d, s } = currentNode;
+    const { x, y } = b.coordinates;
 
-    if (block === fac) {
-      if (routeHeatLoss < end) {
+    if (b === fac) {
+      if (h < end) {
         fac.finalNode = currentNode;
       }
-      end = Math.min(routeHeatLoss, end);
+      end = Math.min(h, end);
       continue;
     }
 
-    const cacheKey = `${x}-${y}-${direction}-${consecutiveStepsInSameDirection}`;
-    if (visited.has(cacheKey) && visited.get(cacheKey) <= routeHeatLoss) {
+    const cacheKey = `${x}-${y}-${d}-${s}`;
+    if (v.has(cacheKey) && v.get(cacheKey) <= h) {
       continue;
     }
-    visited.set(cacheKey, routeHeatLoss);
+    v.set(cacheKey, h);
 
     const neighbors = gN(currentNode, m);
 
     for (const neighborNode of neighbors) {
-      if (
-        neighborNode.consecutiveStepsInSameDirection < 11 &&
-        neighborNode.routeHeatLoss < end
-      ) {
-        const nBlock = neighborNode.block;
+      if (neighborNode.s < 11 && neighborNode.h < end) {
+        const nBlock = neighborNode.b;
         if (nBlock === fac) {
           nBlock.finalNode = currentNode;
         }
-        nBlock.minimumRouteHeatLoss = routeHeatLoss + nBlock.heatLoss;
+        nBlock.minimumRouteHeatLoss = h + nBlock.heatLoss;
         nodesToVisit.push(neighborNode);
       }
     }
