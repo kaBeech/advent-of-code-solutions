0baacbfba670d70d634d442ee0d7cedd3b77b940
kaBeech <contact@kaBeech.com>
2024-02-01 03:38:13 -0800

    Fix queue handling
    Also, let is shorter than const

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day17-Code-Golf/solvePart2.js
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day17-Code-Golf/solvePart2.js
@@ -1,10 +1,10 @@
 import { Heap } from "npm:heap-js";
 
-let gN = (cN, m) => {
-  let ns = [];
-  let { x, y } = cN.b.c;
-  let n = cN.s > 3;
-  let d = cN.d;
+const gN = (cN, m) => {
+  const ns = [];
+  const { x, y } = cN.b.c;
+  const n = cN.s > 3;
+  const d = cN.d;
 
   y > 0 &&
     d !== "s" &&
@@ -23,24 +23,24 @@ let gN = (cN, m) => {
     (d === "w" || n) &&
     ns.push({ b: m[y][x - 1], d: "w", s: 1, h: cN.h + m[y][x - 1].h });
 
-  let dN = ns.find((n) => n.d === d);
+  const dN = ns.find((n) => n.d === d);
   dN && (dN.s = cN.s + 1);
   return ns;
 };
 
-let m = [];
-let s = await Deno.readTextFile("./t.dat");
-let a = [];
+const m = [];
+const s = await Deno.readTextFile("./t.dat");
+const a = [];
 s.trimEnd()
   .split(/\n/)
   .forEach((l) => {
     a.push(l.split(""));
   });
 let y = 0;
-for (let rR of a) {
-  let r = [];
+for (const rR of a) {
+  const r = [];
   let x = 0;
-  for (let rB of rR) {
+  for (const rB of rR) {
     r.push({
       h: +rB,
       m: Infinity,
@@ -53,31 +53,41 @@ for (let rR of a) {
 }
 
 export default () => {
-  let f = m[m.length - 1][m[0].length - 1];
+  const f = m[m.length - 1][m[0].length - 1];
   let z = f.m;
-  let v = new Map();
-  let q = new Heap((a, b) => a.h - b.h);
-  for (let n of gN({ b: m[0][1], d: "e", s: 1, h: m[0][1].h }, m)) {
-    q.push(n);
-  }
+  const v = new Map();
+  const q = new Heap((a, b) => a.h - b.h);
+  q.push(
+    {
+      b: m[0][1],
+      d: "e",
+      s: 1,
+      h: m[0][1].h,
+    },
+    {
+      b: m[1][0],
+      d: "s",
+      s: 1,
+      h: m[1][0].h,
+    }
+  );
   while (q.length > 0) {
-    let cN = q.pop();
-    let { b, h, d, s } = cN;
-    let { x, y } = b.c;
-    let k = `${x}-${y}-${d}-${s}`;
+    const cN = q.pop();
+    const { b, h, d, s } = cN;
+    const { x, y } = b.c;
+    const k = `${x}-${y}-${d}-${s}`;
     if (b === f) {
       z = Math.min(h, z);
       continue;
     }
     if (v.has(k) && v.get(k) <= h) continue;
     v.set(k, h);
-    for (let nN of gN(cN, m)) {
+    for (const nN of gN(cN, m)) {
       if (nN.s < 11 && nN.h < z) {
         nN.b.m = h + nN.b.h;
         q.push(nN);
       }
     }
   }
-
   return z;
 };
