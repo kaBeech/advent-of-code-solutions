a960393e7489a6d0401b5b57076d51f691f6efa7
kaBeech <contact@kaBeech.com>
2024-03-20 13:25:12 -0700

    Improve semantic function naming

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
@@ -11,16 +11,16 @@ let read_file_to_string filename =
 
 let input_challenge = read_file_to_string "challengeInput.dat";;
 
-let solve_part_1 str x y =
-  let rec solve_part_1' str x y i acc =
+let count_garden_plots_inside_diamond str x y =
+  let rec count_garden_plots_inside_diamond' str x y i acc =
     if i = String.length str then acc
-    else if str.[i] = '\n' then solve_part_1' str (-64) (y - 1) (i + 1) acc
-    else if str.[i] = '.' && abs x + abs y <= 64 && (abs x + abs y) mod 2 == 0 then solve_part_1' str (x + 1) y (i + 1) (acc + 1)
-    else solve_part_1' str (x + 1) y (i + 1) acc
+    else if str.[i] = '\n' then count_garden_plots_inside_diamond' str (-64) (y - 1) (i + 1) acc
+    else if str.[i] = '.' && abs x + abs y <= 64 && (abs x + abs y) mod 2 == 0 then count_garden_plots_inside_diamond' str (x + 1) y (i + 1) (acc + 1)
+    else count_garden_plots_inside_diamond' str (x + 1) y (i + 1) acc
   in
-  solve_part_1' str x y 0 0;;
+  count_garden_plots_inside_diamond' str x y 0 0;;
 
-let solution_part_1 = solve_part_1 input_challenge (-64) 64;;
+let solution_part_1 = count_garden_plots_inside_diamond input_challenge (-64) 64;;
 
 print_string "Part 1: the Elf could reach ";
   print_string (string_of_int solution_part_1);
