2177ea9a5f259a75b33ac7aefb8c97a965fa7470
kaBeech <contact@kaBeech.com>
2024-03-20 11:51:52 -0700

    Count dots in string

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
@@ -1,3 +1,18 @@
 let my_string = "#.#.###...#.#";;
 
-print_string my_string;;
+let count_dots str =
+  let rec count_dots' str i =
+    if i = String.length str then 0
+    else if str.[i] = '.' then 1 + count_dots' str (i + 1)
+    else count_dots' str (i + 1)
+  in
+  count_dots' str 0;;
+
+let dots = count_dots my_string;;
+
+(* Print *)
+
+print_endline "hello";
+  print_endline "world";
+  print_endline my_string;;
+  print_endline (string_of_int dots);;
