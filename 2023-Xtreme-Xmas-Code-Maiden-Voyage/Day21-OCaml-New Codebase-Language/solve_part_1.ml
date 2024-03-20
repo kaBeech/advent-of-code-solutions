f2afa979c31e21716128dbb9154a7bfbc47eb1ea
kaBeech <contact@kaBeech.com>
2024-03-20 11:59:02 -0700

    Copy count_dots to solve_part_1

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_1.ml
@@ -143,6 +143,16 @@ let count_dots str =
 let dots_proto = count_dots my_string;;
 let dots_challenge = count_dots input_challenge;;
 
+let solve_part_1 str =
+  let rec solve_part_1' str i acc =
+    if i = String.length str then acc
+    else if str.[i] = '.' then solve_part_1' str (i + 1) (acc + 1)
+    else solve_part_1' str (i + 1) acc
+  in
+  solve_part_1' str 0 0;;
+
+let solution_part_1 = solve_part_1 my_string;;
+
 (* Print *)
 
 print_endline "hello";
@@ -150,3 +160,4 @@ print_endline "hello";
   print_endline my_string;;
   print_endline (string_of_int dots_proto);;
   print_endline (string_of_int dots_challenge);;
+  print_endline (string_of_int solution_part_1);;
