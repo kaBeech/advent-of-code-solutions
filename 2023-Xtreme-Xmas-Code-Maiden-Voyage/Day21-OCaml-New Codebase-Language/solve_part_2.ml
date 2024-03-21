50a4bc3849e0ca3aca7f11e405adb3f950a8448c
kaBeech <contact@kaBeech.com>
2024-03-21 01:11:08 -0700

    Cleanup

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_2.ml
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day21-OCaml-New Codebase-Language/solve_part_2.ml
@@ -11,12 +11,6 @@ let read_file_to_lines filename =
   read_lines []
 
 let input_challenge = read_file_to_lines "challengeInput.dat";;
-(* let my_string = List.nth input_challenge 5;; *)
-(* let my_char = my_string.[4];; *)
-
-(* print_endline my_string;; *)
-(* print_char my_char;; *)
-
 
 (* A black square is a space that can be moved to in an even number of steps *)
 (* A red square is a space that can be moved to in an odd number of steps *)
@@ -26,34 +20,12 @@ let input_challenge = read_file_to_lines "challengeInput.dat";;
 (* Just like the red and black squares, a red section is a section that orthagonally borders only black sections, and vice versa *)
 (* The starting position is in a black section *)
 
-(* Might need to change that 64 to 65 to reflect the new number of steps *)
 let little_steps = 65;;
 let neg_little_steps = -1 * little_steps;;
 
-(*
 (* For tile_color and section_color, use 'r' for red and 'b' for black *)
 let count_color_tiles_in_color_section section_map x y tile_color section_color within_diamond outside_diamond =
   let rec count_color_tiles_in_color_section' section_map x y tile_color section_color within_diamond outside_diamond i j acc =
-    if i = List.length section_map then acc
-    else if j = String.length (List.nth section_map i) then count_color_tiles_in_color_section' section_map (neg_little_steps) (y - 1) tile_color section_color within_diamond outside_diamond (i + 1) 0 acc
-    else if (List.nth section_map i).[j] = '.' then
-      if within_diamond == true && abs x + abs y > little_steps then count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) acc
-      else if outside_diamond == true && abs x + abs y <= little_steps then count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) acc
-      else if tile_color == section_color && (abs x + abs y) mod 2 == 0 then count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) (acc + 1)
-      else if tile_color != section_color && (abs x + abs y) mod 2 != 0 then count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) (acc + 1)
-      else count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) acc
-    else count_color_tiles_in_color_section' section_map (x + 1) y tile_color section_color within_diamond outside_diamond i (j + 1) acc
-  in
-  count_color_tiles_in_color_section' section_map x y tile_color section_color within_diamond outside_diamond 0 0 0;;
-
-*)
-
-(* For tile_color and section_color, use 'r' for red and 'b' for black *)
-let count_color_tiles_in_color_section section_map x y tile_color section_color within_diamond outside_diamond =
-  let rec count_color_tiles_in_color_section' section_map x y tile_color section_color within_diamond outside_diamond i j acc =
-    (* print_string (string_of_int i); *)
-    (* print_string " "; *)
-    (* print_endline (string_of_int j); *)
     if i = List.length section_map then acc
     else if j = String.length (List.nth section_map i) then count_color_tiles_in_color_section' section_map (neg_little_steps) (y - 1) tile_color section_color within_diamond outside_diamond (i + 1) 0 acc
     else if i = 0 || j == 0 || i = 130 || j == 130 then 
@@ -74,10 +46,6 @@ let count_color_tiles_in_color_section section_map x y tile_color section_color 
 
 let red_tiles_in_red_section = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'r' 'r' false false;;
 let red_tiles_in_black_section = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'r' 'b' false false;;
-(* let black_tiles_in_black_section = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'b' 'b' false false;; *)
-(* let black_tiles_in_red_section = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'b' 'r' false false;; *)
-(* let black_tiles_in_black_section_within_diamond = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'b' 'b' true false ;; *)
-(* let black_tiles_in_black_section_outside_diamond = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'b' 'b' false true;; *)
 let red_tiles_in_black_section_within_diamond = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'r' 'b' true false;;
 let red_tiles_in_red_section_outside_diamond = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'r' 'r' false true;;
 let red_tiles_in_red_section_within_diamond = count_color_tiles_in_color_section input_challenge (neg_little_steps) little_steps 'r' 'r' true false;;
@@ -102,9 +70,8 @@ let red_tiles_in_black_section_outside_diamond = count_color_tiles_in_color_sect
    *)
 
 
-(* number_of_inside_sections = 2 * (big_steps^2) - 2 * big_steps + 1 = 2 * (202300^2) - 2 * 202300 + 1 *)
+(* number_of_red_inside_sections = big_steps^2 *)
 (* number_of_black_inside_sections = side_length^2 *)
-(* number_of_red_inside_sections = number_of_inside_sections - number_of_black_inside_sections = 71618852901 *)
 
 (* Variables for solution equation:
    x = big steps
@@ -113,9 +80,8 @@ let red_tiles_in_black_section_outside_diamond = count_color_tiles_in_color_sect
    RS = number of red tiles in red sections
    BD = number of red tiles *inside* the diamond in black sections
    RD = number of red tiles *outside* the diamond in red sections 
-   BI = number of black inside sections (the math to find this is below, but the shorthand is useful for defining the number of red inside sections)
    *)
-(* Solution = <inside sections> ((s^2)BS + (2x^2 - 2x + 1 - BI) </>  + <side sections> 3sBS + 1sBD + 1sRD </> + <corner sections> 2BS + 2BD + 1RD </> *)
+(* Solution = <inside sections> ((x^2)RS + (s^2)BS </>  + <side sections> 3sBS + 1sBD + 1sRD </> + <corner sections> 2BS + 2BD + 1RD </> *)
 
 let x = 202300;;
 let s = x - 1;;
@@ -123,31 +89,12 @@ let bs = red_tiles_in_black_section;;
 let rs = red_tiles_in_red_section;;
 let bd = red_tiles_in_black_section_within_diamond;; 
 let rd = red_tiles_in_red_section_outside_diamond;;
-(* let bc = red_tiles_in_black_section_outside_diamond;; *)
 
-(* print_endline (string_of_int (3 * bs + bd + rd));; *)
-(* print_endline (string_of_int (4 * bd + 3 * bc + rd));; *)
-
-(* let inside_sections = 2 * (x * x) - 2 * x + 1;; *)
-let black_inside_sections = s * s;;
-(* let red_inside_sections = inside_sections - black_inside_sections;; *)
 let red_inside_sections = x * x;;
-let  black_side_sections = 3 * s;;
-let black_side_diamonds = s;;
-let red_side_outside_diamonds = s;;
-let black_corner_sections = 2;;
-let black_corner_diamonds = 2;;
-let red_corner_outside_diamonds = 1;;
-
+let black_inside_sections = s * s;;
 
 let solution_part_2 = (black_inside_sections * bs) + (red_inside_sections * rs) + (3 * s * bs) + (s * bd) + (s * rd) + (2 * bs) + (2 * bd) + rd;;
 
-(* print_endline (string_of_int red_tiles_in_red_section_within_diamond); *)
-  (* print_endline (string_of_int red_tiles_in_black_section); *)
-  (* print_endline (string_of_int black_tiles_in_red_section); *)
-  (* print_endline (string_of_int black_tiles_in_black_section); *)
-  (* print_endline (string_of_int black_tiles_in_black_section_within_diamond); *)
-  (* print_endline (string_of_int black_tiles_in_black_section_outside_diamond); *)
 print_string "Part 2: the Elf could reach ";
   print_string (string_of_int solution_part_2);
   print_endline " garden plots in 26501365 steps";;
