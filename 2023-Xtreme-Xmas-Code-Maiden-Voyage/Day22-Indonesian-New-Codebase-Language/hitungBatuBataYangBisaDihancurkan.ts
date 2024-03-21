5c4204d6d727719548799846054ba0231cb4c0ba
kaBeech <contact@kaBeech.com>
2024-03-21 12:51:06 -0700

    Scaffold getDisintigratableBlocks

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/hitungBatuBataYangBisaDihancurkan.ts
@@ -0,0 +1,7 @@
+import type { Bata } from "./jenis";
+
+export default (daftarBatuBata: Bata[]): number => {
+    let jumlahBatuBataYangDapatHancur = 0
+
+    return jumlahBatuBataYangDapatHancur;
+};
