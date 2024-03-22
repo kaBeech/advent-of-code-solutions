b1707ae8a402ac6cce6756722810c4a7177e2886
kaBeech <contact@kaBeech.com>
2024-03-22 02:14:39 -0700

    Only shift blocks down if they're not already at the bottom

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/geserBataBatunyaKeBawa.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/geserBataBatunyaKeBawa.ts
@@ -6,17 +6,23 @@ export default (daftarBatuBata: Bata[]): Bata[] => {
     const daftarBatuBataYangDiurutkan = daftarBatuBata.sort((a, b) => {
         return a.zTerendah - b.zTerendah;
     });
-
+    for (const batu of daftarBatuBataYangDiurutkan) {
+        if (batu.zTerendah < 20) {
+            console.log(JSON.stringify(batu));
+        }
+    }
     for (const bata of daftarBatuBataYangDiurutkan) {
-        // I think,we could just remove this first wholw if block, and start woth the 'if' after else a few words down. hitungBatuBataDiBawahnya won't mis-identify bricks as being below themselves, even vertically-standing bricks
-        if (bata.zTertinggi !== bata.zTerendah) {
+        // I think,we could just remove this first whole if block, and start with the 'if' after else a few words down. hitungBatuBataDiBawahnya won't mis-identify bricks as being below themselves, even vertically-standing bricks
+        // console.count("TESTTTTTTTTTTTTT")
+        if (bata.zTertinggi !== bata.zTerendah && bata.zTerendah > 1) {
             let suaraBataTerendah = bata.suara.find((suara) => suara.koordinat.z === bata.zTerendah)!;
-            while (!hitungBatuBataDiBawahnya(daftarBatuBataYangDiurutkan, suaraBataTerendah)) {
+            while (!hitungBatuBataDiBawahnya(daftarBatuBataYangDiurutkan, suaraBataTerendah) && bata.zTerendah > 1) {
                 geserBata(bata, -1)
             }
         } else if (bata.zTerendah > 1) {
             let dapatGeser = true;
-            while (dapatGeser) {
+            while (dapatGeser && bata.zTerendah > 1) {
+                // console.count(bata.zTerendah.toString())
                 for (const suara of bata.suara) {
                     if (hitungBatuBataDiBawahnya(daftarBatuBataYangDiurutkan, suara)) {
                         dapatGeser = false
@@ -30,7 +36,9 @@ export default (daftarBatuBata: Bata[]): Bata[] => {
     }
 
     for (const batu of daftarBatuBataYangDiurutkan) {
-        console.log(JSON.stringify(batu));
+        if (batu.zTerendah < 20) {
+            console.log(JSON.stringify(batu));
+        }
     }
     return daftarBatuBataYangDiurutkan;
 };
