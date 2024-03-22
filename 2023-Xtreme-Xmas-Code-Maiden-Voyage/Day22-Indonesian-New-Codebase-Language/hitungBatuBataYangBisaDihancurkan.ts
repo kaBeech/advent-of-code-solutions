5a70073fbdb6d38f71eedfa2d17cf3ec7bbf79f9
kaBeech <contact@kaBeech.com>
2024-03-22 00:31:10 -0700

    Mark non-strutucturally-imperative middle bricks as disintigratable

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/hitungBatuBataYangBisaDihancurkan.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/hitungBatuBataYangBisaDihancurkan.ts
@@ -1,7 +1,25 @@
+import hitungBatuBataDiBawahnya from "./hitungBatuBataDiBawahnya";
 import type { Bata } from "./jenis";
 
 export default (daftarBatuBata: Bata[]): number => {
     let jumlahBatuBataYangDapatHancur = 0
+
+    for (const bata of daftarBatuBata) {
+        const batuBataDiBawahnya = [] as Bata[]
+        for (const suara of bata.suara) {
+            const bataRendah = hitungBatuBataDiBawahnya(daftarBatuBata, suara)
+            if (bataRendah) { batuBataDiBawahnya.push(bataRendah) }
+        }
+        if (batuBataDiBawahnya.length > 1) {
+            for (const bataRendah of batuBataDiBawahnya) {
+                bataRendah.dapatHancur = true
+            }
+        }
+    }
+
+    for (const bata of daftarBatuBata) {
+        if (bata.dapatHancur) { jumlahBatuBataYangDapatHancur++ }
+    }
 
     return jumlahBatuBataYangDapatHancur;
 };
