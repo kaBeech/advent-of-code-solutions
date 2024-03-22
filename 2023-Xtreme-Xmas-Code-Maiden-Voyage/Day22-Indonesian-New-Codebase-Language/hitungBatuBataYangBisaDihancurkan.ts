815db85c43ba616eb337a69e5417ca4d37474d42
kaBeech <contact@kaBeech.com>
2024-03-22 00:51:31 -0700

    Mark bricks with no bricks above them an disintegratable

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/hitungBatuBataYangBisaDihancurkan.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day22-Indonesian-New-Codebase-Language/hitungBatuBataYangBisaDihancurkan.ts
@@ -6,19 +6,29 @@ export default (daftarBatuBata: Bata[]): number => {
 
     for (const bata of daftarBatuBata) {
         const batuBataDiBawahnya = [] as Bata[]
+        const batuBataDiAtas = [] as Bata[]
         for (const suara of bata.suara) {
             const bataRendah = hitungBatuBataDiBawahnya(daftarBatuBata, suara)
             if (bataRendah) { batuBataDiBawahnya.push(bataRendah) }
+            const bataTinggi = hitungBatuBataDiAtas(daftarBatuBata, suara)
+            if (bataTinggi) { batuBataDiAtas.push(bataTinggi) }
         }
         if (batuBataDiBawahnya.length > 1) {
             for (const bataRendah of batuBataDiBawahnya) {
                 bataRendah.dapatHancur = true
             }
         }
+        if (batuBataDiAtas.length === 0) {
+            bata.dapatHancur = true
+        }
     }
 
     for (const bata of daftarBatuBata) {
         if (bata.dapatHancur) { jumlahBatuBataYangDapatHancur++ }
+    }
+
+    for (const bata of daftarBatuBata) {
+        console.log(JSON.stringify(bata))
     }
 
     return jumlahBatuBataYangDapatHancur;
