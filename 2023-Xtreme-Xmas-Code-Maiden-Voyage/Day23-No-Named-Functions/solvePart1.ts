af71db5cc0f54e8071f679c253bd481beec86466
kaBeech <contact@kaBeech.com>
2024-03-27 10:43:31 -0700

    Implement Reverse Dijkstra's algorithm

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
@@ -1,9 +1,10 @@
 import { XYCoordinates } from "../../tools/commonTypes.ts";
 
+// Declate Types
+
 interface Tile {
   coordinates: XYCoordinates;
   value: "#" | "." | "<" | ">" | "^" | "v";
-  visited: boolean;
   distanceFromStart: number;
   reachableTiles: XYCoordinates[];
 }
@@ -12,7 +13,14 @@ interface TrailMap {
   tiles: Tile[];
 }
 
-const inputFile = "./challengeInput.dat"
+interface Path {
+  currentTileCoordinates: XYCoordinates;
+  visitedTiles: XYCoordinates[];
+}
+
+// Parse Input
+
+const inputFile = "./testInput.dat"
 
 const inputLines = await Deno.readTextFile(inputFile).then((text: string) => text.trim().split("\n"));
 
@@ -25,68 +33,111 @@ for (const [y, line] of inputLines.entries()) {
     trailMap.tiles.push({
       coordinates: { x, y },
       value,
-      visited: false,
       distanceFromStart: 0,
       reachableTiles: []
     });
   }
 }
 
-for (const tile of trailMap.tiles) {
-  const { x, y } = tile.coordinates;
-  const adjacentTiles = [
-    { x: x - 1, y },
-    { x: x + 1, y },
-    { x, y: y - 1 },
-    { x, y: y + 1 }
-  ];
-
-  for (const adjacentTile of adjacentTiles) {
-    if (adjacentTile.x < 0 || adjacentTile.y < 0 || adjacentTile.x >= inputLines[0].length || adjacentTile.y >= inputLines.length) {
-      continue;
-    }
-    const adjacentTileValue = trailMap.tiles.find(
-      (t) =>
-        t.coordinates.x === adjacentTile.x && t.coordinates.y === adjacentTile.y
-    )?.value;
-    console.count(`${adjacentTile.x}-${adjacentTile.y}`);
-
-    switch (adjacentTileValue) {
-      case "#":
-        break;
-      case ".":
-        tile.reachableTiles.push(adjacentTile);
-        break;
-      case "<":
-        if (adjacentTile.x === x - 1) {
-          tile.reachableTiles.push(adjacentTile);
-        }
-        break;
-      case ">":
-        if (adjacentTile.x === x + 1) {
-          tile.reachableTiles.push(adjacentTile);
-        }
-        break;
-      case "^":
-        if (adjacentTile.y === y - 1) {
-          tile.reachableTiles.push(adjacentTile);
-        }
-        break;
-      case "v":
-        if (adjacentTile.y === y + 1) {
-          tile.reachableTiles.push(adjacentTile);
-        }
-        break;
-      default:
-        throw new Error(`Unexpected tile value: ${adjacentTileValue}`);
-    }
-  }
-}
-
 export default (async function(): Promise<number> {
-  let currentTileCoordinates = { x: 1, y: 0 };
+  const pathsToExplore: Path[] = [];
+  pathsToExplore.push({ currentTileCoordinates: { x: 1, y: 0 }, visitedTiles: [] });
 
-  const longestHikeSteps = 0
+  while (pathsToExplore.length > 0) {
+    let currentPath = pathsToExplore.pop()!
+    const currentTile = trailMap.tiles.find(
+      (t) =>
+        t.coordinates.x === currentPath.currentTileCoordinates.x &&
+        t.coordinates.y === currentPath.currentTileCoordinates.y
+    )!;
+
+    const { x, y } = currentTile.coordinates;
+    const adjacentTiles = [
+      { x: x - 1, y },
+      { x: x + 1, y },
+      { x, y: y - 1 },
+      { x, y: y + 1 }
+    ];
+
+    for (const adjacentTileCoordinates of adjacentTiles) {
+      if (adjacentTileCoordinates.x < 0 || adjacentTileCoordinates.y < 0 || adjacentTileCoordinates.x >= inputLines[0].length || adjacentTileCoordinates.y >= inputLines.length) {
+        continue;
+      }
+      if (currentPath.visitedTiles.find((t) => t.x === adjacentTileCoordinates.x && t.y === adjacentTileCoordinates.y)) {
+        continue;
+      }
+      const adjacentTile = trailMap.tiles.find(
+        (t) =>
+          t.coordinates.x === adjacentTileCoordinates.x && t.coordinates.y === adjacentTileCoordinates.y
+      )!;
+      if (adjacentTile.distanceFromStart > currentTile.distanceFromStart + 1) {
+        continue;
+      }
+      if (adjacentTile.distanceFromStart < currentTile.distanceFromStart + 1) {
+        adjacentTile.distanceFromStart = currentTile.distanceFromStart + 1;
+      }
+      const nextPath = { currentTileCoordinates: adjacentTileCoordinates, visitedTiles: [...currentPath.visitedTiles, currentTile.coordinates] };
+      switch (adjacentTile.value) {
+        case "#":
+          break;
+        case ".":
+          pathsToExplore.push(nextPath);
+          break;
+        case "<":
+          if (adjacentTileCoordinates.x <= x) {
+            pathsToExplore.push(nextPath);
+          }
+          break;
+        case ">":
+          if (adjacentTileCoordinates.x >= x) {
+            pathsToExplore.push(nextPath);
+          }
+          break;
+        case "^":
+          if (adjacentTileCoordinates.y <= y) {
+            pathsToExplore.push(nextPath);
+          }
+          break;
+        case "v":
+          if (adjacentTileCoordinates.y >= y) {
+            pathsToExplore.push(nextPath);
+          }
+          break;
+        default:
+          throw new Error(`Unexpected tile value: ${adjacentTile.value}`);
+      }
+    }
+    // const reachableTiles = currentTile.reachableTiles.filter(
+    // (t) => !(trailMap.tiles.find((tile) => tile.coordinates.x === t.x && tile.coordinates.y === t.y)?.visited)
+    // );
+
+    // if (reachableTiles.length === 0) {
+    // break;
+    // }
+
+    // currentTileCoordinates = reachableTiles[0];
+
+    // for (const nextTileCoordinates of currentTile.reachableTiles) {
+    // const nextTile = trailMap.tiles.find(
+    // (t) =>
+    // t.coordinates.x === nextTileCoordinates.x &&
+    // t.coordinates.y === nextTileCoordinates.y
+    // )!;
+    // if (nextTile.distanceFromStart <= currentTile.distanceFromStart) {
+    // nextTile.distanceFromStart = currentTile.distanceFromStart + 1;
+    // if (!tilesToVisit.find((t) => t.x === nextTileCoordinates.x && t.y === nextTileCoordinates.y)) {
+    // tilesToVisit.push(nextTileCoordinates);
+    // }
+    // }
+    // }
+
+  }
+
+  let endingNode = trailMap.tiles.find((t) => t.coordinates.x === inputLines[0].length - 2 && t.coordinates.y === inputLines.length - 1)!;
+
+  console.log(JSON.stringify(endingNode));
+
+  const longestHikeSteps = endingNode.distanceFromStart;
 
   // console.log(JSON.stringify(trailMap.tiles));
 
