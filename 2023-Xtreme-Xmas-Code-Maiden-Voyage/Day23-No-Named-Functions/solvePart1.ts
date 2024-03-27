80a08a5befcd562db089f6ab2dd03148229b6df1
kaBeech <contact@kaBeech.com>
2024-03-27 01:17:10 -0700

    Parse input

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
@@ -1,4 +1,4 @@
-import { XYCoordinates } from "../../tools/commonTypes";
+import { XYCoordinates } from "../../tools/commonTypes.ts";
 
 interface Tile {
   coordinates: XYCoordinates;
@@ -10,16 +10,80 @@ interface TrailMap {
   tiles: Tile[];
 }
 
-
 const inputFile = "./challengeInput.dat"
 
 const inputLines = await Deno.readTextFile(inputFile).then((text: string) => text.trim().split("\n"));
+
+const trailMap: TrailMap = {
+  tiles: []
+};
+
+for (const [y, line] of inputLines.entries()) {
+  for (const [x, value] of line.split("").entries()) {
+    trailMap.tiles.push({
+      coordinates: { x, y },
+      value,
+      reachableTiles: []
+    });
+  }
+}
+
+for (const tile of trailMap.tiles) {
+  const { x, y } = tile.coordinates;
+  const adjacentTiles = [
+    { x: x - 1, y },
+    { x: x + 1, y },
+    { x, y: y - 1 },
+    { x, y: y + 1 }
+  ];
+
+  for (const adjacentTile of adjacentTiles) {
+    if (adjacentTile.x < 0 || adjacentTile.y < 0 || adjacentTile.x >= inputLines[0].length || adjacentTile.y >= inputLines.length) {
+      continue;
+    }
+    const adjacentTileValue = trailMap.tiles.find(
+      (t) =>
+        t.coordinates.x === adjacentTile.x && t.coordinates.y === adjacentTile.y
+    )?.value;
+    console.count(`${adjacentTile.x}-${adjacentTile.y}`);
+
+    switch (adjacentTileValue) {
+      case "#":
+        break;
+      case ".":
+        tile.reachableTiles.push(adjacentTile);
+        break;
+      case "<":
+        if (adjacentTile.x === x - 1) {
+          tile.reachableTiles.push(adjacentTile);
+        }
+        break;
+      case ">":
+        if (adjacentTile.x === x + 1) {
+          tile.reachableTiles.push(adjacentTile);
+        }
+        break;
+      case "^":
+        if (adjacentTile.y === y - 1) {
+          tile.reachableTiles.push(adjacentTile);
+        }
+        break;
+      case "v":
+        if (adjacentTile.y === y + 1) {
+          tile.reachableTiles.push(adjacentTile);
+        }
+        break;
+      default:
+        throw new Error(`Unexpected tile value: ${adjacentTileValue}`);
+    }
+  }
+}
 
 export default (async function(): Promise<number> {
 
   const longestHikeSteps = 0
 
-  console.log(inputLines);
+  // console.log(JSON.stringify(trailMap.tiles));
 
   console.log(`Part 1: The longest hike is ${longestHikeSteps} steps long`);
 
