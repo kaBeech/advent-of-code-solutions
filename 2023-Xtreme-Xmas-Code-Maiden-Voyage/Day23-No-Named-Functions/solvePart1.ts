0d9edf67d463ecbbf0829a98c1f5192f64529ae3
kaBeech <contact@kaBeech.com>
2024-03-27 00:59:14 -0700

    Add types

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day23-No-Named-Functions/solvePart1.ts
@@ -1,6 +1,25 @@
+import { XYCoordinates } from "../../tools/commonTypes";
+
+interface Tile {
+  coordinates: XYCoordinates;
+  value: "#" | "." | "<" | ">" | "^" | "v";
+  reachableTiles: XYCoordinates[];
+}
+
+interface TrailMap {
+  tiles: Tile[];
+}
+
+
+const inputFile = "./challengeInput.dat"
+
+const inputLines = await Deno.readTextFile(inputFile).then((text: string) => text.trim().split("\n"));
+
 export default (async function(): Promise<number> {
 
   const longestHikeSteps = 0
+
+  console.log(inputLines);
 
   console.log(`Part 1: The longest hike is ${longestHikeSteps} steps long`);
 
