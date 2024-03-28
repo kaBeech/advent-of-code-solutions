6d841feebfaa169e9226463e176e3483dd14025a
kaBeech <contact@kaBeech.com>
2024-03-28 15:12:39 -0700

    Add types for euclidean geometry

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-No-Comments/pkg/types/reusables.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-No-Comments/pkg/types/reusables.go
@@ -1,7 +1,28 @@
 package types
 
+type XYIntegerCoordinates struct {
+	X int
+	Y int
+}
+
 type XYZIntegerCoordinates struct {
 	X int
 	Y int
 	Z int
+}
+
+type XYFloatCoordinates struct {
+	X float64
+	Y float64
+}
+
+type XYZFloatCoordinates struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type Line2D struct {
+	Slope      float64
+	YIntercept float64
 }
