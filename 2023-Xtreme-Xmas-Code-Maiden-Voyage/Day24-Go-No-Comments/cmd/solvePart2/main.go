fd92bc1ba6669897c54e874b798091a42bb19cf7
kaBeech <contact@kaBeech.com>
2024-03-28 19:49:41 -0700

    Scaffold Solve Part 2

---
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/cmd/solvePart2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/parse"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/types"
+)
+
+func main() {
+	filePath := "testInput.dat"
+	// filePath := "challengeInput.dat"
+	var hailstones []types.Hailstone = parse.ParseInput(filePath)
+	var perfectRockTrajectory types.Hailstone = intersections.GetPerfectRockTrajectory(hailstones)
+	var result float64 = perfectRockTrajectory.Position.X + perfectRockTrajectory.Position.Y + perfectRockTrajectory.Position.Z
+	fmt.Println("Part 2: the sum of the coordinates of th perfect rock trajectory's starting position is", result)
+}
