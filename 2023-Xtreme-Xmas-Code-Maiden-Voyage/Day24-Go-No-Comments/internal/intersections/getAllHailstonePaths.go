ae8178e7a0e58e95f1c5880ffce112b3c6aa3785
kaBeech <contact@kaBeech.com>
2024-03-28 18:58:49 -0700

    Cleanup

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections/getAllHailstonePaths.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections/getAllHailstonePaths.go
@@ -36,28 +36,3 @@ func Get2DHailstonePathIntersections(hailstones []types.Hailstone) []Intersectio
 
 	return intersections
 }
-
-func GetFutureIntersectionsInTestArea(intersections []Intersection, testArea TestArea) []Intersection {
-	var futureIntersections []Intersection
-
-	for _, intersection := range intersections {
-		if checkIfIntersectionIsInTestArea(intersection, testArea) && checkIfIntersectionIsInEitherHailstonesFuturePath(intersection) {
-			futureIntersections = append(futureIntersections, intersection)
-		}
-	}
-
-	return futureIntersections
-}
-
-func checkIfIntersectionIsInTestArea(intersection Intersection, testArea TestArea) bool {
-	return intersection.Intersection.X >= float64(testArea.Min) && intersection.Intersection.X <= float64(testArea.Max) && intersection.Intersection.Y >= float64(testArea.Min) && intersection.Intersection.Y <= float64(testArea.Max)
-}
-
-func checkIfIntersectionIsInEitherHailstonesFuturePath(intersection Intersection) bool {
-
-	intersectionIsInTheFuturePathOfHailstone1 := tools.IsAApproachingB(intersection.Hailstone1.Position.X, intersection.Intersection.X, intersection.Hailstone1.Velocity.X)
-	intersectionIsInTheFuturePathOfHailstone2 := tools.IsAApproachingB(intersection.Hailstone2.Position.X, intersection.Intersection.X, intersection.Hailstone2.Velocity.X)
-
-	return intersectionIsInTheFuturePathOfHailstone1 || intersectionIsInTheFuturePathOfHailstone2
-}
-
