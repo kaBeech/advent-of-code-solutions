1b178a9b8c41bb87bb607a0d177270e6ee3d7d47
kaBeech <contact@kaBeech.com>
2024-03-28 17:24:46 -0700

    Add getIntersections call

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/cmd/solvePart1/main.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/cmd/solvePart1/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/parse"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/types"
 )
 
 func main() {
 	filePath := "testInput.dat"
-	var hailstones []parse.Hailstone = parse.ParseInput(filePath)
+	var hailstones []types.Hailstone = parse.ParseInput(filePath)
+	var hailstoneIntersections []types.HailstoneIntersection = intersections.Get2DHailstonePathIntersections(hailstones)
 	fmt.Println(hailstones)
 }
