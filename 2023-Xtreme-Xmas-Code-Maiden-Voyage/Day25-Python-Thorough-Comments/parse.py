7ad34ea0be0af7405578016cb71a78042823c43d
kaBeech <contact@kaBeech.com>
2024-04-10 14:35:20 -0700

    Add reverse component connections

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/parse.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/parse.py
@@ -60,7 +60,24 @@ def parse_input(file_location: str):
 
         # Create a Component object and add it to the list of Components
         components.append(Component(id, connections.split(" ")))
-    
+   
+    # Since the input only list unidirectional connections, add the reverse
+    #   connections to each Component
+    # Since we know that the input is perfectly formatted and there are no 
+    #   duplicate ids, we can use this method: for each component, for each 
+    #   connection, find the component with that connnection id and add the 
+    #   current component's id to its list of connections
+    # This is O(n^2) but we only have to do it once and the input is small-ish 
+    #   so I'm okay with it
+    for component in components:
+        for connection in component.connections:
+            for other_component in components:
+                if other_component.id == connection:
+                    other_component.connections.append(component.id)
+                    break
+
+    print(components)
+
     # Return the list of Components
     return components
 
