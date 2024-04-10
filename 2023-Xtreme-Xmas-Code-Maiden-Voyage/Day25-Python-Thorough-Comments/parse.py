ffa8aed05410507d5dcd23c387c90c2194366607
kaBeech <contact@kaBeech.com>
2024-04-10 15:31:07 -0700

    Add Connection objects
    They're the ones we want to actually track heat on

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/parse.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/parse.py
@@ -1,21 +1,18 @@
 # A Component will be an object with the following properties: id (a 
-#   3-character string), connections (a list of other Component ids), and heat 
-#   (an integer initially set to 0)
+#   3-character string) and connected_components (a list of other Component ids)
 class Component:
-
     # Initialize the Component object
-    def __init__(self, id: str, connections: list):
+    def __init__(self, id: str, connected_components: list):
         self.id = id
-        self.connections = connections
-        self.heat = 0
+        self.connected_components = connected_components
 
     # Return a string representation of the Component object for printing
     def __str__(self):
-        return f"Component {self.id} with connections {self.connections} and heat {self.heat}"
+        return f"Component {self.id} with connections {self.connected_components}"
 
     # Return a string representation of the Component object for debugging
     def __repr__(self):
-        return f"Component {self.id} with connections {self.connections} and heat {self.heat}"
+        return f"Component {self.id} with connections {self.connected_components}"
 
     # Add equality comparison method
     def __eq__(self, other):
@@ -27,7 +24,37 @@ class Component:
     def __hash__(self):
         return hash(self.id)
 
+# A Connection will be an object with the following properties: component1 (a 
+#   Component object), component2 (a different Component object), and heat (an 
+#   integer initially set to 0)
+class Connection:
+        # Initialize the Connection object
+        def __init__(self, component1: Component, component2: Component):
+            self.component1 = component1
+            self.component2 = component2
+            self.heat = 0
+    
+        # Return a string representation of the Connection object for printing
+        def __str__(self):
+            return f"Connection between {self.component1.id} and {self.component2.id} with heat {self.heat}"
+    
+        # Return a string representation of the Connection object for debugging
+        def __repr__(self):
+            return f"Connection between {self.component1.id} and {self.component2.id} with heat {self.heat}"
+    
+        # Add equality comparison method
+        def __eq__(self, other):
+            if not isinstance(other, Connection):
+                return False
+            return (self.component1 == other.component1 and self.component2 == other.component2) or (self.component1 == other.component2 and self.component2 == other.component1)
+    
+        # Add hash method
+        def __hash__(self):
+            return hash(self.component1) ^ hash(self.component2)
+
+
 # Take the input file location as a string and return a list of Components
+#   and a list of Connections
 # Assume the input consists of lines of the form "cid: con ect ion" where "cid" 
 #   is an id (a 3-character string), and everything after the colon 
 #   ("con ect ion") is a space-separated list of ids of other Components that 
@@ -35,9 +62,6 @@ class Component:
 # For example, the input line "jqt: rhn xhk nvd" would create a Component with 
 #   id "jqt", connections ["rhn", "xhk", "nvd"], and heat 0
 def parse_input(file_location: str):
-    # Initialize the list of Components
-    components = []
-
     # Open the file
     file = open(file_location)
 
@@ -46,6 +70,10 @@ def parse_input(file_location: str):
 
     # Close the file
     file.close()
+
+    # Initialize the lists of Components and Connections
+    components = []
+    connections = []
 
     # For each raw component string, create a Component object and add it to 
     #   the list of Components
@@ -56,24 +84,27 @@ def parse_input(file_location: str):
             continue
 
         # Split the raw component string into an id and a list of connections
-        id, connections = component_raw.split(": ")
+        id, connections_raw = component_raw.split(": ")
 
         # Create a Component object and add it to the list of Components
-        components.append(Component(id, connections.split(" ")))
-   
-    # Since the input only list unidirectional connections, add the reverse
-    #   connections to each Component
+        components.append(Component(id, connections_raw.split(" ")))
+    
+    # For each pair of Components in the list of Components, create a Connection
+    # Since the input only list unidirectional connections, also add the 
+    #   reverse connections to each Component
     # Since we know that the input is perfectly formatted and there are no 
     #   duplicate ids, we can use this method: for each component, for each 
-    #   connection, find the component with that connnection id and add the 
-    #   current component's id to its list of connections
+    #   connection, find the component with that connected component's id and 
+    #   add the current component's id to its list of connections. Then create 
+    #   a Connection object and add it to the list of Connections
     # This is O(n^2) but we only have to do it once and the input is small-ish 
     #   so I'm okay with it
     for component in components:
         for connection in component.connections:
-            for other_component in components:
-                if other_component.id == connection:
-                    other_component.connections.append(component.id)
+            for connected_component in components:
+                if connected_component.id == connection:
+                    connected_component.connections.append(component.id)
+                    connections.append(Connection(component, connected_component))
                     break
 
     print(components)
