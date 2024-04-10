b82be5c9bfa164502be9fd9cc221afec2f9678ca
kaBeech <contact@kaBeech.com>
2024-04-10 14:58:20 -0700

    Clarify with comments

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/part1.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/part1.py
@@ -1,7 +1,10 @@
 from parse import parse_input
 
 def solve_part1():
+    # Get the list of components from the input file (make sure to specify the 
+    #   correct file path as a string)
     components = parse_input("test_input.dat")
+    
     result = 42 
     return result
 
