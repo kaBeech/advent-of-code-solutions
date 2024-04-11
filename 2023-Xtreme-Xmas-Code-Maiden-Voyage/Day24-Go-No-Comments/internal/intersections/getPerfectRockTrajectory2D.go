83b1eefce1936914ea6b0ddd1edc077634d06706
kaBeech <contact@kaBeech.com>
2024-04-11 15:56:04 -0700

    Import from package name, not file name

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections/getPerfectRockTrajectory2D.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections/getPerfectRockTrajectory2D.go
@@ -1,11 +1,11 @@
 package intersections
 
 import (
-	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/matrix"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/tools"
 	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/types"
 )
 
 func GetPerfectRockTrajectory2D(inputMatrix [][]float64) types.PerfectRockTrajectory2D {
-	result := matrix.SolveSystemOfEquations(inputMatrix)
+	result := tools.SolveSystemOfEquations(inputMatrix)
 	return types.PerfectRockTrajectory2D{Position: types.XYFloatCoordinates{X: result[0], Y: result[1]}, Velocity: types.XYFloatCoordinates{X: result[2], Y: result[3]}}
 }
