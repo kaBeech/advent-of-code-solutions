7965f79cd07e6890a69ad6578c2139b3b66e2e98
kaBeech <contact@kaBeech.com>
2024-04-10 20:16:57 -0700

    Use pointers instead of passing around objects

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/connection.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/connection.py
@@ -1,22 +1,20 @@
-from component import Component
-
 # A Connection will be an object with the following properties: component1 (a 
 #   Component object), component2 (a different Component object), and heat (an 
 #   integer initially set to 0)
 class Connection:
         # Initialize the Connection object
-        def __init__(self, component1: Component, component2: Component):
+        def __init__(self, component1: str, component2: str):
             self.component1 = component1
             self.component2 = component2
             self.heat = 0
     
         # Return a string representation of the Connection object for printing
         def __str__(self):
-            return f"Connection between {self.component1.id} and {self.component2.id} with heat {self.heat}"
+            return f"Connection between {self.component1} and {self.component2} with heat {self.heat}"
     
         # Return a string representation of the Connection object for debugging
         def __repr__(self):
-            return f"Connection between {self.component1.id} and {self.component2.id} with heat {self.heat}"
+            return f"Connection between {self.component1} and {self.component2} with heat {self.heat}"
     
         # Add equality comparison method
         def __eq__(self, other):
@@ -30,8 +28,8 @@ class Connection:
 
 def get_connection_by_component_ids(component1_id: str, component2_id: str, connections: list[Connection]) -> Connection | None:
     for connection in connections:
-        if connection.component1.id == component1_id and connection.component2.id == component2_id:
+        if connection.component1 == component1_id and connection.component2 == component2_id:
             return connection
-        if connection.component1.id == component2_id and connection.component2.id == component1_id:
+        if connection.component1 == component2_id and connection.component2 == component1_id:
             return connection
     return None
