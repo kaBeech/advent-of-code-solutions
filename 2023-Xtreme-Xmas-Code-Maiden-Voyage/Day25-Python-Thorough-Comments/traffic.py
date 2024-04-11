cba11658e0c948c74319180c0d733f86dc9e473b
kaBeech <contact@kaBeech.com>
2024-04-10 17:20:40 -0700

    Scaffold traffic simulation

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/traffic.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/traffic.py
@@ -1,4 +1,12 @@
-def simulate_traffic(components):
-    print(components)
+from heapq import heappop, heappush
 
-print(simulate_traffic("Hello!"))
+def simulate_traffic(components, connections):
+    # Initialze a connections heap so we don't have to sort as much
+    connections_by_heat = []
+    for connection in connections:
+        heappush_by_heat(connections_by_heat, connection)
+
+
+def heappush_by_heat(heap, connection):
+    # Use a negative heat value to make the heap a min-heap
+    heappush(heap, (-connection.heat, connection))
