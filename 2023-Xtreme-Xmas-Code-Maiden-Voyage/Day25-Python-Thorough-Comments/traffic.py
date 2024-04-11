cada78ea26a4e99f1d2b0a7ec3e6a7c202ddf37e
kaBeech <contact@kaBeech.com>
2024-04-10 17:24:19 -0700

    Scaffold random traffic path simulation

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/traffic.py
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day25-Python-Thorough-Comments/traffic.py
@@ -1,12 +1,34 @@
-from heapq import heappop, heappush
+from random import choice as random_choice
 
 def simulate_traffic(components, connections):
-    # Initialze a connections heap so we don't have to sort as much
-    connections_by_heat = []
-    for connection in connections:
-        heappush_by_heat(connections_by_heat, connection)
+    # I was going to use a to reduce sorting, but I'm not that familiar with 
+    #   heaps in python and my preliminary testing yielded errors when using 
+    #   duplicate priority values, so we'll just sort for now
+    
+    # Simulate random paths until the 3rd hottest component has a heat value 
+    #   that is both greater than 10 and at least double the heat value of 
+    #   the 4th hottest component
+    while connections[2].heat <= 2 * connections[3].heat or connections[2].heat <= 10:
+        # Simulate a random path
+        simulate_random_traffic_path(components, connections)
 
+        # Sort the connections by heat
+        connections.sort(key=lambda connection: connection.heat, reverse=True)
 
-def heappush_by_heat(heap, connection):
-    # Use a negative heat value to make the heap a min-heap
-    heappush(heap, (-connection.heat, connection))
+     
+def simulate_random_traffic_path(components, connections):
+    # Randomly choose starting and ending components
+    component_start = random_choice(components)
+    component_end = random_choice(components)
+
+    # If the starting and ending components are the same, choose a new ending
+    while component_start == component_end:
+        component_end = random_choice(components)
+
+    # Find a path from the starting component to the ending component
+    path = find_path(component_start, component_end, connections)
+
+    # Increment the heat of each connection in the path
+    for connection in path:
+        connection.heat += 1
+
