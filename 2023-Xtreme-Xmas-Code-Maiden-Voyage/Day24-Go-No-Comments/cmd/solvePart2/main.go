ab7d46701a062c555ffe0db8648d1df0ad05c26f
kaBeech <contact@kaBeech.com>
2024-04-11 23:05:55 -0700

    Print result as an integer

--- 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/cmd/solvePart2/main.go
+++ 2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/cmd/solvePart2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/intersections"
 	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/internal/parse"
+	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/tools"
 	"github.com/kaBeech/advent-of-code-solutions/2023-Xtreme-Xmas-Code-Maiden-Voyage/Day24-Go-No-Comments/pkg/types"
 )
 
@@ -12,6 +13,7 @@ func main() {
 	// filePath := "challengeInput.dat"
 	var hailstones []types.Hailstone = parse.ParseInput(filePath)
 	var perfectRockTrajectory types.PerfectRockTrajectory = intersections.GetPerfectRockTrajectory(hailstones)
-	var result float64 = perfectRockTrajectory.Position.X + perfectRockTrajectory.Position.Y + perfectRockTrajectory.Position.Z
+	println(perfectRockTrajectory.Position.X, perfectRockTrajectory.Position.Y, perfectRockTrajectory.Position.Z, perfectRockTrajectory.Velocity.X, perfectRockTrajectory.Velocity.Y, perfectRockTrajectory.Velocity.Z)
+	var result int = tools.GetInt(perfectRockTrajectory.Position.X + perfectRockTrajectory.Position.Y + perfectRockTrajectory.Position.Z)
 	fmt.Println("Part 2: The sum of the coordinates of the perfect rock trajectory's starting position is", result)
 }
