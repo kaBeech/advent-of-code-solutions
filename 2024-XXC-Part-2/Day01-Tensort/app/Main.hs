2a76b18fa27a8f0d345f5a280a866da41f1c2466
kaBeech <contact@kaBeech.com>
2024-12-02 15:35:04 -0800

    Cleanup

--- 2024-XXC-Part-2/Day01-Tensort/app/Main.hs
+++ 2024-XXC-Part-2/Day01-Tensort/app/Main.hs
@@ -7,13 +7,13 @@ import Similarity (getSimilarityScore)
 
 main :: IO ()
 main = do
-  putStrLn "Part 1: The total distance between our lists is:"
-  input <- readFile "challenge_input.dat" -- Change to "challenge_input.dat"
+  input <- readFile "test_input.dat" -- Change to "challenge_input.dat"
   let (listL, listR) = parseInput input
   let sortedL = tensort listL
   let sortedR = tensort listR
   let diffs = getDiffs sortedL sortedR
+  putStrLn "Part 1: The total distance between our lists is:"
   print $ sum diffs
-  putStrLn "Part 2: The similarity score for our lists is:"
   let similarityScore = getSimilarityScore sortedL sortedR 0
+  putStrLn "Part 2: The similarity score for our lists is:"
   print similarityScore
