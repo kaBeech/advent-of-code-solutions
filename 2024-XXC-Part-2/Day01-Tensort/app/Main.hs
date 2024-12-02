28db5ddc6061437ac168d7b31b9f9298e7bbc1ed
kaBeech <contact@kaBeech.com>
2024-12-02 14:54:22 -0800

    Solve Part 1

--- 2024-XXC-Part-2/Day01-Tensort/app/Main.hs
+++ 2024-XXC-Part-2/Day01-Tensort/app/Main.hs
@@ -7,7 +7,7 @@ import ParseInput (parseInput)
 main :: IO ()
 main = do
   putStrLn "The total distance between our lists is:"
-  input <- readFile "test_input.dat"
+  input <- readFile "test_input.dat" -- Change to "challenge_input.dat"
   let (listL, listR) = parseInput input
   let sortedL = tensort listL
   let sortedR = tensort listR
