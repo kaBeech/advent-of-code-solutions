9453d20701cb4147808d2d8d8620744b62de1642
kaBeech <contact@kaBeech.com>
2024-12-03 13:01:46 -0800

    Solve Part 1

--- 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
+++ 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
@@ -6,9 +6,23 @@ import Text.Regex.TDFA.Text ()
 main :: IO ()
 main = do
   programMemory <- readFile "test.dat"
+  -- programMemory <- readFile "challenge.dat"
   print "Part 1: Adding the answers together yields: "
-  print programMemory
   -- This may match "mtl(X,Y)" and "mvl(X,Y)" however I don't have those
   --    patterns in my programMemory. This may be solved with a second regex.
   let matches = getAllTextMatches (programMemory =~ "m[t-v]l[(][0-9]{1,3},[0-9]{1,3}[)]") :: [String]
-  print matches
+  let total = calcProds matches
+  print total
+
+calcProds :: [String] -> Int
+calcProds = acc 0
+  where
+    acc :: Int -> [String] -> Int
+    acc total [] = total
+    acc total (inst : insts) = acc (total + calcProd inst) insts
+
+calcProd :: String -> Int
+calcProd inst = do
+  let x = read (inst =~ "[0-9]{1,3}" :: String) :: Int
+  let y = read (drop 1 (inst =~ ",[0-9]{1,3}" :: String)) :: Int
+  x * y
