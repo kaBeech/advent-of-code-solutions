968c2337deebad95c5be2e21493bdeaadc31c3b1
kaBeech <contact@kaBeech.com>
2024-12-03 12:08:44 -0800

    Get regex working without modules

--- 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
+++ 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
@@ -1,5 +1,12 @@
+-- To make this work with no modvles,
+-- do `cabal install --lib regex-tdfa --package-env .` in this directory
+import Text.Regex.TDFA
+import Text.Regex.TDFA.Text ()
+
 main :: IO ()
 main = do
   programMemory <- readFile "test.dat"
   print "Part 1: Adding the answers together yields: "
   print programMemory
+  let matches = getAllTextMatches (programMemory =~ "[a-z]+") :: [String]
+  print matches
