dd2f80d7141f629ad08e7506c74a6c96e071da0d
kaBeech <contact@kaBeech.com>
2024-12-03 12:48:02 -0800

    Clarify comment

--- 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
+++ 2024-XXC-Part-2/Day03-No-Letter-U/solve.hs
@@ -8,7 +8,7 @@ main = do
   programMemory <- readFile "test.dat"
   print "Part 1: Adding the answers together yields: "
   print programMemory
-  -- This may match "mtl..." and "mvl..." however I don't have those patterns in
-  --    my programMemory. This may be solved with a second regex.
+  -- This may match "mtl(X,Y)" and "mvl(X,Y)" however I don't have those
+  --    patterns in my programMemory. This may be solved with a second regex.
   let matches = getAllTextMatches (programMemory =~ "m[t-v]l[(][0-9]{1,3},[0-9]{1,3}[)]") :: [String]
   print matches
