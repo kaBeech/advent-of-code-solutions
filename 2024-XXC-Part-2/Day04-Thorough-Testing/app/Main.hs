ca83bec3887cf7bcbf38abd84e99763718d157b6
kaBeech <contact@kaBeech.com>
2024-12-04 11:38:07 -0800

    Solve Part 1

--- 2024-XXC-Part-2/Day04-Thorough-Testing/app/Main.hs
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/app/Main.hs
@@ -4,6 +4,6 @@ import Solve (solvePart1)
 
 main :: IO ()
 main = do
-  -- input <- read "challenge_input.dat"
-  input <- readFile "test_input.dat"
+  input <- readFile "challenge_input.dat"
+  -- input <- readFile "test_input.dat"
   putStrLn ("Part 1: \"XMAS\" appears this many times: " ++ show (solvePart1 input))
