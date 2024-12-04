5c0ba3f08140d0b917b757ba5e2c4f82db7722d0
kaBeech <contact@kaBeech.com>
2024-12-04 14:19:34 -0800

    Check that the solution for Part 1 is correct

--- 2024-XXC-Part-2/Day04-Thorough-Testing/test/Main.hs
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/test/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import Solve (solvePart1)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  input <- readFile "test_input.dat"
+  let t1 = testPart1Solution input
+   in if t1 then putStrLn "All tests passed!" else putStrLn "Some tests failed!"
+
+testPart1Solution :: String -> Bool
+testPart1Solution input =
+  let result = solvePart1 input
+   in ( (result == 18)
+          || error ("Testing solvePart1 failed: Expected 18, but got " ++ show result)
+      )
