632e431fb7c4f4d8c7e116721e4c47edf07ae574
kaBeech <contact@kaBeech.com>
2024-12-04 11:25:05 -0800

    Parse input

--- 2024-XXC-Part-2/Day04-Thorough-Testing/src/Parse.hs
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/src/Parse.hs
@@ -8,4 +8,10 @@ import Types (WordMap)
 --   >>> parseInput "XMA\nSXM\nASX"
 --   [[('X',(0,0)),('M',(1,0)),('A',(2,0))],[('S',(0,1)),('X',(1,1)),('M',(2,1))],[('A',(0,2)),('S',(1,2)),('X',(2,2))]]
 parseInput :: String -> WordMap
-parseInput _inputPath = [[('X', (0, 0)), ('M', (1, 0)), ('A', (2, 0))], [('S', (0, 1)), ('X', (1, 1)), ('M', (2, 1))], [('A', (0, 2)), ('S', (1, 2)), ('X', (2, 2))]]
+parseInput input =
+  zipWith
+    ( \y row ->
+        zipWith (\x letter -> (letter, (x, y))) [0 ..] row
+    )
+    [0 ..]
+    (lines input)
