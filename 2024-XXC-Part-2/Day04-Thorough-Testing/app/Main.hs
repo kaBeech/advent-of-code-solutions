584ed08c09a5b774595f2ce0b9f7331ab54456dc
kaBeech <contact@kaBeech.com>
2024-12-03 23:23:00 -0800

    Prep Day 4

---
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Solve (solvePart1)
+
+main :: IO ()
+main = do
+  putStrLn ("Part 1: \"XMAS\" appears this many times: " ++ show solvePart1)
---
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/src/Solve.hs
@@ -0,0 +1,4 @@
+module Solve (solvePart1) where
+
+solvePart1 :: Int
+solvePart1 = 24
