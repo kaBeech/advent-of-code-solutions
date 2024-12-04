86c6ce2b6490bd4952e7633870d5be5027b6613a
kaBeech <contact@kaBeech.com>
2024-12-04 00:41:58 -0800

    Scaffold solvePart1

--- 2024-XXC-Part-2/Day04-Thorough-Testing/app/Main.hs
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/app/Main.hs
@@ -4,4 +4,6 @@ import Solve (solvePart1)
 
 main :: IO ()
 main = do
-  putStrLn ("Part 1: \"XMAS\" appears this many times: " ++ show solvePart1)
+  -- input <- read "challenge_input.dat"
+  input <- readFile "test_input.dat"
+  putStrLn ("Part 1: \"XMAS\" appears this many times: " ++ show (solvePart1 input))
--- 2024-XXC-Part-2/Day04-Thorough-Testing/src/Solve.hs
+++ 2024-XXC-Part-2/Day04-Thorough-Testing/src/Solve.hs
@@ -1,4 +1,28 @@
 module Solve (solvePart1) where
 
-solvePart1 :: Int
-solvePart1 = 24
+import Parse (parseInput)
+import Search (countWords)
+
+-- | Takes a string representation of a WordMap (including newLines) and
+--   returns the number of times the word "XMAS" appears in it, Word Search
+--   style.
+
+-- | ==== __Examples__
+--   >>> solvePart1 "XMAS\nMMMM\nAMAM\nSMMS"
+--   3
+--
+--   >>> solvePart1 "SMMS\nMAMA\nMMMM\nSAMX"
+--   3
+--
+--   >>> solvePart1 "MMMMM\nMMMSX\nMMAMM\nMMAMM\nXSMMM"
+--   2
+--
+--   >>> solvePart1 "XAMAASXMMASX"
+--   0
+--
+--   >>> solvePart1 "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX"
+--   18
+solvePart1 :: String -> Int
+solvePart1 input =
+  let wordMap = parseInput input
+   in countWords "XMAS" wordMap
