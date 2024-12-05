bfada9a63e7d1aca5b7a3949b338da72e8707741
kaBeech <contact@kaBeech.com>
2024-12-04 23:48:06 -0800

    Add mkDict

--- 2024-XXC-Part-2/Day05-No-Strings/src/Rule.hs
+++ 2024-XXC-Part-2/Day05-No-Strings/src/Rule.hs
@@ -1,3 +1,10 @@
 module Rule (mkDict) where
 
 import Types (Rule, RulesDict)
+
+mkDict :: [Rule] -> RulesDict
+mkDict = acc []
+  where
+    acc :: RulesDict -> [Rule] -> RulesDict
+    acc dict [] = dict
+    acc dict ((before, after) : rules) = acc (insertRule dict before after) rules
