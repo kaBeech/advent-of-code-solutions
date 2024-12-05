7b9c82a29a082992309001a18cbb3eefa510d0f9
kaBeech <contact@kaBeech.com>
2024-12-05 00:18:09 -0800

    Add getOrderedUpdates

--- 2024-XXC-Part-2/Day05-No-Strings/src/Update.hs
+++ 2024-XXC-Part-2/Day05-No-Strings/src/Update.hs
@@ -1,1 +1,12 @@
 module Update (getOrderedUpdates, sumMiddlePages) where
+
+import Types (RulesDict, Update)
+
+getOrderedUpdates :: RulesDict -> [Update] -> [Update]
+getOrderedUpdates rulesDict = filter (checkIfUpdateIsOrdered rulesDict)
+
+checkIfUpdateIsOrdered :: RulesDict -> Update -> Bool
+checkIfUpdateIsOrdered _ [] = True
+checkIfUpdateIsOrdered rulesDict (x : xs) = case lookup x rulesDict of
+  Nothing -> checkIfUpdateIsOrdered rulesDict xs
+  Just befores -> not (any (`elem` befores) xs) && checkIfUpdateIsOrdered rulesDict xs
