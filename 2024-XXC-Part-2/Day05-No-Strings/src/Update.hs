1b2db4a493d96ff2c2c9b20e8df79f6f44a4527a
kaBeech <contact@kaBeech.com>
2024-12-05 00:24:06 -0800

    Add sumMiddlePages

--- 2024-XXC-Part-2/Day05-No-Strings/src/Update.hs
+++ 2024-XXC-Part-2/Day05-No-Strings/src/Update.hs
@@ -10,3 +10,9 @@ checkIfUpdateIsOrdered _ [] = True
 checkIfUpdateIsOrdered rulesDict (x : xs) = case lookup x rulesDict of
   Nothing -> checkIfUpdateIsOrdered rulesDict xs
   Just befores -> not (any (`elem` befores) xs) && checkIfUpdateIsOrdered rulesDict xs
+
+sumMiddlePages :: [Update] -> Int
+sumMiddlePages = sum . map getMiddlePage
+
+getMiddlePage :: Update -> Int
+getMiddlePage update = update !! (length update `div` 2)
