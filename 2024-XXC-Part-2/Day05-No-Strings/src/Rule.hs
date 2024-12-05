fa00b1dceb1b97c0fd91109e8cec3faa005accb1
kaBeech <contact@kaBeech.com>
2024-12-04 23:50:35 -0800

    Add insertRule

--- 2024-XXC-Part-2/Day05-No-Strings/src/Rule.hs
+++ 2024-XXC-Part-2/Day05-No-Strings/src/Rule.hs
@@ -8,3 +8,8 @@ mkDict = acc []
     acc :: RulesDict -> [Rule] -> RulesDict
     acc dict [] = dict
     acc dict ((before, after) : rules) = acc (insertRule dict before after) rules
+
+insertRule :: RulesDict -> Int -> Int -> RulesDict
+insertRule dict before after = case lookup after dict of
+  Nothing -> (after, [before]) : dict
+  Just befores -> (after, before : befores) : filter ((/= after) . fst) dict
