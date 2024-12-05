47e5f22e3e56648892ed493c7c03704b27a684a8
kaBeech <contact@kaBeech.com>
2024-12-05 01:42:15 -0800

    Solve Part 1

--- 2024-XXC-Part-2/Day05-No-Strings/app/Main.hs
+++ 2024-XXC-Part-2/Day05-No-Strings/app/Main.hs
@@ -4,6 +4,6 @@ import Solve (solvePart1)
 
 main :: IO ()
 main = do
-  input <- readFile "challenge_input.dat"
-  -- input <- readFile "test_input.dat"
+  -- input <- readFile "challenge_input.dat"
+  input <- readFile "test_input.dat"
   putStrLn ("Part 1: What do you get if you add up the middle page number from those correctly-ordered updates? " ++ show (solvePart1 input))
