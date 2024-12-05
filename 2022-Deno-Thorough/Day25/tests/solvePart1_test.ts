de5eee6ef353555f4ab72faa991d159a2da25628
kaBeech <contact@kaBeech.com>
2024-12-04 21:52:42 -0800

    Solve Part 1

--- 2022-Deno-Thorough/Day25/tests/solvePart1_test.ts
+++ 2022-Deno-Thorough/Day25/tests/solvePart1_test.ts
@@ -2,8 +2,7 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { solvePart1 } from "../solvePart1.ts";
 
-const testInput = "./tests/testInput.txt";
-const challengeInput = "./tests/challengeInput.txt";
+const testInput = "./tests/test_input.dat";
 
 Deno.test("testInput returns 2=-1=0", async () => {
     const result = await solvePart1(testInput);
