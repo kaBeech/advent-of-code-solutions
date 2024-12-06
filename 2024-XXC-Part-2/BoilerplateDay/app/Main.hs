80d01c65241122fa6211c8cca41ef252976d6906
kaBeech <contact@kaBeech.com>
2024-12-06 13:10:20 -0800

    Update printout

--- 2024-XXC-Part-2/BoilerplateDay/app/Main.hs
+++ 2024-XXC-Part-2/BoilerplateDay/app/Main.hs
@@ -6,4 +6,4 @@ main :: IO ()
 main = do
   -- input <- readFile "challenge_input.dat"
   input <- readFile "test_input.dat"
-  putStrLn ("Part 1: What is the answer for Part 1? It is: " ++ show (solvePart1 input))
+  putStrLn ("Part 1: What is the answer for Part 1? Answer: " ++ show (solvePart1 input))
