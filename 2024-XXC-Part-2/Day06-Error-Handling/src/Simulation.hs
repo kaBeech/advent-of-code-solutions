9127987f577e953dddfe0ac4a4480b1151157360
kaBeech <contact@kaBeech.com>
2024-12-06 15:03:44 -0800

    Scaffold solvePart1

---
+++ 2024-XXC-Part-2/Day06-Error-Handling/src/Simulation.hs
@@ -0,0 +1,6 @@
+module Simulation (runSimulation) where
+
+import Types (AreaMap, Simulation, XYCoord)
+
+runSimulation :: Simulation -> Simulation
+runSimulation simulation = simulation
--- 2024-XXC-Part-2/Day06-Error-Handling/src/Solve.hs
+++ 2024-XXC-Part-2/Day06-Error-Handling/src/Solve.hs
@@ -1,25 +1,17 @@
 module Solve (solvePart1) where
 
 import Parse (parseInput)
-import Types (CharMap)
+import Simulation (runSimulation)
 
--- | Takes a string representation of a CharMap (including newLines) and
---   returns the area of the map.
+-- | Takes a string representation of an AreaMap (including newLines) and
+--   returns the number of positions visited by the guard.
 
 -- | ==== __Examples__
---   >>> solvePart1 "HEL\nLOW\nORL\nD!!"
---   12
---
---   >>> solvePart1 "XMAS\nMMMM\nAMAM\nSMMS"
---   16
+--   >>> solvePart1 ".#.\n.^.\n#.#"
+--   2
 solvePart1 :: String -> Int
 solvePart1 input =
-  let wordMap = parseInput input
-   in getArea wordMap
-
-getArea :: CharMap -> Int
-getArea wordMap =
-  let xWidth = length (head wordMap)
-      yHeight = length wordMap
-      area = xWidth * yHeight
-   in area
+  let simulationInit = parseInput input
+      simulationFinal = runSimulation simulationInit
+      (visitedCount, _, _, _) = simulationFinal
+   in visitedCount
