9c1dd5c78467ab759bca664179e3200b95d2ed89
kaBeech <contact@kaBeech.com>
2024-12-06 15:20:48 -0800

    Scaffold runSimulation

--- 2024-XXC-Part-2/Day06-Error-Handling/src/Simulation.hs
+++ 2024-XXC-Part-2/Day06-Error-Handling/src/Simulation.hs
@@ -3,4 +3,13 @@ module Simulation (runSimulation) where
 import Types (AreaMap, Simulation, XYCoord)
 
 runSimulation :: Simulation -> Simulation
-runSimulation simulation = simulation
+runSimulation sim =
+  let (visitedCount, currentPos, currentDir, areaMap) = sim
+      simFinished = not (isInBounds currentPos areaMap)
+      (areaMap', newlyVisitedTiles) = visitTile currentPos areaMap
+      visitedCount' = visitedCount + newlyVisitedTiles
+      currentDir' = findSafeStep currentPos currentDir areaMap'
+      currentPos' = step currentPos currentDir'
+   in if simFinished
+        then sim
+        else runSimulation (visitedCount', currentPos', currentDir', areaMap')
