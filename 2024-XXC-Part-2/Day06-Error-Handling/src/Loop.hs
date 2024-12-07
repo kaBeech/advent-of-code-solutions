87de15059ed3301c244a4d103f86059a474a0cf6
kaBeech <contact@kaBeech.com>
2024-12-07 14:04:35 -0800

    Start loop sims from startPos, not from the new block

--- 2024-XXC-Part-2/Day06-Error-Handling/src/Loop.hs
+++ 2024-XXC-Part-2/Day06-Error-Handling/src/Loop.hs
@@ -8,12 +8,12 @@ countLoopOpps = acc 0
   where
     acc :: Int -> [Tile] -> Simulation -> Int
     acc count [] _ = count
-    acc count ((_, _, _, (x, y)) : tiles) sim@(_, _, _, areaMap) =
+    acc count ((_, _, _, (x, y)) : tiles) sim@(_, startPos, _, areaMap) =
       let _tileToBlock@(_, visited, _, _) = areaMap !! y !! x
           blockedTile = (False, False, [], (x, y))
           areaMap' = take y areaMap ++ [take x (areaMap !! y) ++ [blockedTile] ++ drop (x + 1) (areaMap !! y)] ++ drop (y + 1) areaMap
           isStartingTile = visited
-          sim' = (False, (x, y), 3, areaMap')
+          sim' = (False, startPos, 3, areaMap')
           (simulationLoops, _, _, _) = runSimulation sim'
           count' = if not isStartingTile && simulationLoops then count + 1 else count
        in acc count' tiles sim
