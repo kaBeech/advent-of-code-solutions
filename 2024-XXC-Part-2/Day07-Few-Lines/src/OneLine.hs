41f2e84999d4b4db619148016b77774ebee8de74
kaBeech <contact@kaBeech.com>
2024-12-07 19:30:46 -0800

    Move other functions into equationIsSolvable

--- 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
@@ -11,14 +11,13 @@ solvePart input validOperators =
    in sum (map fst solvableEquations)
 
 equationIsSolvable validOperators (result, operands) =
-  let operators = genOperators validOperators operands
+  let operators = genOperators (length operands - 2)
+      genOperators n =
+        if n == 0
+          then [[op] | op <- validOperators]
+          else do
+            rest <- genOperators (n - 1)
+            operator <- validOperators
+            return (operator : rest)
       tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
    in any (tryEquation 0 (result : operands)) operators
-
-genOperators validOperators operands = acc (length operands - 2)
-  where
-    acc 0 = [[op] | op <- validOperators]
-    acc n = do
-      rest <- acc (n - 1)
-      operator <- validOperators
-      return (operator : rest)
