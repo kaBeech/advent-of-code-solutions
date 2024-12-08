0535cb5f5ed284b12001f718451e485dd1014ae7
kaBeech <contact@kaBeech.com>
2024-12-07 20:17:36 -0800

    Make sure challenge input still works

--- 2024-XXC-Part-2/Day07-Few-Lines/app/Main.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/app/Main.hs
@@ -4,7 +4,7 @@ import Solve (solve)
 
 main :: IO ()
 main = do
-  -- input <- readFile "challenge_input.dat"
-  input <- readFile "test_input.dat"
+  input <- readFile "challenge_input.dat"
+  -- input <- readFile "test_input.dat"
   putStrLn ("Part 1: What is their total calibration result? Answer: " ++ show (fst (solve input)))
   putStrLn ("Part 2: What is their total calibration result? Answer: " ++ show (snd (solve input)))
