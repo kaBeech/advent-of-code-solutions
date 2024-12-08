63af20385903580a86396850f3177729a638169b
kaBeech <contact@kaBeech.com>
2024-12-07 19:44:29 -0800

    Move genOperators back into tryEquation

--- 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
@@ -10,9 +10,5 @@ solvePart input = sum (map fst (filter (equationIsSolvable) (map (\line -> (read
 
 equationIsSolvable (result, operands) =
   let tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
+      genOperators n = if n == 0 then [[op] | op <- "*+|"] else [(operator : rest) | rest <- genOperators (n - 1), operator <- "*+|"]
    in any (tryEquation 0 (result : operands)) (genOperators (length operands - 2))
-
-genOperators n =
-  if n == 0
-    then [[op] | op <- "*+|"]
-    else [(operator : rest) | rest <- genOperators (n - 1), operator <- "*+|"]
