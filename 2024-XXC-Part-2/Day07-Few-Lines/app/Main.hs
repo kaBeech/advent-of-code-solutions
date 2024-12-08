c8bf016d7577b85c9c8082bdda8eba3f96a05db7
kaBeech <contact@kaBeech.com>
2024-12-07 16:18:13 -0800

    Solve Part 1

--- 2024-XXC-Part-2/Day07-Few-Lines/app/Main.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/app/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import Solve (solvePart1)
+import Solve (solve)
 
 main :: IO ()
 main = do
-  -- input <- readFile "challenge_input.dat"
-  input <- readFile "test_input.dat"
-  putStrLn ("Part 1: What is their total calibration result? Answer: " ++ show (solvePart1 input))
+  input <- readFile "challenge_input.dat"
+  -- input <- readFile "test_input.dat"
+  putStrLn ("Part 1: What is their total calibration result? Answer: " ++ show (solve input))
