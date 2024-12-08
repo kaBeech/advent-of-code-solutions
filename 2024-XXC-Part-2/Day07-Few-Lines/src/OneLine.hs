ec3ded3d7d16bc0590a826ed39d40f6ac811dc01
kaBeech <contact@kaBeech.com>
2024-12-07 19:37:30 -0800

    Solve only Part 2 for now

--- 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
@@ -3,18 +3,20 @@ module OneLine
   )
 where
 
-solveParts input = (solvePart input "*+", solvePart input "*+|")
+-- solveParts input = (solvePart input "*+", solvePart input "*+|")
+solveParts input = (solvePart input, solvePart input)
 
-solvePart input validOperators = sum (map fst (filter (equationIsSolvable validOperators) (map (\line -> (read (head (words (map (\c -> if c == ':' then ' ' else c) line))), map read (tail (words (map (\c -> if c == ':' then ' ' else c) line))))) (lines input))))
+solvePart input = sum (map fst (filter (equationIsSolvable) (map (\line -> (read (head (words (map (\c -> if c == ':' then ' ' else c) line))), map read (tail (words (map (\c -> if c == ':' then ' ' else c) line))))) (lines input))))
 
-equationIsSolvable validOperators (result, operands) =
+equationIsSolvable (result, operands) =
   let operators = genOperators (length operands - 2)
-      genOperators n =
-        if n == 0
-          then [[op] | op <- validOperators]
-          else do
-            rest <- genOperators (n - 1)
-            operator <- validOperators
-            return (operator : rest)
       tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
    in any (tryEquation 0 (result : operands)) operators
+
+genOperators n =
+  if n == 0
+    then [[op] | op <- "*+|"]
+    else do
+      rest <- genOperators (n - 1)
+      operator <- "*+|"
+      return (operator : rest)
