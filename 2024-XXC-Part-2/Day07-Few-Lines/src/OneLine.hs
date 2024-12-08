8b9a543dbb26ad55acb461f549f862a4b0309e8b
kaBeech <contact@kaBeech.com>
2024-12-07 20:09:33 -0800

    Inline equationIsSolvable

--- 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
@@ -7,7 +7,6 @@ where
 solveParts input = (solvePart input, solvePart input)
 
 solvePart input =
-  let equationIsSolvable (result, operands) = any (tryEquation 0 (result : operands)) (genOperators (length operands - 2))
-      genOperators n = if n == 0 then [[op] | op <- "*+|"] else [(operator : rest) | rest <- genOperators (n - 1), operator <- "*+|"]
+  let genOperators n = if n == 0 then [[op] | op <- "*+|"] else [(operator : rest) | rest <- genOperators (n - 1), operator <- "*+|"]
       tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
-   in sum (map fst (filter (equationIsSolvable) (map (\line -> (read (head (words (map (\c -> if c == ':' then ' ' else c) line))), map read (tail (words (map (\c -> if c == ':' then ' ' else c) line))))) (lines input))))
+   in sum (map fst (filter ((\equation -> any (tryEquation 0 (uncurry (:) equation)) (genOperators (length (snd equation) - 2)))) (map (\line -> (read (head (words (map (\c -> if c == ':' then ' ' else c) line))), map read (tail (words (map (\c -> if c == ':' then ' ' else c) line))))) (lines input))))
