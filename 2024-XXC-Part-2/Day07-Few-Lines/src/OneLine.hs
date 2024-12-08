ac60b4dcb18b4392398cc9ff8afa30778a58474a
kaBeech <contact@kaBeech.com>
2024-12-07 19:41:45 -0800

    Remove do notation

--- 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
+++ 2024-XXC-Part-2/Day07-Few-Lines/src/OneLine.hs
@@ -9,14 +9,10 @@ solveParts input = (solvePart input, solvePart input)
 solvePart input = sum (map fst (filter (equationIsSolvable) (map (\line -> (read (head (words (map (\c -> if c == ':' then ' ' else c) line))), map read (tail (words (map (\c -> if c == ':' then ' ' else c) line))))) (lines input))))
 
 equationIsSolvable (result, operands) =
-  let operators = genOperators (length operands - 2)
-      tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
-   in any (tryEquation 0 (result : operands)) operators
+  let tryEquation total equation operators = if total == 0 then tryEquation (head (tail equation)) ((head equation) : (tail (tail equation))) operators else if null (tail equation) then total == (head equation) else (total <= (head equation)) && (if (head operators) == '|' then tryEquation (read (show total ++ show (head (tail equation)))) ((head equation) : (tail (tail equation))) (tail operators) else if (head operators) == '+' then tryEquation (total + (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators) else tryEquation (total * (head (tail equation))) ((head equation) : (tail (tail equation))) (tail operators))
+   in any (tryEquation 0 (result : operands)) (genOperators (length operands - 2))
 
 genOperators n =
   if n == 0
     then [[op] | op <- "*+|"]
-    else do
-      rest <- genOperators (n - 1)
-      operator <- "*+|"
-      return (operator : rest)
+    else [(operator : rest) | rest <- genOperators (n - 1), operator <- "*+|"]
