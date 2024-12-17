caf70d942e68d4ade08e2d065605a99ed5a6a716
kaBeech <contact@kaBeech.com>
2024-12-17 15:10:42 -0800

    Remove '.' characters from the AntennaMap

--- 2024-XXC-Part-2/Day08-Prolog/solve.pl
+++ 2024-XXC-Part-2/Day08-Prolog/solve.pl
@@ -2,8 +2,8 @@
 
 % Solve Part 1
 solve :-
-    read_file('test_input.dat', CharList),
-    writeln(CharList).  % Display the result for verification
+    read_file('test_input.dat', AntennaMap),
+    writeln(AntennaMap).
 
 % Read file and convert to list of char(Char, X, Y)
 % For example, an input file containing this data:
@@ -13,26 +13,31 @@ solve :-
 % ....
 %
 % ..will output this AntennaMap like this:
-% [char(.,0,0),char(.,1,0),char(.,2,0),char(0,3,0),char(A,0,1),char(.,1,1),char(0,2,1),char(.,3,1),char(.,0,2),char(.,1,2),char(A,2,2),char(A,3,2),char(.,0,3),char(.,1,3),char(.,2,3),char(.,3,3)]
-read_file(Filename, CharList) :-
+% [char(0,8,1),char(0,5,2),char(0,7,3),char(0,4,4),char(A,6,5),char(A,8,8),char(A,9,9)]
+read_file(Filename, AntennaMap) :-
     open(Filename, read, Stream),
-    read_lines(Stream, 0, CharList),
+    read_lines(Stream, 0, AntennaMap),
     close(Stream).
 
 % Read lines until end of file, accumulating characters with coordinates
-read_lines(Stream, Y, CharList) :-
+read_lines(Stream, Y, AntennaMap) :-
     read_line_to_string(Stream, Line),
     (Line \= end_of_file
         -> (string_chars(Line, Chars),
             process_line(Chars, 0, Y, ThisLineChars),
             Y1 is Y + 1,
             read_lines(Stream, Y1, RestChars),
-            append(ThisLineChars, RestChars, CharList))
-        ; CharList = []).
+            append(ThisLineChars, RestChars, AntennaMap))
+        ; AntennaMap = []).
 
-% Process each character in a line, recording its coordinates
+% Process each character in a line (except '.' characters), 
+% recording its coordinates
 process_line([], _, _, []).
+process_line(['.'|Rest], X, Y, RestChars) :-  
+    X1 is X + 1,
+    process_line(Rest, X1, Y, RestChars).
 process_line([Char|Rest], X, Y, [char(Char, X, Y)|RestChars]) :-
+    Char \= '.',
     X1 is X + 1,
     process_line(Rest, X1, Y, RestChars).
 
