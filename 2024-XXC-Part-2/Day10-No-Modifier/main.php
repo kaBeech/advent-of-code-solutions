0f1998eda6435ced9aac5180f155d55d7d8a84d0
kaBeech <contact@kaBeech.com>
2024-12-19 15:57:39 -0800

    Correct parsing

--- 2024-XXC-Part-2/Day10-No-Modifier/main.php
+++ 2024-XXC-Part-2/Day10-No-Modifier/main.php
@@ -6,9 +6,9 @@ include 'src/scoreMap.php';
 include 'src/scoreTrail.php';
 
 echo 'Part 1: What is the sum of the scores of all trailheads on your topographic map? Answer: ' .
-    parseInput(
-        // file_get_contents('challenge_input.dat')
         scoreMap(
-            file_get_contents('test_input.dat')
-        )
+            parseInput(
+                // file_get_contents('challenge_input.dat')
+                    file_get_contents('test_input.dat')
+            )
     ) . PHP_EOL;
