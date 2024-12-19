ed24c3d491e9a3c0d5b418c01636b88eac308bf0
kaBeech <contact@kaBeech.com>
2024-12-18 21:16:09 -0800

    Add getNodes

--- 2024-XXC-Part-2/Day08-Prolog-and-TypeScript/part2/getUniqueNodes.ts
+++ 2024-XXC-Part-2/Day08-Prolog-and-TypeScript/part2/getUniqueNodes.ts
@@ -1,19 +1,35 @@
-import { AntennaMap, NodeDict } from "./types.ts";
+import { Antenna, AntennaMap, NodeDict } from "./types.ts";
 
 export default (
   antennaMap: AntennaMap,
 ): number[] => {
-  const nodesDict: NodeDict = {};
+  let nodeDict: NodeDict = {};
+  let freqsScanned: string[] = [];
   antennaMap.antennas.forEach((antenna) => {
-    if (!nodesDict[antenna.coordinates.x.toString()]) {
-      nodesDict[antenna.coordinates.x.toString()] = [antenna.coordinates.y];
-    } else {
-      nodesDict[antenna.coordinates.x.toString()].push(antenna.coordinates.y);
-    }
+    [nodeDict, freqsScanned] = getNodes(antenna, antennaMap, nodeDict, freqsScanned);
   });
   let nodes: number[] = [];
-  for (const key in nodesDict) {
-    nodes = nodes.concat(nodesDict[key]);
+  for (const key in nodeDict) {
+    nodes = nodes.concat(nodeDict[key]);
   }
   return nodes;
 };
+
+const getNodes = (
+  antenna: Antenna,
+  antennaMap: AntennaMap,
+  nodeDict: NodeDict,
+  freqsScanned: string[]
+): [NodeDict, string[]] => {
+  if (freqsScanned.includes(antenna.frequency)) {
+    return [nodeDict, freqsScanned];
+  }
+  const matchingAntennas = antennaMap.antennas.filter((a) =>
+    a.frequency === antenna.frequency &&
+    !(a.coordinates.x === antenna.coordinates.x &&
+      a.coordinates.y === antenna.coordinates.y));
+  matchingAntennas.forEach((matchingAntenna) => {
+    nodeDict = getNodesFromAntennaPair(antenna, matchingAntenna, nodeDict);
+  });
+  return [nodeDict, freqsScanned];
+}
