730cd34d63d95440d74eb8fc5fcf976dc90ba3ed
kaBeech <contact@kaBeech.com>
2024-12-19 01:29:26 -0800

    Fix disk swap errors

--- 2024-XXC-Part-2/Day09-PHP-Style/solve.php
+++ 2024-XXC-Part-2/Day09-PHP-Style/solve.php
@@ -133,11 +133,12 @@ function findFreeSpace(array $disk, int $fileStart, int $fileSize): int
 
 function swap(array $disk, int $fileStart, int $fileSize, int $freeSpaceStart): array
 {
-    $left = array_slice($disk, 0, $fileStart);
+    $left = array_slice($disk, 0, $freeSpaceStart);
     $swap1 = array_slice($disk, $freeSpaceStart, $fileSize);
-    $middleStart = $fileStart + $fileSize;
-    $middle = array_slice($disk, $middleStart, $freeSpaceStart - $middleStart);
+    $middleStart = $freeSpaceStart + $fileSize;
+    $middle = array_slice($disk, $middleStart, $fileStart - $middleStart);
     $swap2 = array_slice($disk, $fileStart, $fileSize);
-    $right = array_slice($disk, $freeSpaceStart + $fileSize);
-    return array_merge($left, $swap2, $middle, $swap1, $right);
+    $right = array_slice($disk, $fileStart + $fileSize);
+    $swappedDisk = array_merge($left, $swap2, $middle, $swap1, $right);
+    return $swappedDisk;
 }
