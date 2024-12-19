af906fd7079d1e1ac48318a787a0eadaf40e66e9
kaBeech <contact@kaBeech.com>
2024-12-18 19:03:33 -0800

    Get nodes

--- 2024-XXC-Part-2/Day08-Prolog/solve.pl
+++ 2024-XXC-Part-2/Day08-Prolog/solve.pl
@@ -3,7 +3,8 @@
 % Solve Part 1
 solve :-
     read_file('test_input.dat', AntennaMap),
-    writeln(AntennaMap).
+    get_nodes(AntennaMap, Nodes),
+    writeln(Nodes).
 
 % Read file and convert to list of char(Char, X, Y)
 % For example, an input file containing this data:
@@ -41,3 +42,47 @@ process_line([Char|Rest], X, Y, [char(Char, X, Y)|RestChars]) :-
     X1 is X + 1,
     process_line(Rest, X1, Y, RestChars).
 
+% Get nodes from antenna pairs
+get_nodes(AntennaMap, Nodes) :-
+    group_by_frequency(AntennaMap, GroupedAntennas),
+    findall(Node, 
+        (member(FreqGroup, GroupedAntennas),
+         antenna_pair_to_nodes(FreqGroup, Node)),
+        Nodes).
+
+% Group antennas by their frequency
+group_by_frequency([], []).
+group_by_frequency([char(Freq,X,Y)|Rest], GroupedResult) :-
+    group_by_frequency(Rest, GroupedRest),
+    add_to_frequency_group(char(Freq,X,Y), GroupedRest, GroupedResult).
+
+% Add antenna to its frequency group
+add_to_frequency_group(char(Freq,X,Y), [], [[char(Freq,X,Y)]]).
+add_to_frequency_group(char(Freq,X,Y), [Group|Rest], [NewGroup|Rest]) :-
+    Group = [char(Freq,_,_)|_],
+    append(Group, [char(Freq,X,Y)], NewGroup).
+add_to_frequency_group(char(Freq,X,Y), [Group|Rest], [Group|NewRest]) :-
+    Group = [char(OtherFreq,_,_)|_],
+    Freq \= OtherFreq,
+    add_to_frequency_group(char(Freq,X,Y), Rest, NewRest).
+
+% Generate nodes from a group of same-frequency antennas
+antenna_pair_to_nodes(Group, node(NodeX, NodeY)) :-
+    length(Group, Len),
+    Len > 1,
+    member(char(_,X1,Y1), Group),
+    member(char(_,X2,Y2), Group),
+    X1 @< X2,  % Ensure we don't process the same pair twice
+    (
+        % Calculate first node
+        DiffX is X2 - X1,
+        DiffY is Y2 - Y1,
+        NodeX is X2 + DiffX,
+        NodeY is Y2 + DiffY
+    ;
+        % Calculate second node
+        DiffX is X2 - X1,
+        DiffY is Y2 - Y1,
+        NodeX is X1 - DiffX,
+        NodeY is Y1 - DiffY
+    ).
