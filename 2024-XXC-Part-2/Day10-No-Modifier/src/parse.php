44efd7e9ede6e3a256964453f433a9302bec497f
kaBeech <contact@kaBeech.com>
2024-12-20 10:39:32 -0800

    Prep for memoization

--- 2024-XXC-Part-2/Day10-No-Modifier/src/parse.php
+++ 2024-XXC-Part-2/Day10-No-Modifier/src/parse.php
@@ -5,7 +5,11 @@ function parseInput(string $input): array
     $trailMap = [];
     $lines = explode("\n", $input);
     foreach ($lines as $line) {
-        $trailMap[] = str_split($line);
+        $chars = str_split($line);
+        foreach ($chars as $char) {
+            // [elevation, [trailheads reachable from this tile]]
+            $trailMap[] = [(int) $char, []];
+        }
     }
     return $trailMap;
 }
