09154c3b994f6edf228ef2b76b610c4cbcecb9e6
kaBeech <contact@kaBeech.com>
2025-05-24 19:17:05 -0700

    Update Part 2 display text

--- 2024-XXC-Part-2/Day11-No-Modifier/app/Main.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/app/Main.hs
@@ -8,5 +8,5 @@ main = do
   input <- readFile "data/challenge_input.dat"
   putStrLn "Part 1: How many stones will you have after blinking 25 times?"
   putStrLn $ solvePart1 input
-  putStrLn "Part 2: TBA"
+  putStrLn "Part 2: How many stones would you have after blinking a total of 75 times?"
   putStrLn $ solvePart2 input
