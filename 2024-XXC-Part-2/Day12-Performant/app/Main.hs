a081ee967241d8776513d9f94b6402a8af344ca9
kaBeech <contact@kaBeech.com>
2025-05-25 12:39:41 -0700

    Update display text

--- 2024-XXC-Part-2/Day12-Performant/app/Main.hs
+++ 2024-XXC-Part-2/Day12-Performant/app/Main.hs
@@ -7,5 +7,5 @@ main = do
   input <- readFile "data/challenge_input.dat"
   putStrLn "Part 1: What is the total price of fencing all regions on your map?"
   putStrLn $ solvePart1 input
-  putStrLn "Part 2: TBA"
+  putStrLn "Part 2: What is the new total price of fencing all regions on your map?"
   putStrLn $ solvePart2 input
