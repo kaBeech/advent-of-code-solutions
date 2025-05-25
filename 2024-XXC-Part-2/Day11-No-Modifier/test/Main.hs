a1cbba40a14af0b1558800d52a64c33c5ec783f6
kaBeech <contact@kaBeech.com>
2025-05-24 18:22:28 -0700

    Add Part 1 tests

--- 2024-XXC-Part-2/Day11-No-Modifier/test/Main.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/test/Main.hs
@@ -6,17 +6,9 @@ import System.Exit
 
 main :: IO ()
 main = do
-  input1 <- readFile "data/test_input_1.dat"
-  input2 <- readFile "data/test_input_2.dat"
-  input3 <- readFile "data/test_input_3.dat"
-  input4 <- readFile "data/test_input_4.dat"
-  input5 <- readFile "data/test_input_5.dat"
-  res1 <- test 1 input1 "1"
-  res2 <- test 2 input2 "2"
-  res3 <- test 3 input3 "4"
-  res4 <- test 4 input4 "3"
-  res5 <- test 5 input5 "36"
-  let allPassed = res1 && res2 && res3 && res4 && res5
+  input1 <- readFile "data/test_input.dat"
+  res1 <- test 1 input1 "55312"
+  let allPassed = res1
   unless allPassed exitFailure
 
 test :: Int -> String -> String -> IO Bool
