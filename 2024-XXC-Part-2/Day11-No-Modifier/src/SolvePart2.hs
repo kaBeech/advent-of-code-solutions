3a332ef671c6f425d184eded9ada179789551e45
kaBeech <contact@kaBeech.com>
2025-05-24 20:19:07 -0700

    Scaffold Part 2

--- 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart2.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart2.hs
@@ -1,4 +1,11 @@
 module SolvePart2 (solvePart2) where
 
-solvePart2 :: String -> String
-solvePart2 topographicalMap = topographicalMap
+import Blink (blinkV2)
+import Parse (parseInput)
+
+solvePart2 :: Int -> String -> String
+solvePart2 n input = result
+  where
+    result = show $ length stones'
+    stones' = blinkV2 n stones
+    stones = parseInput input
