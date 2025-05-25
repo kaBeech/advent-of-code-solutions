0bf591be0f04d932668e30d1bbf4e0c427d45003
kaBeech <contact@kaBeech.com>
2025-05-24 18:06:55 -0700

    Solve Part 2

--- 2024-XXC-Part-2/Day10-No-Modifier/haskell/src/SolvePart2.hs
+++ 2024-XXC-Part-2/Day10-No-Modifier/haskell/src/SolvePart2.hs
@@ -1,4 +1,13 @@
 module SolvePart2 (solvePart2) where
 
+import GetTrailheads (getTrailheads)
+import Parse (parseInputFlat)
+import ScoreTrailhead (getTrailheadRating)
+
 solvePart2 :: String -> String
-solvePart2 topographicalMap = topographicalMap
+solvePart2 topographicalMapRaw = show totalScore
+  where
+    totalScore = sum scores
+    scores = map (getTrailheadRating topoMap) trailheads
+    trailheads = getTrailheads topoMap
+    topoMap = parseInputFlat topographicalMapRaw
