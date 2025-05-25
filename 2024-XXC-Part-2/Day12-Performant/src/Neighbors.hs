c0ea2b9dc5b989f08040df02e2dccf7b1c564e15
kaBeech <contact@kaBeech.com>
2025-05-25 16:33:20 -0700

    Cleanup

--- 2024-XXC-Part-2/Day12-Performant/src/Neighbors.hs
+++ 2024-XXC-Part-2/Day12-Performant/src/Neighbors.hs
@@ -1,6 +1,5 @@
 module Neighbors
   ( getNeighbors,
-    isAdjacent,
     isAdjacentToAny,
     getNeighborsAll,
   )
@@ -11,14 +10,14 @@ import Types (GardenFlatMap, Tile)
 getNeighbors :: GardenFlatMap -> Tile -> [Tile]
 getNeighbors gardenMap tile = filter (isAdjacent tile) gardenMap
 
-getNeighborsAll :: GardenFlatMap -> Tile -> [Tile]
-getNeighborsAll gardenMap tile = filter (isNeighbor tile) gardenMap
-
-isNeighbor :: Tile -> Tile -> Bool
-isNeighbor (_, (x, y)) (_, (x', y')) = abs (x - x') <= 1 && abs (y - y') <= 1
-
 isAdjacent :: Tile -> Tile -> Bool
 isAdjacent (_, (x, y)) (_, (x', y')) = abs (x - x') + abs (y - y') == 1
 
 isAdjacentToAny :: [Tile] -> Tile -> Bool
 isAdjacentToAny tiles tile = any (isAdjacent tile) tiles
+
+getNeighborsAll :: GardenFlatMap -> Tile -> [Tile]
+getNeighborsAll gardenMap tile = filter (isNeighbor tile) gardenMap
+
+isNeighbor :: Tile -> Tile -> Bool
+isNeighbor (_, (x, y)) (_, (x', y')) = abs (x - x') <= 1 && abs (y - y') <= 1
