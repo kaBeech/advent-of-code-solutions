595e3298e5b41b2500c97145071a2868c0dfbdb3
kaBeech <contact@kaBeech.com>
2025-05-24 22:58:39 -0700

    Update display text

--- 2024-XXC-Part-2/Day12-Performant/app/Main.hs
+++ 2024-XXC-Part-2/Day12-Performant/app/Main.hs
@@ -6,7 +6,7 @@ import SolvePart2 (solvePart2)
 main :: IO ()
 main = do
   input <- readFile "data/challenge_input.dat"
-  putStrLn "Part 1: What is the sum of the scores of all trailheads on your topographic map?"
+  putStrLn "Part 1: What is the total price of fencing all regions on your map?"
   putStrLn $ solvePart1 input
   putStrLn "Part 2: TBA"
   putStrLn $ solvePart2 input
