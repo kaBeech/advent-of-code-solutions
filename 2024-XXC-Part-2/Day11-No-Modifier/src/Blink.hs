c72080feae46bbc6739998cdcd54027eea97b0b0
kaBeech <contact@kaBeech.com>
2025-05-24 20:21:40 -0700

    Try a DFS-style approach

--- 2024-XXC-Part-2/Day11-No-Modifier/src/Blink.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/src/Blink.hs
@@ -1,4 +1,4 @@
-module Blink (blink) where
+module Blink (blink, blinkV2) where
 
 blink :: [String] -> [String]
 blink = concatMap applyBlink
@@ -15,3 +15,22 @@ applyBlink stone
 
 simplify :: String -> String
 simplify s = show (read s :: Integer)
+
+blinkV2 :: Int -> [String] -> [String]
+blinkV2 n = concatMap (applyBlinkV2 n)
+
+applyBlinkV2 :: Int -> String -> [String]
+applyBlinkV2 n stone = applyBlinkV2' n [stone]
+
+applyBlinkV2' :: Int -> [String] -> [String]
+applyBlinkV2' 0 stone = stone
+applyBlinkV2' n ["0"] = applyBlinkV2' (n - 1) ["1"]
+applyBlinkV2' n [stone] =
+  if even $ length stone
+    then applyBlinkV2' (n - 1) [simplify left] ++ applyBlinkV2' (n - 1) [simplify right]
+    else applyBlinkV2' (n - 1) [show (2024 * read stone :: Integer)]
+  where
+    half = length stone `div` 2
+    left = take half stone
+    right = drop half stone
+applyBlinkV2' _ _ = error "Invalid input for applyBlinkV2'"
--- 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart1.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart1.hs
@@ -1,11 +1,11 @@
 module SolvePart1 (solvePart1) where
 
-import Blink (blink)
+import Blink (blinkV2)
 import Parse (parseInput)
 
 solvePart1 :: String -> String
 solvePart1 input = result
   where
     result = show $ length stones'
-    stones' = iterate blink stones !! 25
+    stones' = blinkV2 25 stones
     stones = parseInput input
