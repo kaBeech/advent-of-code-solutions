a00cb74a34ee1924f4e320e66c61b3a71e98d9ec
kaBeech <contact@kaBeech.com>
2025-05-24 18:41:45 -0700

    Scaffold SolvePart1

--- 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart1.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/src/SolvePart1.hs
@@ -1,4 +1,8 @@
 module SolvePart1 (solvePart1) where
 
+import Parse (parseInput)
+
 solvePart1 :: String -> String
-solvePart1 topographicalMap = topographicalMap
+solvePart1 input = concat $ iterate blink stones !! 25
+  where
+    stones = parseInput input
