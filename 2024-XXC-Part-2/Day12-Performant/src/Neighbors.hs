d840a1e2d45c5e1809e331aa52611ddc62a4be0c
kaBeech <contact@kaBeech.com>
2025-05-25 16:19:53 -0700

    Add getNeighborsAll

--- 2024-XXC-Part-2/Day12-Performant/src/Neighbors.hs
+++ 2024-XXC-Part-2/Day12-Performant/src/Neighbors.hs
@@ -1,9 +1,21 @@
-module Neighbors (getNeighbors, isAdjacent, isAdjacentToAny) where
+module Neighbors
+  ( getNeighbors,
+    isAdjacent,
+    isAdjacentToAny,
+    getNeighborsAll,
+  )
+where
 
 import Types (GardenFlatMap, Tile)
 
 getNeighbors :: GardenFlatMap -> Tile -> [Tile]
 getNeighbors gardenMap tile = filter (isAdjacent tile) gardenMap
+
+getNeighborsAll :: GardenFlatMap -> Tile -> [Tile]
+getNeighborsAll gardenMap tile = filter (isNeighbor tile) gardenMap
+
+isNeighbor :: Tile -> Tile -> Bool
+isNeighbor (_, (x, y)) (_, (x', y')) = abs (x - x') <= 1 && abs (y - y') <= 1
 
 isAdjacent :: Tile -> Tile -> Bool
 isAdjacent (_, (x, y)) (_, (x', y')) = abs (x - x') + abs (y - y') == 1
