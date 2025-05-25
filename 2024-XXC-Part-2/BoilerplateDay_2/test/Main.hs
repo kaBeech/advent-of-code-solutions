8d850ef47e91666ae831166f648af8a8757013c2
kaBeech <contact@kaBeech.com>
2025-05-24 22:44:25 -0700

    Cleanup boilerplate

--- 2024-XXC-Part-2/BoilerplateDay_2/test/Main.hs
+++ 2024-XXC-Part-2/BoilerplateDay_2/test/Main.hs
@@ -2,27 +2,21 @@ module Main (main) where
 
 import Control.Monad (unless)
 import SolvePart1 (solvePart1)
+import SolvePart2 (solvePart2)
 import System.Exit
 
 main :: IO ()
 main = do
-  input1 <- readFile "data/test_input_1.dat"
-  input2 <- readFile "data/test_input_2.dat"
-  input3 <- readFile "data/test_input_3.dat"
-  input4 <- readFile "data/test_input_4.dat"
-  input5 <- readFile "data/test_input_5.dat"
-  res1 <- test 1 input1 "1"
-  res2 <- test 2 input2 "2"
-  res3 <- test 3 input3 "4"
-  res4 <- test 4 input4 "3"
-  res5 <- test 5 input5 "36"
-  let allPassed = res1 && res2 && res3 && res4 && res5
+  input <- readFile "data/test_input.dat"
+  res1 <- test solvePart1 1 input "1"
+  res2 <- test solvePart2 2 input "2"
+  let allPassed = res1 && res2
   unless allPassed exitFailure
 
-test :: Int -> String -> String -> IO Bool
-test n input expected = do
+test :: (String -> String) -> Int -> String -> String -> IO Bool
+test f n input expected = do
   putStrLn $ "Test Input " ++ show n ++ " should equal " ++ expected ++ "..."
-  let result = solvePart1 input
+  let result = f input
       passed = result == expected
   putStrLn $ "Result: \n" ++ result
   if passed
