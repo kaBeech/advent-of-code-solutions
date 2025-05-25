aad8ff88af34f33c729f9c103002cb9cd6a3c8d4
kaBeech <contact@kaBeech.com>
2025-05-24 19:13:55 -0700

    Simplify numbers

--- 2024-XXC-Part-2/Day11-No-Modifier/src/Blink.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/src/Blink.hs
@@ -6,9 +6,12 @@ blink = concatMap applyBlink
 applyBlink :: String -> [String]
 applyBlink stone
   | stone == "0" = ["1"]
-  | even $ length stone = [left, right]
+  | even $ length stone = [simplify left, simplify right]
   | otherwise = [show $ 2024 * read stone]
   where
     half = length stone `div` 2
     left = take half stone
     right = drop half stone
+
+simplify :: String -> String
+simplify s = show (read s :: Integer)
