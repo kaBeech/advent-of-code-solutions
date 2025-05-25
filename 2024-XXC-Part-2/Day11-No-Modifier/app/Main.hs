251b4856a908fa191cf1c4cc3fa6276a6e580fc0
kaBeech <contact@kaBeech.com>
2025-05-24 18:23:04 -0700

    Update solution display text

--- 2024-XXC-Part-2/Day11-No-Modifier/app/Main.hs
+++ 2024-XXC-Part-2/Day11-No-Modifier/app/Main.hs
@@ -6,7 +6,7 @@ import SolvePart2 (solvePart2)
 main :: IO ()
 main = do
   input <- readFile "data/challenge_input.dat"
-  putStrLn "Part 1: What is the sum of the scores of all trailheads on your topographic map?"
+  putStrLn "Part 1: How many stones will you have after blinking 25 times?"
   putStrLn $ solvePart1 input
   putStrLn "Part 2: TBA"
   putStrLn $ solvePart2 input
