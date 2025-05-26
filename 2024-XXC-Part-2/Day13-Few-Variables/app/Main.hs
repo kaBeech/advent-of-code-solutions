13568d5e0070fa864418c9bbbff5778543359723
kaBeech <contact@kaBeech.com>
2025-05-25 18:36:08 -0700

    Update display text

--- 2024-XXC-Part-2/Day13-Few-Variables/app/Main.hs
+++ 2024-XXC-Part-2/Day13-Few-Variables/app/Main.hs
@@ -7,5 +7,5 @@ main = do
   input <- readFile "data/challenge_input.dat"
   putStrLn "Part 1: What is the fewest tokens you would have to spend to win all possible prizes?"
   putStrLn $ solvePart1 input
-  putStrLn "Part 2: TBA"
+  putStrLn "Part 2: What is the fewest tokens you would have to spend to win all possible prizes?"
   putStrLn $ solvePart2 input
