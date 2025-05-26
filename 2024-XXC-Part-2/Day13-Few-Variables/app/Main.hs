e92ecf0957aec8089c79a5f537463a2105b0e2a9
kaBeech <contact@kaBeech.com>
2025-05-25 18:31:32 -0700

    Update display text

--- 2024-XXC-Part-2/Day13-Few-Variables/app/Main.hs
+++ 2024-XXC-Part-2/Day13-Few-Variables/app/Main.hs
@@ -5,7 +5,7 @@ import Solve (solvePart1, solvePart2)
 main :: IO ()
 main = do
   input <- readFile "data/challenge_input.dat"
-  putStrLn "Part 1: What is the sum of the scores of all trailheads on your topographic map?"
+  putStrLn "Part 1: What is the fewest tokens you would have to spend to win all possible prizes?"
   putStrLn $ solvePart1 input
   putStrLn "Part 2: TBA"
   putStrLn $ solvePart2 input
