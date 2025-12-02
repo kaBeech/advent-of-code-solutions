74bbdd504bf4aa972905cabe7179a7ecfdaff7a6
kaBeech <contact@kaBeech.com>
2025-12-02 01:02:25 -0800

    Document and test with comment examples

--- 2025-Haskell-Simple/src/Day02/Part1.hs
+++ 2025-Haskell-Simple/src/Day02/Part1.hs
@@ -8,16 +8,30 @@ solvePart1 input = show $ sum $ concatMap invalidIds $ ranges input
 -- | If this list comprehension technique is unfamiliar, check out the
 --   following webpage:
 --   https://learnyouahaskell.github.io/starting-out.html#im-a-list-comprehension
+--
+-- | ==== __Examples__
+--   >>> invalidIds (95,115)
+--   [99]
 invalidIds :: (Int, Int) -> [Int]
 invalidIds (lower, upper) =
   [productId | productId <- [lower .. upper], invalidId productId]
 
+-- | ==== __Examples__
+--   >>> invalidId 11
+--   True
+--   >>> invalidId 1234
+--   False
+--   >>> invalidId 111
+--   False
 invalidId :: Int -> Bool
 invalidId productId = take halfLength text == drop halfLength text
   where
     text = show productId
     halfLength = length text `div` 2
 
+-- | ==== __Examples__
+--   >>> ranges "11-22,95-115"
+--   [(11,22),(95,115)]
 ranges :: Text -> [(Int, Int)]
 ranges input = rangesIntPairs
   where
--- 2025-Haskell-Simple/src/Day02/Part2.hs
+++ 2025-Haskell-Simple/src/Day02/Part2.hs
@@ -7,10 +7,24 @@ import Prelude hiding (length)
 solvePart2 :: Text -> String
 solvePart2 input = show $ sum $ concatMap invalidIds $ ranges input
 
+-- | If this list comprehension technique is unfamiliar, check out the
+--   following webpage:
+--   https://learnyouahaskell.github.io/starting-out.html#im-a-list-comprehension
+--
+-- | ==== __Examples__
+--   >>> invalidIds (95,115)
+--   [99,111]
 invalidIds :: (Int, Int) -> [Int]
 invalidIds (lower, upper) =
   [productId | productId <- [lower .. upper], invalidId productId]
 
+-- | ==== __Examples__
+--   >>> invalidId 11
+--   True
+--   >>> invalidId 1234
+--   False
+--   >>> invalidId 111
+--   True
 invalidId :: Int -> Bool
 invalidId productId = not $ null identicalSplits
   where
@@ -19,12 +33,24 @@ invalidId productId = not $ null identicalSplits
     identicalSplits =
       [size | size <- [1 .. halfLength], chunksAllIdentical size text]
 
+-- | ==== __Examples__
+--   >>> chunksAllIdentical 2 (pack "121212")
+--   True
+--   >>> chunksAllIdentical 1 (pack "121212")
+--   False
+--   >>> chunksAllIdentical 1 (pack "111111")
+--   True
+--   >>> chunksAllIdentical 2 (pack "123456")
+--   False
 chunksAllIdentical :: Int -> Text -> Bool
 chunksAllIdentical size text = all (== firstChunk) chunks
   where
     firstChunk = at chunks 0
     chunks = chunksOf size text
 
+-- | ==== __Examples__
+--   >>> ranges "11-22,95-115"
+--   [(11,22),(95,115)]
 ranges :: Text -> [(Int, Int)]
 ranges input = rangesIntPairs
   where
