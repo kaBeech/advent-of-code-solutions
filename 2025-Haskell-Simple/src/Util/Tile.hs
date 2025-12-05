dc50952e1d0d9535125cad3384c3eff2968cbdf5
kaBeech <contact@kaBeech.com>
2025-12-04 20:10:55 -0800

    Reorganize

--- 2025-Haskell-Simple/src/Util/Tile.hs
+++ 2025-Haskell-Simple/src/Util/Tile.hs
@@ -2,6 +2,14 @@ module Util.Tile (tilesAdjacentTo) where
 
 import Types (Tile, tileXY)
 
+tilesAdjacentTo :: Tile -> [Tile] -> [Tile]
+tilesAdjacentTo tile = filter (tilesAdjacent tile)
+
+tilesAdjacent :: Tile -> Tile -> Bool
+tilesAdjacent firstTile secondTile =
+  tilesOrthogonallyAdjacent firstTile secondTile
+    || tilesDiagonallyAdjacent firstTile secondTile
+
 tilesHorizontallyAdjacent :: Tile -> Tile -> Bool
 tilesHorizontallyAdjacent firstTile secondTile =
   abs (x1 - x2) == 1 && y1 == y2
@@ -27,11 +35,3 @@ tilesDiagonallyAdjacent firstTile secondTile =
   where
     (x1, y1) = tileXY firstTile
     (x2, y2) = tileXY secondTile
-
-tilesAdjacent :: Tile -> Tile -> Bool
-tilesAdjacent firstTile secondTile =
-  tilesOrthogonallyAdjacent firstTile secondTile
-    || tilesDiagonallyAdjacent firstTile secondTile
-
-tilesAdjacentTo :: Tile -> [Tile] -> [Tile]
-tilesAdjacentTo tile = filter (tilesAdjacent tile)
