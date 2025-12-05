2da9f573d860c51a2252e3f0078cd9c03b1a5e4b
kaBeech <contact@kaBeech.com>
2025-12-04 19:49:28 -0800

    Clarify with Types

--- 2025-Haskell-Simple/src/Day04/Part1.hs
+++ 2025-Haskell-Simple/src/Day04/Part1.hs
@@ -1,7 +1,7 @@
 module Day04.Part1 (solvePart1) where
 
 import Data.Text (Text, pack)
-import Types (Tile (..), toTileMap)
+import Types (Tile (..), TileMap, toTileMap)
 import Util.Tile (tilesAdjacentTo)
 
 solvePart1 :: Text -> String
@@ -10,15 +10,15 @@ solvePart1 input =
   where
     tileMap = toTileMap input
 
-hasAccessiblePaperRoll :: [Tile] -> Tile -> Bool
+hasAccessiblePaperRoll :: TileMap -> Tile -> Bool
 hasAccessiblePaperRoll tileMap tile =
   hasPaperRoll tile && hasLessThan4Neighbors tileMap tile
 
-hasLessThan4Neighbors :: [Tile] -> Tile -> Bool
+hasLessThan4Neighbors :: TileMap -> Tile -> Bool
 hasLessThan4Neighbors tileMap tile =
   length (tilesAdjacentTo tile (withPaperRolls tileMap)) < 4
 
-withPaperRolls :: [Tile] -> [Tile]
+withPaperRolls :: TileMap -> TileMap
 withPaperRolls = filter hasPaperRoll
 
 hasPaperRoll :: Tile -> Bool
