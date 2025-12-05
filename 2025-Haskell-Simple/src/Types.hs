9099637544ad8aa44376eb9b24ea67988a7892d9
kaBeech <contact@kaBeech.com>
2025-12-04 17:42:20 -0800

    Add TileMap type

--- 2025-Haskell-Simple/src/Types.hs
+++ 2025-Haskell-Simple/src/Types.hs
@@ -1,1 +1,34 @@
 module Types where
+
+import Data.Text (Text, chunksOf, lines, unpack)
+import Prelude hiding (lines)
+
+type Coordinates = [Int]
+
+data Tile = Tile
+  { tileContent :: Text,
+    tileCoordinates :: Coordinates
+  }
+  deriving (Eq)
+
+toTile :: Text -> Coordinates -> Tile
+toTile text coordinates =
+  Tile {tileContent = text, tileCoordinates = coordinates}
+
+instance Ord Tile where
+  (<=) firstTile secondTile =
+    tileCoordinates firstTile <= tileCoordinates secondTile
+
+type TileMap = [Tile]
+
+toTileMap :: Text -> [(Coordinates, Text)]
+toTileMap text = concatMap toColumns numberedRows
+  where
+    rows = reverse $ lines text
+    numberedRows = zip [0 .. length rows - 1] rows
+
+toColumns :: (Int, Text) -> [(Coordinates, Text)]
+toColumns (x, rowText) =
+  zip
+    [[x, y] | y <- [0 .. length (unpack rowText) - 1]]
+    (chunksOf 1 rowText)
