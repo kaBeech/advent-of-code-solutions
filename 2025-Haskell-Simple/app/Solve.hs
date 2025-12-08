145276a61a416acb2538fb0c8b47ead6b8c885d2
kaBeech <contact@kaBeech.com>
2025-12-07 16:46:53 -0800

    Prettify console output

--- 2025-Haskell-Simple/app/Solve.hs
+++ 2025-Haskell-Simple/app/Solve.hs
@@ -13,11 +13,12 @@ solveDay n = do
   prompt1 <- readFile $ "data/Day" ++ dayString n ++ "/prompt1.txt"
   prompt2 <- readFile $ "data/Day" ++ dayString n ++ "/prompt2.txt"
   let (resultPart1, resultPart2) = execDay n input
-  putStrLn $ "Solving Day " ++ dayString n ++ "!"
+  putStrLn $ "==================================\n"
+  putStrLn $ "Solving Day " ++ dayString n ++ "!\n"
   putStrLn $ "Part 1: " ++ prompt1
-  putStrLn $ "Solution: " ++ resultPart1
+  putStrLn $ "Solution: " ++ resultPart1 ++ "\n"
   putStrLn $ "Part 2: " ++ prompt2
-  putStrLn $ "Solution: " ++ resultPart2
+  putStrLn $ "Solution: " ++ resultPart2 ++ "\n"
 
 time :: IO t -> IO t
 time f = do
@@ -25,5 +26,5 @@ time f = do
   result <- f
   end <- getCPUTime
   let diff = fromIntegral (end - start) / (10 ^ (12 :: Integer))
-  printf "Computation time: %0.3f sec\n" (diff :: Double)
+  printf "Computation time: %0.3f sec\n\n" (diff :: Double)
   return result
