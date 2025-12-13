2d509d03147c6e1e1581b34debc9b18ed50d8b24
kaBeech <contact@kaBeech.com>
2025-12-13 14:56:28 -0800

    Parse input

--- 2025-Haskell-Simple/src/Day08/Part1.hs
+++ 2025-Haskell-Simple/src/Day08/Part1.hs
@@ -1,6 +1,10 @@
 module Day08.Part1 (solvePart1) where
 
-import Data.Text (Text, unpack)
+import Data.Text (Text, lines)
+import Util.Text (toXYZ)
+import Prelude hiding (lines)
 
 solvePart1 :: Text -> String
-solvePart1 input = unpack input
+solvePart1 input = show coords
+  where
+    coords = map toXYZ $ lines input
