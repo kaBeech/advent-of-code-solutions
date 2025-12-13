0fdd716a0f137bbddd5b16d48e8c64356365ccfe
kaBeech <contact@kaBeech.com>
2025-12-13 13:49:27 -0800

    Improve Performance
    Only touch tiles if they have a paper roll or are neighbours to one

--- 2025-Haskell-Simple/src/Day04/Part1.hs
+++ 2025-Haskell-Simple/src/Day04/Part1.hs
@@ -1,9 +1,8 @@
 module Day04.Part1 (solvePart1) where
 
-import Data.List (intersect)
 import Data.Text (Text)
 import Math.Geometry.Grid (neighbours)
-import Math.Geometry.GridMap (GridMap (keys))
+import Math.Geometry.GridMap (GridMap (keys), (!))
 import qualified Math.Geometry.GridMap as GridMap
 import Util.Coordinates (XYCoordinates)
 import Util.Text (toTileMap)
@@ -11,13 +10,17 @@ import Util.Tile (TileMap)
 
 solvePart1 :: Text -> String
 solvePart1 input =
-  show $ length $ filter (hasLessThan4Neighbors paperRolls) (keys paperRolls)
+  show $ length $ filter (isAccessible tileMap) (keys paperRolls)
   where
-    paperRolls = GridMap.filter ('@' ==) (toTileMap input)
+    tileMap = toTileMap input
+    paperRolls = GridMap.filter ('@' ==) tileMap
 
-hasLessThan4Neighbors :: TileMap -> XYCoordinates -> Bool
-hasLessThan4Neighbors tileMap coords =
-  length neighborsActual < 4
+isAccessible :: TileMap -> XYCoordinates -> Bool
+isAccessible tileMap coords =
+  length neighborsWithPaperRolls < 4
   where
-    neighborsOnFullGrid = neighbours tileMap coords
-    neighborsActual = neighborsOnFullGrid `intersect` keys tileMap
+    neighborsWithPaperRolls =
+      filter (hasPaperRoll tileMap) (neighbours tileMap coords)
+
+hasPaperRoll :: TileMap -> XYCoordinates -> Bool
+hasPaperRoll tileMap coords = tileMap ! coords == '@'
