d5ceb6117a971048257f0803e437364adddd061c
kaBeech <contact@kaBeech.com>
2025-12-13 13:31:21 -0800

    Fix logic for determining neighbors

--- 2025-Haskell-Simple/src/Day04/Part2.hs
+++ 2025-Haskell-Simple/src/Day04/Part2.hs
@@ -1,5 +1,6 @@
 module Day04.Part2 (solvePart2) where
 
+import Data.List (intersect, (\\))
 import Data.Text (Text)
 import Math.Geometry.Grid (neighbours)
 import Math.Geometry.GridMap (GridMap (keys))
@@ -17,16 +18,18 @@ solvePart2 input =
       removeRolls (keys originalPaperRolls) originalPaperRolls
 
 hasLessThan4Neighbors :: TileMap -> XYCoordinates -> Bool
-hasLessThan4Neighbors tileMap tile =
-  length (neighbours tileMap tile) < 4
+hasLessThan4Neighbors tileMap coords =
+  length neighborsActual < 4
+  where
+    neighborsOnFullGrid = neighbours tileMap coords
+    neighborsActual = neighborsOnFullGrid `intersect` keys tileMap
 
 removeRolls :: [XYCoordinates] -> TileMap -> TileMap
 removeRolls [] tileMap = tileMap
 removeRolls (coords : coordsToCheck) tileMap
-  | hasLessThan4Neighbors tileMap coords =
-      removeRolls (adjacentTiles ++ dedup coordsToCheck) $
-        GridMap.delete coords tileMap
+  | hasLessThan4Neighbors tileMap coords = removeRolls coordsToCheck' tileMap'
   | otherwise = removeRolls coordsToCheck tileMap
   where
-    adjacentTiles = neighbours tileMap coords
-    dedup = filter (`notElem` adjacentTiles)
+    adjacentTiles = neighbours tileMap coords `intersect` keys tileMap
+    coordsToCheck' = adjacentTiles ++ (coordsToCheck \\ adjacentTiles)
+    tileMap' = GridMap.delete coords tileMap
