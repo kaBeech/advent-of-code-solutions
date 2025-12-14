5727e676e1a585b38d5aa8454e5ade2aafb26ec5
kaBeech <contact@kaBeech.com>
2025-12-14 00:49:38 -0800

    Improve Performance

--- 2025-Haskell-Simple/src/Day08/Part1.hs
+++ 2025-Haskell-Simple/src/Day08/Part1.hs
@@ -1,20 +1,14 @@
 module Day08.Part1 (solvePart1) where
 
 import Data.Function (on)
-import Data.List (minimumBy, sortBy)
+import Data.List (sort, sortBy)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Ord (Down (Down), comparing)
 import Data.Text (Text, lines)
 import Util.Coordinates (XYZCoordinates, distanceXYZ)
 import Util.Text (toXYZ)
-import Util.Tuple (sort)
 import Prelude hiding (lines)
-
-infinity :: Float
-infinity = fromIntegral (maxBound :: Int)
-
-sortByLength :: [[a]] -> [[a]]
-sortByLength = sortBy (flip compare `on` length)
 
 type JunctionBox = XYZCoordinates
 
@@ -22,32 +16,72 @@ type Circuit = [JunctionBox]
 
 type CircuitMap = Map XYZCoordinates Circuit
 
-type Connections = [(XYZCoordinates, XYZCoordinates)]
+type Connection = (Float, XYZCoordinates, XYZCoordinates)
+
+type Connections = [Connection]
 
 solvePart1 :: Text -> String
 solvePart1 input =
-  show $ product $ take 3 $ map length $ sortByLength $ Map.elems circuitMap
+  show $ product $ map length $ take 3 longestCircuits
   where
+    maxConnections = numberOfPairs junctionBoxes
+    longestCircuits = sortBy (flip compare `on` length) $ Map.elems circuitMap
+    closestPairs' = sort $ closestPairs [] maxConnections junctionBoxes
     junctionBoxes = map toXYZ $ lines input
     (_, circuitMap) =
-      iterate (connectClosestJunctionBoxes junctionBoxes) ([], Map.empty) !! 1000
+      iterate
+        (connectClosestJunctionBoxes junctionBoxes)
+        (closestPairs', Map.empty)
+        !! maxConnections
+
+numberOfPairs :: [JunctionBox] -> Int
+numberOfPairs junctionBoxes | length junctionBoxes > 20 = 1000 | otherwise = 10
+
+closestPairs :: Connections -> Int -> [JunctionBox] -> Connections
+closestPairs connections _ [] = connections
+closestPairs connections _ [_] = connections
+closestPairs connections maxConnections (jb1 : rest) =
+  closestPairs connections' maxConnections rest
+  where
+    connections' = addPairs connections maxConnections jb1 rest
+
+addPairs :: Connections -> Int -> JunctionBox -> [JunctionBox] -> Connections
+addPairs connections _ _ [] = connections
+addPairs connections maxConnections jb1 (jb2 : rest)
+  | length connections < maxConnections =
+      addPairs (newConnection : connections) maxConnections jb1 rest
+  | newDistance < furthestDistance =
+      addPairs (newConnection : connectionsTail) maxConnections jb1 rest
+  | otherwise = addPairs connections maxConnections jb1 rest
+  where
+    newDistance = distanceXYZ jb1 jb2
+    newConnection = (newDistance, jb1, jb2)
+    sortedConnections = sortBy (comparing Data.Ord.Down) connections
+    (furthestDistance, connectionsTail) = getFurthest sortedConnections
+
+getFurthest :: Connections -> (Float, Connections)
+getFurthest ((furthestDistance, _, _) : connectionsTail) =
+  (furthestDistance, connectionsTail)
+getFurthest [] = error "No connections!"
 
 connectClosestJunctionBoxes ::
   [JunctionBox] -> (Connections, CircuitMap) -> (Connections, CircuitMap)
-connectClosestJunctionBoxes junctionBoxes (connections, circuits) =
-  (newConnection : connections, circuits')
-  where
-    circuits' =
-      mergeCircuits circuits (Map.keys jb1Key) (Map.keys jb2Key) newConnection
-    jb1Key = Map.filter (elem jb1) circuits
-    jb2Key = Map.filter (elem jb2) circuits
-    newConnection@(jb1, jb2) = sort $ closestJunctionBoxes ((0, 0, 0), (0, 0, 0)) junctionBoxes infinity connections junctionBoxes
+connectClosestJunctionBoxes _ ([], _) = error "Out of connections!"
+connectClosestJunctionBoxes
+  _
+  (newConnection@(_, jb1, jb2) : connections, circuits) =
+    (connections, circuits')
+    where
+      circuits' =
+        mergeCircuits circuits (Map.keys jb1Key) (Map.keys jb2Key) newConnection
+      jb1Key = Map.filter (elem jb1) circuits
+      jb2Key = Map.filter (elem jb2) circuits
 
 mergeCircuits ::
   CircuitMap ->
   [XYZCoordinates] ->
   [XYZCoordinates] ->
-  (JunctionBox, JunctionBox) ->
+  Connection ->
   CircuitMap
 mergeCircuits allCircuits [key1] [key2] _ =
   Map.insertWith
@@ -55,11 +89,11 @@ mergeCircuits allCircuits [key1] [key2] _ =
     key1
     (allCircuits Map.! key2)
     (Map.delete key2 allCircuits)
-mergeCircuits allCircuits [] [key2] (jb1, _) =
+mergeCircuits allCircuits [] [key2] (_, jb1, _) =
   Map.insertWith (++) key2 [jb1] allCircuits
-mergeCircuits allCircuits [key1] [] (_, jb2) =
+mergeCircuits allCircuits [key1] [] (_, _, jb2) =
   Map.insertWith (++) key1 [jb2] allCircuits
-mergeCircuits allCircuits [] [] (jb1, jb2) =
+mergeCircuits allCircuits [] [] (_, jb1, jb2) =
   Map.insertWith (++) jb2 [jb1, jb2] allCircuits
 mergeCircuits _ keys1 keys2 _ =
   error $
@@ -67,29 +101,3 @@ mergeCircuits _ keys1 keys2 _ =
       ++ show keys1
       ++ " and "
       ++ show keys2
-
-closestJunctionBoxes :: (JunctionBox, JunctionBox) -> [JunctionBox] -> Float -> Connections -> Circuit -> (JunctionBox, JunctionBox)
-closestJunctionBoxes closestPair [] _ _ _ = closestPair
-closestJunctionBoxes closestPair [jb] _ _ _ = closestPair
-closestJunctionBoxes closestPair (jb : rest) closestDistance connections allJunctionBoxes
-  | closestDistance' < closestDistance = closestJunctionBoxes (jb, jb') rest closestDistance' ((jb, jb') : connections) allJunctionBoxes
-  | otherwise = closestJunctionBoxes closestPair rest closestDistance connections allJunctionBoxes
-  where
-    (jb', closestDistance') = closestJunctionBox jb closestDistance connections rest
-
-closestJunctionBox :: JunctionBox -> Float -> Connections -> [JunctionBox] -> (JunctionBox, Float)
-closestJunctionBox jb closestDistance connections rest = (closestJb, closestDistance')
-  where
-    closestJb = minimumBy (compare `on` distance closestDistance connections jb) rest
-    closestDistance' = distance closestDistance connections jb closestJb
-
-distance :: Float -> Connections -> JunctionBox -> JunctionBox -> Float
-distance closestDistance connections jb1@(x1, y1, z1) jb2@(x2, y2, z2)
-  | absDiff x1 x2 > closestDistance = infinity
-  | absDiff y1 y2 > closestDistance = infinity
-  | absDiff z1 z2 > closestDistance = infinity
-  | jb1 == jb2 = infinity
-  | sort (jb1, jb2) `elem` connections = infinity
-  | otherwise = distanceXYZ jb1 jb2
-  where
-    absDiff a b = fromIntegral $ abs (a - b)
